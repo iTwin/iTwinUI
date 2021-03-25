a2383ee3b50a9c1e438431839a24b1ccc7eb5c1c
Mayank <9084735+mayank99@users.noreply.github.com>
2021-03-25 19:43:23 +0000

    Merged PR 152784: Remove positioning from footer
    - Fixed sticky footer bug by removing all positioning rules
    - Also updated visual tests
    
    Related work items: #580777

--- backstop/scenarios/footer.js
+++ backstop/scenarios/footer.js
@@ -1,3 +1,13 @@
 const { scenario } = require('../scenarioHelper');
 
-module.exports = [scenario('basic', { selectors: ['document'] })];
+module.exports = [
+  scenario('footer hidden below viewport', {
+    selectors: ['viewport'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+  scenario('footer visible in viewport', {
+    selectors: ['viewport'],
+    viewports: [{ width: 800, height: 600 }],
+    scrollToSelector: ['.iui-legal-footer'],
+  }),
+];
