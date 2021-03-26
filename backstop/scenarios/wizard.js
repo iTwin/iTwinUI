3659e4b16c185578ebafd31311bbb48d14663aed
Vykis <81580355+veekeys@users.noreply.github.com>
2021-03-26 12:00:37 +0000

    Merged PR 153398: Wizard tests, iui-focus mixin improvement
    Thank you for your contribution to the iTwinUi project! Please make sure to complete all of the items below before submitting the PR.
    
    ## Checklist
    
    - [ ] Add visual tests for your component
    - [ ] Verify that all existing tests pass
    - [ ] Add an entry to the changelog for any new components and changes
    - [ ] Add screenshots of the key elements of the component below
    - [ ] Add any additional comments below describing the features you've implemented
    - [ ] Link work item
    
    ## Screenshots
    
    ## Additional Comments
    Added Wizard tests
    Added focus on completed steps
    Improve focus mixin, so you could pass different value
    Added Green focus var
    
    Related work items: #582970

--- backstop/scenarios/wizard.js
+++ backstop/scenarios/wizard.js
@@ -1,3 +1,25 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
-module.exports = [scenario('basic')];
+module.exports = [
+  scenario('Default wizard', {
+    selectors: ['#demo-default'],
+  }),
+  scenario('Long wizard', {
+    selectors: ['#demo-long'],
+  }),
+  scenario('Workflow wizard', {
+    selectors: ['#demo-workflow'],
+  }),
+  scenario('Default wizard hover on complete', {
+    actions: [hover('#demo-default .iui-wizards-step-completed:first-child')],
+    selectors: ['#demo-default'],
+  }),
+  scenario('Long wizard focus on complete', {
+    actions: [
+      focus(
+        '#demo-long .iui-wizards-step-completed:first-child > .iui-wizards-step-circle'
+      ),
+    ],
+    selectors: ['#demo-long'],
+  }),
+];
