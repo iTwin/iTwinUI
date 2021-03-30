49ca2b7efe6bce5de1f846d87162ff3b3ec63cb4
Jon <849817+FlyersPh9@users.noreply.github.com>
2021-03-30 20:48:26 +0000

    Merged PR 154417: Add select tests
    - Added tests for select component.
    - Set fixed width of selects for tests.
    - Added examples with the menu open by default.
    
    Related work items: #586682

--- backstop/scenarios/select.js
+++ backstop/scenarios/select.js
@@ -1,3 +1,37 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
-module.exports = [scenario('basic')];
+module.exports = [
+  // Select types
+  scenario('Type Default', {
+    selectors: ['#demo-default'],
+  }),
+  scenario('Type Default Opened', {
+    selectors: ['#demo-default-opened'],
+  }),
+
+  // Hover states
+  //// Select
+  scenario('State hover', {
+    actions: [hover('#test-select-1')],
+    selectors: ['#demo-default-singular'],
+  }),
+
+  //// Select menu item
+  scenario('State Menu hover', {
+    actions: [hover('#test-select-2')],
+    selectors: ['#demo-default-singular-opened'],
+  }),
+
+  // Focus states
+  //// Select
+  scenario('State focus', {
+    actions: [focus('#test-select-3')],
+    selectors: ['#demo-default-singular'],
+  }),
+
+  //// Select menu item
+  scenario('State Menu focus', {
+    actions: [focus('#test-select-2')],
+    selectors: ['#demo-default-singular-opened'],
+  }),
+];
