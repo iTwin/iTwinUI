c0ae25107fe9aba150cc3611822895bb9ecf73db
Mayank <9084735+mayank99@users.noreply.github.com>
2021-04-08 19:11:27 +0000

    Merged PR 155999: Add license and copyright headers
    - Added MIT license file.
    - Added `scripts/copyrightLinter.js` to lint html, scss and js. New command is `yarn lint:copyright`.
    - Updated `yarn build:css` to write headers into lib/css after post processing.
    - Updated all files with copyright headers.
    
    Related work items: #570146

---
+++ scripts/copyrightLinter.js
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+const fs = require('fs');
+const fg = require('fast-glob');
+
+const pattern = process.argv.slice(2).filter((x) => x !== '--fix');
+const filePaths = fg.sync(pattern, {
+  dot: true,
+  ignore: [
+    '**/node_modules',
+    '**/backstop/**/*.{js,css}',
+    '**/backstop/minified',
+    '**/backstop/results',
+  ],
+});
+
+const copyrightLine1 = `Copyright (c) Bentley Systems, Incorporated. All rights reserved.`;
+const copyrightLine2 = `See LICENSE.md in the project root for license terms and full copyright notice.`;
+
+const copyrightBannerScss = `// ${copyrightLine1}\n// ${copyrightLine2}`;
+const copyrightBannerHtml = `<!--\n  ${copyrightLine1}\n  ${copyrightLine2}\n-->`;
+const copyrightBannerJs =
+  '/*---------------------------------------------------------------------------------------------\n ' +
+  `* ${copyrightLine1}\n ` +
+  `* ${copyrightLine2}\n ` +
+  '*--------------------------------------------------------------------------------------------*/';
+
+if (filePaths) {
+  filePaths.forEach((filePath) => {
+    const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+    if (
+      !fileContent.includes(copyrightLine1) &&
+      !fileContent.includes(copyrightLine2)
+    ) {
+      if (process.argv.includes('--fix')) {
+        switch (filePath.substring(filePath.lastIndexOf('.'))) {
+          case '.js':
+          case '.ts':
+          case '.css':
+            fs.writeFileSync(filePath, `${copyrightBannerJs}\n${fileContent}`);
+            break;
+          case '.html':
+            fs.writeFileSync(
+              filePath,
+              `${copyrightBannerHtml}\n${fileContent}`
+            );
+            break;
+          case '.scss':
+            fs.writeFileSync(
+              filePath,
+              `${copyrightBannerScss}\n${fileContent}`
+            );
+            break;
+        }
+      } else {
+        process.exitCode = 1;
+        console.log(`copyrightLinter.js failed at ${filePath}`);
+      }
+    }
+  });
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 const red = (text) => `\x1b[31m${text}\x1b[0m`;
 const green = (text) => `\x1b[32m${text}\x1b[0m`;
 const yellow = (text) => `\x1b[33m${text}\x1b[0m`;
