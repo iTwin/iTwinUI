4865c25c1ef88c1fd440e580a1b33483c7aae7ff
FlyersPh9 <FlyersPh9@gmail.com>
2021-04-13 14:37:29 -0400

    Adding and approving tests.

--- backstop/scenarios/date-picker.js
+++ backstop/scenarios/date-picker.js
@@ -5,6 +5,9 @@ module.exports = [
   scenario('Type Date', {
     selectors: ['#demo-date'],
   }),
+  scenario('Type Date & Time', {
+    selectors: ['#demo-date-and-time'],
+  }),
 
   // Hover states
   //// Date
@@ -12,11 +15,19 @@ module.exports = [
     actions: [hover('#test-date')],
     selectors: ['#demo-date'],
   }),
+  scenario('State hover time', {
+    actions: [hover('#test-time')],
+    selectors: ['#demo-date-and-time'],
+  }),
 
   // Focus states
   //// Date
   scenario('State focus date', {
     actions: [focus('#test-date')],
     selectors: ['#demo-date'],
+  }),
+  scenario('State focus time', {
+    actions: [focus('#test-time')],
+    selectors: ['#demo-date-and-time'],
   }),
 ];
