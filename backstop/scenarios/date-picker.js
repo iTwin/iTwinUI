32a2dc8b88e7603309b014fedce8d991564f6dd1
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2021-04-13 09:08:35 -0700

    Date picker class name refactor (#2)
    - All class names have been shortened.
    - `.iui-date-picker` no longer wraps around the entry point.
    - Added tests.
    - Removed entry point from demo page.
    - Added some accessibility options suggested in [this article](https://www.w3.org/TR/2019/NOTE-wai-aria-practices-1.1-20190814/examples/dialog-modal/datepicker-dialog.html).

--- backstop/scenarios/date-picker.js
+++ backstop/scenarios/date-picker.js
@@ -1,8 +1,22 @@
-const { scenario, click } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('basic', {
-    actions: [click('#date-picker-toggle')],
-    selectors: ['.iui-date-picker-calendar'],
+  // Date picker types
+  scenario('Type Date', {
+    selectors: ['#demo-date'],
+  }),
+
+  // Hover states
+  //// Date
+  scenario('State hover date', {
+    actions: [hover('#test-date')],
+    selectors: ['#demo-date'],
+  }),
+
+  // Focus states
+  //// Date
+  scenario('State focus date', {
+    actions: [focus('#test-date')],
+    selectors: ['#demo-date'],
   }),
 ];
