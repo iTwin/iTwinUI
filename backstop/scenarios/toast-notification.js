6f60a603ee9e3b24dfa1266ba5d5b939da0bb58d
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2021-04-15 08:23:37 -0700

    Toast refactor (#13)
    - Class name refactor.
    - Using borderless button for close button.
    - Close button is vertically centered to better match alert.
    - Anchor link is using the color of the status.
    - Support for multiple anchor links.
    - Added tests.

--- backstop/scenarios/toast-notification.js
+++ backstop/scenarios/toast-notification.js
@@ -1,3 +1,36 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
-module.exports = [scenario('basic', { selectors: ['.iui-toast-wrapper'] })];
+module.exports = [
+  // Toast types
+  scenario('Type All', {
+    selectors: ['.iui-toast-wrapper'],
+  }),
+
+  // Hover states
+  scenario('State hover link in informational', {
+    actions: [hover('#demo-informational a')],
+    selectors: ['#demo-informational'],
+  }),
+  scenario('State hover link in positive', {
+    actions: [hover('#demo-positive a')],
+    selectors: ['#demo-positive'],
+  }),
+  scenario('State hover link in negative', {
+    actions: [hover('#demo-negative a')],
+    selectors: ['#demo-negative'],
+  }),
+
+  // Focus states
+  scenario('State focus link in informational', {
+    actions: [focus('#demo-informational a')],
+    selectors: ['#demo-informational'],
+  }),
+  scenario('State focus link in positive', {
+    actions: [focus('#demo-positive a')],
+    selectors: ['#demo-positive'],
+  }),
+  scenario('State focus link in negative', {
+    actions: [focus('#demo-negative a')],
+    selectors: ['#demo-negative'],
+  }),
+];
