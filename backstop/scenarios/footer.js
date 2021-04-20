f3ce4d28d96d0905df88fb598f8343c371019392
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-04-20 11:47:15 -0400

    Adding tests.

--- backstop/scenarios/footer.js
+++ backstop/scenarios/footer.js
@@ -1,13 +1,32 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('footer hidden below viewport', {
+  // Footer types
+  scenario('Type hidden below viewport', {
     selectors: ['viewport'],
     viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('footer visible in viewport', {
+  scenario('Type visible in viewport', {
     selectors: ['viewport'],
     viewports: [{ width: 800, height: 600 }],
     scrollToSelector: ['.iui-legal-footer'],
+  }),
+
+  // Hover states
+  //// Footer link
+  scenario('State hover', {
+    selectors: ['viewport'],
+    viewports: [{ width: 800, height: 600 }],
+    scrollToSelector: ['.iui-legal-footer'],
+    actions: [hover('#test-link')],
+  }),
+
+  // Focus states
+  //// Footer link
+  scenario('State focus', {
+    selectors: ['viewport'],
+    viewports: [{ width: 800, height: 600 }],
+    scrollToSelector: ['.iui-legal-footer'],
+    actions: [focus('#test-link')],
   }),
 ];
