caf319f852a08b183030144728301184e72ff46d
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-04-22 17:25:07 -0400

    Changes and adding tests.

--- backstop/scenarios/menu.js
+++ backstop/scenarios/menu.js
@@ -1,7 +1,18 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('basic', {
-    selectors: ['#demo-default'],
+  // Menu types
+  scenario('Type list', {
+    selectors: ['#demo-list'],
+  }),
+  scenario('Type complex', {
+    selectors: ['#demo-complex'],
+  }),
+
+  // Hover & focus states
+  //// List menu
+  scenario('State hover and focus', {
+    selectors: ['#demo-list'],
+    actions: [hover('#test-link-1'), focus('#test-link-2')],
   }),
 ];
