0f47de2d332f5b3f918aeb4acc2141fbbc000814
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-04-23 17:41:11 -0400

    Refining and approving tests.

--- backstop/scenarios/user-icon.js
+++ backstop/scenarios/user-icon.js
@@ -1,3 +1,13 @@
 const { scenario } = require('../scenarioHelper');
 
 module.exports = [scenario('basic')];
+
+module.exports = [
+  // User icon types
+  scenario('Type default', {
+    selectors: ['#demo-default'],
+  }),
+  scenario('Type online status', {
+    selectors: ['#demo-status'],
+  }),
+];
