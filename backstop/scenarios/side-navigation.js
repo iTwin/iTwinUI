dbe91619b02dd160e17b3250170f473fc504aa10
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-05-11 13:04:22 -0400

    Updating and approving tests.

--- backstop/scenarios/side-navigation.js
+++ backstop/scenarios/side-navigation.js
@@ -1,4 +1,4 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, click, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
   // Side Navigation types
@@ -7,6 +7,20 @@ module.exports = [
   }),
   scenario('Type expanded', {
     actions: [click('#test-expand')],
+    selectors: ['#demo-side-navigation'],
+  }),
+
+  // Hover & focus states
+  scenario('State collapsed hover and focus', {
+    actions: [hover('#test-tab-2'), focus('#test-tab-1')],
+    selectors: ['#demo-side-navigation'],
+  }),
+  scenario('State expanded hover and focus', {
+    actions: [
+      click('#test-expand'),
+      hover('#test-tab-2'),
+      focus('#test-tab-1'),
+    ],
     selectors: ['#demo-side-navigation'],
   }),
 ];
