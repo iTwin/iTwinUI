b16adc3f0958a33702d121aab7f6f69b042ecef6
Mayank <9084735+mayank99@users.noreply.github.com>
2021-05-17 14:27:47 -0400

    Add side navigation (#73)
    Added `.iui-side-navigation` and `.iui-sidenav-button` components.
    
    Co-authored-by: FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>

---
+++ backstop/scenarios/side-navigation.js
@@ -0,0 +1,26 @@
+const { scenario, click, hover, focus } = require('../scenarioHelper');
+
+module.exports = [
+  // Side Navigation types
+  scenario('Type collapsed', {
+    selectors: ['#demo-side-navigation'],
+  }),
+  scenario('Type expanded', {
+    actions: [click('#test-expand')],
+    selectors: ['#demo-side-navigation'],
+  }),
+
+  // Hover & focus states
+  scenario('State collapsed hover and focus', {
+    actions: [hover('#test-tab-2'), focus('#test-tab-1')],
+    selectors: ['#demo-side-navigation'],
+  }),
+  scenario('State expanded hover and focus', {
+    actions: [
+      click('#test-expand'),
+      hover('#test-tab-2'),
+      focus('#test-tab-1'),
+    ],
+    selectors: ['#demo-side-navigation'],
+  }),
+];
