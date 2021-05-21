178dfdf857dc26ebbb81c017d1a2aeb62c4779f9
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2021-05-21 13:01:58 -0400

    Add additional sizes to radial progress indicator (#99)

--- backstop/scenarios/progress-indicator.js
+++ backstop/scenarios/progress-indicator.js
@@ -4,11 +4,11 @@ module.exports = [
   // Progress indicator types
   scenario('linear', {
     selectors: ['#demo-linear'],
-    hideSelectors: ['.iui-indeterminate'],
+    hideSelectors: ['.iui-indeterminate', 'h3'],
   }),
   scenario('radial', {
     selectors: ['#demo-radial'],
-    hideSelectors: ['.iui-indeterminate'],
+    hideSelectors: ['h3', 'p'],
   }),
   scenario('overlay', {
     selectors: ['#demo-overlay'],
