87846f387adeb15c5e4cd4e62ee43475083be25a
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-05-21 16:34:34 -0400

    Changes to tests.

--- backstop/scenarios/breadcrumbs.js
+++ backstop/scenarios/breadcrumbs.js
@@ -2,10 +2,21 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-  const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
-  module.exports = [
-    scenario('default', {
-      selectors: ['#demo-default'],
-    }),
-  ];
+module.exports = [
+  // Breadcrumb types
+  scenario('Type Breadcrumb', {
+    selectors: ['#demo-all'],
+  }),
+
+  // Hover & focus states
+  scenario('State hover and focus links', {
+    actions: [hover('#test-1'), focus('#test-2')],
+    selectors: ['#demo-anchors'],
+  }),
+  scenario('State hover and focus buttons', {
+    actions: [hover('#test-3'), focus('#test-4')],
+    selectors: ['#demo-buttons'],
+  }),
+];
