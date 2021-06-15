c7bc96aeacdc74af79102996f22ea2cab408b592
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-06-15 13:11:49 -0400

    New tests.

--- backstop/scenarios/tabs.js
+++ backstop/scenarios/tabs.js
@@ -1,3 +1,17 @@
 const { scenario } = require('../scenarioHelper');
 
-module.exports = [scenario('basic')];
+module.exports = [
+  // Types
+  scenario('Type default', {
+    selectors: ['#demo-default'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+  scenario('Type borderless', {
+    selectors: ['#demo-borderless'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+  scenario('Type pill', {
+    selectors: ['#demo-pill'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+];
