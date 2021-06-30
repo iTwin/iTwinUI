c61728ebe0841414721c4ce09ec59028c3bf01d2
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-06-30 17:04:56 -0400

    Major changes.

--- backstop/scenarios/menu.js
+++ backstop/scenarios/menu.js
@@ -1,7 +1,26 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('basic', {
+  // Menu types
+  scenario('Type Default', {
+    selectors: ['#demo-default'],
+  }),
+  scenario('Type Header', {
+    selectors: ['#demo-header'],
+  }),
+  scenario('Type Select', {
+    selectors: ['#demo-select'],
+  }),
+  scenario('Type Default Dropdown Button', {
+    selectors: ['#demo-button'],
+  }),
+  scenario('Type Borderless Dropdown Button', {
+    selectors: ['#demo-borderless-button'],
+  }),
+
+  // Hover & focus states
+  scenario('State hover & focus', {
+    actions: [hover('#test-menu-1'), focus('#test-menu-2')],
     selectors: ['#demo-default'],
   }),
 ];
