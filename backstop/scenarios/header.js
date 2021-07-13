31081eaf976c17998bd2be7ddf8019aac570be45
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-07-13 16:22:43 -0400

    Adding tests and approving with known issue.

--- backstop/scenarios/header.js
+++ backstop/scenarios/header.js
@@ -4,9 +4,17 @@ module.exports = [
   // Header types
   scenario('Type Default', {
     selectors: ['#demo-default'],
+    hideSelectors: [
+      '.iui-notification-primary::before',
+      '.iui-notification-positive::before',
+    ],
   }),
   scenario('Type Slim', {
     selectors: ['#demo-slim'],
+    hideSelectors: [
+      '.iui-notification-warning::before',
+      '.iui-notification-negative::before',
+    ],
   }),
 
   // Hover & focus states
@@ -14,11 +22,19 @@ module.exports = [
   scenario('Type Default State hover & focus', {
     actions: [hover('#test-button-1'), focus('#test-button-2')],
     selectors: ['#demo-default'],
+    hideSelectors: [
+      '.iui-notification-primary::before',
+      '.iui-notification-positive::before',
+    ],
   }),
 
   //// Slim
   scenario('Type Default State hover & focus', {
     actions: [hover('#test-button-3'), focus('#test-button-4')],
     selectors: ['#demo-slim'],
+    hideSelectors: [
+      '.iui-notification-warning::before',
+      '.iui-notification-negative::before',
+    ],
   }),
 ];
