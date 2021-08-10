af1be22c8ee46f0e0ab05d1cdc447f461ed2ae48
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-08-10 13:21:18 -0400

    Updating tests & removing old images.

--- backstop/scenarios/wizard.js
+++ backstop/scenarios/wizard.js
@@ -1,23 +1,33 @@
 const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('Default wizard', {
+  // Types
+  scenario('Type default', {
     selectors: ['#demo-default'],
+    viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('Long wizard', {
+  scenario('Type long', {
     selectors: ['#demo-long'],
+    viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('Workflow wizard', {
+  scenario('Type workflow', {
     selectors: ['#demo-workflow'],
+    viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('Default wizard hover on complete', {
+
+  // Hover states
+  scenario('State default hover on complete', {
     actions: [hover('#demo-default .iui-completed:first-child')],
     selectors: ['#demo-default'],
+    viewports: [{ width: 800, height: 600 }],
   }),
+
+  // Focus states
   scenario('Long wizard focus on complete', {
     actions: [
       focus('#demo-long .iui-completed:first-child .iui-wizard-circle'),
     ],
     selectors: ['#demo-long'],
+    viewports: [{ width: 800, height: 600 }],
   }),
 ];
