ff28631e36ae3b22f1bc331901aa0df2fe722417
Saally Campos <88331924+saaaaaally@users.noreply.github.com>
2021-08-26 11:12:02 -0500

    Added test scenarios

--- backstop/scenarios/color-picker.js
+++ backstop/scenarios/color-picker.js
@@ -2,10 +2,34 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('default', {
-    selectors: ['#demo-default'],
+  // Color picker types
+  scenario('Basic', {
+    selectors: ['#demo-basic'],
+  }),
+  scenario('Advanced', {
+    selectors: ['#demo-advanced'],
+  }),
+
+  // Hover states
+  scenario('State hover color swatch', {
+    actions: [hover('#test-color-swatch')],
+    selectors: ['#demo-basic'],
+  }),
+  scenario('State hover color dot', {
+    actions: [hover('#test-color-dot')],
+    selectors: ['#demo-advanced'],
+  }),
+
+  // Focus states
+  scenario('State focus color swatch', {
+    actions: [focus('#test-color-swatch')],
+    selectors: ['#demo-basic'],
+  }),
+  scenario('State focus color dot', {
+    actions: [focus('#test-color-dot')],
+    selectors: ['#demo-advanced'],
   }),
 ];
