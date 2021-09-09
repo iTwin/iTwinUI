25c7db6ef75a5c7260a9ce9c9f4190f6ca45d833
Vilius <36186912+bentleyvk@users.noreply.github.com>
2021-09-09 12:40:46 +0300

    fix(Tooltip): Fixed `Tooltip` content clipping when it is next to the viewport edge (iTwin/iTwinUI-react#314)

--- src/core/utils/Popover.tsx
+++ src/core/utils/Popover.tsx
@@ -69,6 +69,7 @@ export const Popover = React.forwardRef((props: PopoverProps, ref) => {
     },
     role: undefined,
     offset: [0, 0],
+    maxWidth: '',
     ...props,
     plugins: [
       lazyLoad,
