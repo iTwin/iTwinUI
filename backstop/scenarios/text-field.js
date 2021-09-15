a3405f2d6fa97baf8d4062c2500d5d3edba33cac
Mayank <9084735+mayank99@users.noreply.github.com>
2021-09-15 03:23:52 -0400

    feat(labeled-inputs): Add new layout options using grid (#287)

--- backstop/scenarios/text-field.js
+++ backstop/scenarios/text-field.js
@@ -7,6 +7,9 @@ module.exports = [
   scenario('Type Inline', {
     selectors: ['#demo-inline'],
   }),
+  scenario('Type Hybrid', {
+    selectors: ['#demo-hybrid'],
+  }),
   scenario('Type Without Label', {
     selectors: ['#demo-no-label'],
   }),
