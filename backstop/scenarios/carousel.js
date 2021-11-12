342d5422441ddbbd85f5f70610f62b1afc29a30d
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-11-12 13:06:00 -0500

    Carousel buttons work.

--- backstop/scenarios/carousel.js
+++ backstop/scenarios/carousel.js
@@ -7,14 +7,13 @@ const { scenario, hover, focus } = require('../scenarioHelper');
 module.exports = [
   scenario('Type Actionable', {
     selectors: ['#demo-default'],
-  }),
-  scenario('Type Non-actionable', {
-    selectors: ['#demo-non-actionable'],
+    viewports: [{ width: 800, height: 600 }],
   }),
 
   // Hover & focus states
   scenario('Type Actionable', {
     actions: [hover('#test-1'), focus('#test-2')],
     selectors: ['#demo-default'],
+    viewports: [{ width: 800, height: 600 }],
   }),
 ];
