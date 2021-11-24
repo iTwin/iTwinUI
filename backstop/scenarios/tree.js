5251bfc0be120f4877e87773d739fd806aef098d
Saally Campos <88331924+saaaaaally@users.noreply.github.com>
2021-11-24 15:45:29 -0600

    Update test images

--- backstop/scenarios/tree.js
+++ backstop/scenarios/tree.js
@@ -7,17 +7,22 @@ const { scenario } = require('../scenarioHelper');
 module.exports = [
   scenario('Default', {
     selectors: ['#demo-default'],
+    viewports: [{ width: 800, height: 600 }],
   }),
   scenario('With Captions', {
     selectors: ['#demo-captions'],
+    viewports: [{ width: 800, height: 600 }],
   }),
   scenario('With Checkbox', {
     selectors: ['#demo-checkbox'],
+    viewports: [{ width: 800, height: 600 }],
   }),
   scenario('Disabled', {
     selectors: ['#demo-disabled'],
+    viewports: [{ width: 800, height: 600 }],
   }),
   scenario('With Icons', {
     selectors: ['#demo-icons'],
+    viewports: [{ width: 800, height: 600 }],
   }),
 ];
