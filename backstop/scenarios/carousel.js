4b5eaa3db256a5ccc3ec25f18a4ac7dbf970d13f
FlyersPh9 <849817+FlyersPh9@users.noreply.github.com>
2021-12-22 16:26:47 -0500

    Ready for review.

--- backstop/scenarios/carousel.js
+++ backstop/scenarios/carousel.js
@@ -5,13 +5,23 @@
 const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
-  scenario('Type Actionable', {
+  scenario('Type default', {
     selectors: ['#demo-default'],
     viewports: [{ width: 800, height: 600 }],
   }),
 
+  scenario('Type many slides', {
+    selectors: ['#demo-many-slides'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+
+  scenario('Type counter', {
+    selectors: ['#demo-counter'],
+    viewports: [{ width: 800, height: 600 }],
+  }),
+
   // Hover & focus states
-  scenario('Type Actionable', {
+  scenario('State hover & focus', {
     actions: [hover('#test-1'), focus('#test-2')],
     selectors: ['#demo-default'],
     viewports: [{ width: 800, height: 600 }],
