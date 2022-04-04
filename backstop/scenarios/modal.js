d3b5491e7acd24445151fd80cffd17755de0f2da
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2022-04-04 13:04:50 -0400

    Initial push.

--- backstop/scenarios/modal.js
+++ backstop/scenarios/modal.js
@@ -6,13 +6,22 @@ module.exports = [
     // when "Open modal" sometimes is and sometimes is not hovered.
     actions: [click('#open-modal'), hover('body')],
     selectors: ['document'],
-    hideSelectors: ['body > *:not(.iui-modal)'],
+    hideSelectors: ['body > *:not(.iui-modal-backdrop)'],
   }),
+
   scenario('full-page-modal', {
     // Hovering `body` to avoid random test failures
     // when "Open modal" sometimes is and sometimes is not hovered.
     actions: [click('#open-full-page-modal'), hover('body')],
     selectors: ['document'],
-    hideSelectors: ['body > *:not(.iui-modal)'],
+    hideSelectors: ['body > *:not(.iui-modal-backdrop)'],
+  }),
+
+  scenario('resizable-modal', {
+    // Hovering `body` to avoid random test failures
+    // when "Open modal" sometimes is and sometimes is not hovered.
+    actions: [click('#open-resizable-modal'), hover('body')],
+    selectors: ['document'],
+    hideSelectors: ['body > *:not(.iui-modal-backdrop)'],
   }),
 ];
