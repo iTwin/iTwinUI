f24f2ae981b2ce98f2711bb24ec16b341fcf8a04
adamhock <78564221+adamhock@users.noreply.github.com>
2022-04-26 08:48:05 -0400

    Added new tests and images for hover and focus

--- backstop/scenarios/radio-tile.js
+++ backstop/scenarios/radio-tile.js
@@ -2,10 +2,20 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
   scenario('default', {
     selectors: ['#demo-default'],
+  }),
+  // Hover
+  scenario('State hover', {
+    actions: [hover('#test-radio-tile')],
+    selectors: ['#test-radio-tile'],
+  }),
+  // Focus
+  scenario('State focus', {
+    actions: [focus('input[id="radio-tile-demo-1.3"]')],
+    selectors: ['#test-radio-tile'],
   }),
 ];
