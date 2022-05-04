6f1972a8e294b7e10e7e3c15c403969b6c135a1e
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2022-05-04 08:50:58 -0500

    Fix tests failing

--- backstop/scenarios/dialog.js
+++ backstop/scenarios/dialog.js
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-const { scenario } = require('../scenarioHelper');
+const { scenario, click, hover } = require('../scenarioHelper');
 
 module.exports = [
   scenario('basic', {
