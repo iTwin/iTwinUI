d24862131bd20b1c714ca8989ee01fb50900d349
veekeys <81580355+veekeys@users.noreply.github.com>
2022-05-12 12:43:26 +0300

    Added multi select tag component

---
+++ backstop/assets/multiSelectTag.js
@@ -0,0 +1,41 @@
+/**
+ * Web component for multi select tag. Use in other components examples to simplify html.
+ * <multi-select-tag value="Option 1"></multi-select-tag>
+ */
+class MultiSelectTag extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const value = this.getAttribute('value');
+
+    const innerHtml = `
+    <span
+      class="iui-multi-select-tag"
+      tabindex="0"
+    >
+      <span
+        class="iui-multi-select-tag-label"
+        title="${value}"
+      >
+        ${value}
+      </span>
+      <button
+        class="iui-multi-select-tag-button"
+        aria-label="Delete tag"
+        type="button"
+        tabindex="-1"
+      >
+        <svg-close-small
+          class="iui-multi-select-tag-button-icon"
+          aria-hidden="true"
+        ></svg-close-small>
+      </button>
+    </span>
+    `;
+    this.parentElement.insertAdjacentHTML('beforeend', innerHtml);
+    this.remove();
+  }
+}
+customElements.define('multi-select-tag', MultiSelectTag);
