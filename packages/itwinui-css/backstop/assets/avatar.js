295a08c206c0094ca3bd924274aca4b92caa8000
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2022-05-23 08:22:45 -0500

    chore: Rename Components (#642)
    Renamed ToastNotification to Toast, Wizard to Stepper, and UserIcon to Avatar.

--- packages/itwinui-css/backstop/assets/avatar.js
+++ packages/itwinui-css/backstop/assets/avatar.js
@@ -1,7 +1,7 @@
 /**
- * Web component for user icon. Use in other components examples to simplify html. Choose between 2 predefined types/people.
- * <user-icon size="small" type="2"></user-icon>
- * <user-icon size="x-large" type="1" status="online" showPlaceholder="true"></user-icon>
+ * Web component for avatar. Use in other components examples to simplify html. Choose between 2 predefined types/people.
+ * <x-avatar size="small" type="2"></x-avatar>
+ * <x-avatar size="x-large" type="1" status="online" showPlaceholder="true"></x-avatar>
  */
 const persons = {
   1: {
@@ -16,7 +16,7 @@ const persons = {
   },
 };
 
-class UserIcon extends HTMLElement {
+class Avatar extends HTMLElement {
   constructor() {
     super();
   }
@@ -29,7 +29,7 @@ class UserIcon extends HTMLElement {
 
     const innerHtml = `
     <span
-      class="iui-user-icon iui-${size}"
+      class="iui-avatar iui-${size}"
       title="${person.title}"
     >
       <abbr
@@ -61,4 +61,4 @@ class UserIcon extends HTMLElement {
     this.remove();
   }
 }
-customElements.define('user-icon', UserIcon);
+customElements.define('x-avatar', Avatar); //custom html elements must have a "-" to avoid potential conflicts with native html elements
