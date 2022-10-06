e3dd2db582faa20e17acd950810418554bde2043
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2022-10-06 11:55:32 -0400

    Test images.

--- packages/itwinui-css/backstop/scenarios/expandable-block.js
+++ packages/itwinui-css/backstop/scenarios/expandable-block.js
@@ -3,33 +3,25 @@ const { scenario, hover, focus } = require('../scenarioHelper');
 module.exports = [
   // Types
   scenario('Type default', {
-    selectors: ['#demo-default'],
-    viewports: [{ width: 800, height: 600 }],
-  }),
-  scenario('Type with caption', {
-    selectors: ['#demo-captions'],
-    viewports: [{ width: 800, height: 600 }],
-  }),
-  scenario('Type small', {
-    selectors: ['#demo-small'],
+    selectors: ['#demo-group-default'],
     viewports: [{ width: 800, height: 600 }],
   }),
   scenario('Type borderless', {
-    selectors: ['#demo-borderless'],
+    selectors: ['#demo-group-borderless'],
     viewports: [{ width: 800, height: 600 }],
   }),
 
-  // Hover states
-  scenario('State hover', {
-    actions: [hover('#demo-captions .iui-expanded .iui-header')],
-    selectors: ['#demo-captions'],
+  // Hover & focus states
+  scenario('Type default State hover & focus', {
+    actions: [hover('#test-01 .iui-expandable-block-header'), focus('#test-02 .iui-expandable-block-header')],
+    selectors: ['#demo-group-default'],
     viewports: [{ width: 800, height: 600 }],
   }),
 
   // Focus states
-  scenario('State focus', {
-    actions: [focus('#demo-captions .iui-expanded .iui-header')],
-    selectors: ['#demo-captions'],
+  scenario('Type borderless State hover & focus', {
+    actions: [hover('#test-03 .iui-expandable-block-header'), focus('#test-04 .iui-expandable-block-header')],
+    selectors: ['#demo-group-borderless'],
     viewports: [{ width: 800, height: 600 }],
   }),
 ];
