7e7d14316a4dde1232d5a3e2d651aec49e5379d1
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2022-11-08 14:06:47 -0600

    Adds visual testing for high contrast light and dark. Adjusts naming convention for themes.

--- packages/itwinui-css/backstop/scenarios/index.js
+++ packages/itwinui-css/backstop/scenarios/index.js
@@ -15,10 +15,41 @@ fs.readdirSync('backstop/scenarios/').forEach((file) => {
   scenarios = scenarios.concat(newScenarios);
 });
 
+const lightScenarios = scenarios.map((s) => {
+  const lightScenario = Object.assign({}, s);
+  lightScenario.label = `${lightScenario.label}_light`;
+  return lightScenario;
+});
+
+const highContrastLightScenarios = scenarios.map((s) => {
+  const hcLightScenario = Object.assign({}, s);
+  hcLightScenario.label = `${hcLightScenario.label}_hc_light`;
+  return hcLightScenario;
+});
+
 const darkScenarios = scenarios.map((s) => {
   const darkScenario = Object.assign({}, s);
-  darkScenario.label = `dark_${darkScenario.label}`;
+  // labelStrings = darkScenario.label.split('_');
+  // labelStrings.splice(1, 0, 'dark');
+  // darkScenario.label = labelStrings.join('_');
+  darkScenario.label = `${darkScenario.label}_dark`;
   return darkScenario;
 });
 
-module.exports = scenarios.concat(darkScenarios);
+const highContrastDarkScenarios = scenarios.map((s) => {
+  const hcDarkScenario = Object.assign({}, s);
+  hcDarkScenario.label = `${hcDarkScenario.label}_hc_dark`;
+  return hcDarkScenario;
+});
+
+// module.exports = scenarios
+//   .concat(highContrastLightScenarios, darkScenarios, highContrastDarkScenarios)
+//   .sort(function (a, b) {
+//     const labelA = a.label.toUpperCase();
+//     const labelB = b.label.toUpperCase();
+//     return labelA < labelB ? -1 : labelA > labelB ? 1 : 0;
+//   });
+
+module.exports = lightScenarios.concat(highContrastLightScenarios, darkScenarios, highContrastDarkScenarios);
+
+//!! instead of sorting alphabetically with theme after component name, put theme in front of component name but have a custom sort to put all the same test together?
