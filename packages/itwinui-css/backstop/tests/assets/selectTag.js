bb2be3807344855b56231815a20a7c04bc7e9a64
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2022-12-07 12:26:24 -0500

    Updating select tag web component.

--- packages/itwinui-css/backstop/tests/assets/selectTag.js
+++ packages/itwinui-css/backstop/tests/assets/selectTag.js
@@ -10,11 +10,12 @@ class SelectTag extends HTMLElement {
   connectedCallback() {
     const value = this.getAttribute('value');
     const dismissible = this.hasAttribute('dismissible');
+    const nonfocusable = this.hasAttribute('nonfocusable');
 
     const innerHtml = `
     <span
       class="iui-select-tag"
-      tabindex="0"
+      ${nonfocusable ? `` : 'tabindex="0"'}
     >
       <span
         class="iui-select-tag-label"
@@ -24,9 +25,9 @@ class SelectTag extends HTMLElement {
       </span>
       ${
         dismissible
-          ? `<button class="iui-select-tag-button" aria-label="Delete tag" type="button" tabindex="-1">
+          ? `<span class="iui-select-tag-button" aria-label="Delete tag" role="button">
             <svg-close-small class="iui-select-tag-button-icon" aria-hidden="true"></svg-close-small>
-          </button>`
+          </span>`
           : ''
       }
     </span>
