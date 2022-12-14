c9c1dc298b71bdc4c431a029e09bc1188bd3c8bc
adamhock <78564221+adamhock@users.noreply.github.com>
2022-12-14 11:22:59 -0500

    Added new test images

--- packages/itwinui-css/backstop/scenarios/input.js
+++ packages/itwinui-css/backstop/scenarios/input.js
@@ -1,4 +1,4 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
   scenario('Type Default', {
@@ -12,5 +12,21 @@ module.exports = [
   }),
   scenario('Type Without Label', {
     selectors: ['#demo-no-label'],
+  }),
+  scenario('State hover in default', {
+    actions: [hover('#test-input')],
+    selectors: ['#demo-input'],
+  }),
+  scenario('State hover with status', {
+    actions: [hover('#test-input-with-status')],
+    selectors: ['#demo-input-with-status'],
+  }),
+  scenario('State focus in default', {
+    actions: [focus('#test-input')],
+    selectors: ['#demo-input'],
+  }),
+  scenario('State focus with status', {
+    actions: [focus('#test-input-with-status')],
+    selectors: ['#demo-input-with-status'],
   }),
 ];
--- packages/itwinui-css/backstop/scenarios/textarea.js
+++ packages/itwinui-css/backstop/scenarios/textarea.js
@@ -1,4 +1,4 @@
-const { scenario } = require('../scenarioHelper');
+const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
   scenario('default', {
@@ -6,5 +6,21 @@ module.exports = [
   }),
   scenario('inline', {
     selectors: ['#demo-inline'],
+  }),
+  scenario('State hover in default', {
+    actions: [hover('#test-textarea')],
+    selectors: ['#demo-textarea'],
+  }),
+  scenario('State hover with status', {
+    actions: [hover('#test-textarea-with-status')],
+    selectors: ['#demo-textarea-with-status'],
+  }),
+  scenario('State focus in default', {
+    actions: [focus('#test-textarea')],
+    selectors: ['#demo-textarea'],
+  }),
+  scenario('State focus with status', {
+    actions: [focus('#test-textarea-with-status')],
+    selectors: ['#demo-textarea-with-status'],
   }),
 ];
