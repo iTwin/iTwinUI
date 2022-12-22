a09a271f8e50f2e3fe8b7e9ed0da1a0558c4d4d7
Mayank <mayank99@users.noreply.github.com>
2022-12-22 12:59:17 -0500

    chore: Fix jest trying to run transformed test files

--- packages/itwinui-react/jest.config.js
+++ packages/itwinui-react/jest.config.js
@@ -209,11 +209,11 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
 
-  // testPathIgnorePatterns: [
-
-  //   "\\\\node_modules\\\\"
-
-  // ],
+  testPathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+    '\\\\esm\\\\',
+    '\\\\cjs\\\\',
+  ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
 
