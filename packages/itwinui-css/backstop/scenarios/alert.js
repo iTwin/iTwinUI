96cb07f218abf540f3e24bbd5a3ce650d34efc1e
Mayank <mayank99@users.noreply.github.com>
2023-01-03 14:21:13 -0500

    Revert unintentional changes

--- packages/itwinui-css/backstop/scenarios/alert.js
+++ packages/itwinui-css/backstop/scenarios/alert.js
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
 const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
@@ -18,18 +14,12 @@ module.exports = [
   // Hover & focus states
   scenario('State hover link & focus close button', {
     selectors: ['#test-single-alert'],
-    actions: [
-      hover('#test-single-alert a'),
-      focus('#test-single-alert button'),
-    ],
+    actions: [hover('#test-single-alert a'), focus('#test-single-alert button')],
     viewports: [{ width: 400, height: 600 }],
   }),
   scenario('State hover close button & focus link', {
     selectors: ['#test-single-alert'],
-    actions: [
-      hover('#test-single-alert button'),
-      focus('#test-single-alert a'),
-    ],
+    actions: [hover('#test-single-alert button'), focus('#test-single-alert a')],
     viewports: [{ width: 400, height: 600 }],
   }),
 ];
