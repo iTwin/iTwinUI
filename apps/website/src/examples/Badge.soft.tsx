ea2d0584e3f674695f23eb7b38311887ce0b323b
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2023-01-05 10:12:05 -0500

    Initial push.

---
+++ apps/website/src/examples/Badge.soft.tsx
@@ -0,0 +1,32 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { Badge } from '@itwin/itwinui-react';
+
+export default () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        columnGap: 4,
+        placeItems: 'center',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+      }}
+    >
+      <Badge backgroundColor='skyblue'>Skyblue</Badge>
+      <Badge backgroundColor='celery'>Celery</Badge>
+      <Badge backgroundColor='froly'>Froly</Badge>
+      <Badge backgroundColor='steelblue'>Steelblue</Badge>
+      <Badge backgroundColor='sunglow'>Sunglow</Badge>
+      <Badge backgroundColor='seabuckthorn'>Seabuckthorn</Badge>
+      <Badge backgroundColor='montecarlo'>Montecarlo</Badge>
+      <Badge backgroundColor='poloblue'>Poloblue</Badge>
+      <Badge backgroundColor='bouquet'>Bouquet</Badge>
+      <Badge backgroundColor='ash'>Ash</Badge>
+      <Badge backgroundColor='oak'>Oak</Badge>
+    </div>
+  );
+};
