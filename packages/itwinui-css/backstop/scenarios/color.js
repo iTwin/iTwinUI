ab0e1bc2e866e3702e0c51d1bcaeb4b4469a97f7
Mayank <9084735+mayank99@users.noreply.github.com>
2023-01-05 10:16:33 -0500

    chore: Fix and combine copyright linters (#952)
    - moved a combined version of copyrightLinter to scripts, deleted old versions from `shared` and `configs`
    - ran yarn lint:copyright which added missing copyright headers to some .js files

--- packages/itwinui-css/backstop/scenarios/color.js
+++ packages/itwinui-css/backstop/scenarios/color.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 const { scenario } = require('../scenarioHelper');
 
 module.exports = [scenario('Type all', { selectors: ['#demo-all'], viewports: [{ width: 300, height: 600 }] })];
