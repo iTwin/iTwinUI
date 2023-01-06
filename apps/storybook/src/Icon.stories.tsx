c51b7c70d16871731191f38b3511c7166c1af73c
Mayank <mayank99@users.noreply.github.com>
2023-01-06 12:44:46 -0500

    add stories

---
+++ apps/storybook/src/Icon.stories.tsx
@@ -0,0 +1,123 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+import { Icon, IconProps, Slider } from '@itwin/itwinui-react';
+import {
+  SvgInfoCircular,
+  SvgPlaceholder,
+  SvgStatusError,
+  SvgStatusSuccess,
+  SvgStatusWarning,
+} from '@itwin/itwinui-icons-react';
+
+export default {
+  component: Icon,
+  title: 'Utilities/Icon',
+  argTypes: {
+    size: {
+      control: 'radio',
+      options: ['auto', 'small', 'medium', 'large'],
+    },
+    fill: {
+      control: 'radio',
+      options: ['default', 'informational', 'positive', 'warning', 'negative'],
+    },
+  },
+} as Meta<IconProps>;
+
+export const Default: Story<IconProps> = (args) => {
+  return (
+    <Icon {...args}>
+      <SvgPlaceholder />
+    </Icon>
+  );
+};
+Default.args = {
+  size: 'medium',
+  fill: 'default',
+};
+
+export const Statuses: Story<IconProps> = (args) => {
+  return (
+    <>
+      <Icon fill='informational' {...args}>
+        <SvgInfoCircular />
+      </Icon>
+      <Icon fill='negative' {...args}>
+        <SvgStatusError />
+      </Icon>
+      <Icon fill='positive' {...args}>
+        <SvgStatusSuccess />
+      </Icon>
+      <Icon fill='warning' {...args}>
+        <SvgStatusWarning />
+      </Icon>
+    </>
+  );
+};
+Statuses.args = {};
+Statuses.decorators = [
+  (Story) => (
+    <div style={{ display: 'flex', gap: 4 }}>
+      <Story />
+    </div>
+  ),
+];
+
+export const Autoscale: Story<IconProps> = (args) => {
+  return (
+    <Icon {...args}>
+      <SvgPlaceholder />
+    </Icon>
+  );
+};
+Autoscale.args = {
+  size: 'auto',
+  fill: 'default',
+};
+Autoscale.decorators = [
+  (Story) => (
+    <AutoScaleContraption>
+      <Story />
+    </AutoScaleContraption>
+  ),
+];
+
+/** Helper component to demonstrate auto-scaling text using a slider to adjust font size */
+const AutoScaleContraption = ({ children }: { children: React.ReactNode }) => {
+  const [size, setSize] = React.useState(() => 14 / 16);
+
+  return (
+    <div style={{ display: 'flex', gap: 16, alignItems: 'start' }}>
+      <p
+        style={{
+          width: 'min(400px, 90%)',
+          fontSize: `${size}rem`,
+          lineHeight: 1.3,
+        }}
+      >
+        <span style={{ marginRight: 4, verticalAlign: 'middle' }}>
+          {children}
+        </span>
+        This icon will scale with text. Try adjusting the slider.
+      </p>
+
+      <Slider
+        orientation='vertical'
+        values={[size]}
+        onUpdate={([newSize]) => setSize(newSize)}
+        min={0.5}
+        max={2.5}
+        step={0.05}
+        minLabel=''
+        maxLabel=''
+        tooltipProps={() => ({ visible: false })}
+        style={{ height: 'min(150px, 90vh)' }}
+        thumbProps={() => ({ 'aria-label': 'Adjust font size' })}
+      />
+    </div>
+  );
+};
