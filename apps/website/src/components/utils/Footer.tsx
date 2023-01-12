462f11d9e85280eac7b67fd13106ae7b4c0c4fd0
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-01-12 18:03:54 +0200

    Moved to Astro file

--- apps/website/src/components/utils/Footer.tsx
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
-
-export default (props: React.ComponentProps<'footer'>) => {
-  const currentYear = new Date().getFullYear();
-  return <footer {...props}>© {currentYear} Bentley Systems, Inc</footer>;
-};
