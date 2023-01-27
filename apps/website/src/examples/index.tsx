b0a41f45bc7af287d238758729c555c70e350cec
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2023-01-27 15:15:34 -0500

    Remove from index

--- apps/website/src/examples/index.tsx
+++ apps/website/src/examples/index.tsx
@@ -23,7 +23,6 @@ export { default as CodeMainExample } from './Code.main';
 export { default as DatePickerMainExample } from './DatePicker.main';
 export { default as DialogStaticExample } from './Dialog.static';
 export { default as DialogMainExample } from './Dialog.main';
-export { default as DialogModalExample } from './Dialog.modal';
 export { default as DropdownMenuMainExample } from './DropdownMenu.main';
 export { default as NonIdealStateMainExample } from './NonIdealState.main';
 export { default as ExpandableBlockMainExample } from './ExpandableBlock.main';
