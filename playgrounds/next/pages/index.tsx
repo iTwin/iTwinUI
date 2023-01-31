018fd3aee7dbbb23fc8d90386347d68acffe292b
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-01-31 14:41:03 -0500

    Playground to test all cases

--- playgrounds/next/pages/index.tsx
+++ playgrounds/next/pages/index.tsx
@@ -4,42 +4,29 @@ import { SvgAdd } from '@itwin/itwinui-icons-react';
 export default function Home() {
   return (
     <>
+      {/* child = .iui-svg-icon */}
       <Avatar
         size='large'
         abbreviation='TR'
         backgroundColor={'orange'}
         child={
-          // <img src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png' />
-          // <SvgAdd className='iui-icon' />
           <Icon>
             <SvgAdd />
           </Icon>
         }
-        // image={
-        //   // <img src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png' />
-        //   // <Icon>
-        //   <SvgAdd />
-        //   // </Icon>
-        // }
         title='Terry Rivers'
       />
+
+      {/* child = svg */}
       <Avatar
         size='large'
         abbreviation='TR'
         backgroundColor={'orange'}
-        child={
-          // <img src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png' />
-          // <SvgAdd className='iui-icon' />
-          <SvgAdd />
-        }
-        // image={
-        //   // <img src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png' />
-        //   // <Icon>
-        //   <SvgAdd />
-        //   // </Icon>
-        // }
+        child={<SvgAdd />}
         title='Terry Rivers'
       />
+
+      {/* child = .iui-icon */}
       <Avatar
         size='large'
         abbreviation='TR'
@@ -47,6 +34,19 @@ export default function Home() {
         child={<SvgAdd className='iui-icon' />}
         title='Terry Rivers'
       />
+
+      {/* No breaking change when passing in image*/}
+      <Avatar
+        size='large'
+        abbreviation='TR'
+        backgroundColor={'orange'}
+        image={
+          <img src='https://itwinplatformcdn.azureedge.net/iTwinUI/user-placeholder.png' />
+        }
+        title='Terry Rivers'
+      />
+
+      {/* Passing old `<img>` tag in child instead of image prop */}
       <Avatar
         size='large'
         abbreviation='TR'
