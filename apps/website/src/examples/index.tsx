a00e10d16e69107e7715e8bfed0c78c7210f58a6
Mayank <mayank99@users.noreply.github.com>
2023-02-27 14:06:32 -0500

    nonidealstate ✅

--- apps/website/src/examples/index.tsx
+++ apps/website/src/examples/index.tsx
@@ -63,6 +63,8 @@ export { default as HeaderMainExample } from './Header.main';
 
 export { default as InformationPanelMainExample } from './InformationPanel.main';
 
+export { default as NonIdealStateMainExample } from './NonIdealState.main';
+
 export { default as BlockquoteMainExample } from './Blockquote.main';
 export { default as ButtonMainExample } from './Button.main';
 export { default as ButtonDefaultExample } from './Button.default';
@@ -73,7 +75,6 @@ export { default as IdeasButtonMainExample } from './IdeasButton.main';
 export { default as CheckboxMainExample } from './Checkbox.main';
 export { default as CarouselMainExample } from './Carousel.main';
 export { default as CodeMainExample } from './Code.main';
-export { default as NonIdealStateMainExample } from './NonIdealState.main';
 export { default as KeyboardMainExample } from './Keyboard.main';
 export { default as InputMainExample } from './Input.main';
 export { default as LabelMainExample } from './Label.main';
