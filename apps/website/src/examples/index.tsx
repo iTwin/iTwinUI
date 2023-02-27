4f362eda8bcbbdb16b8cf32d9cb4a2e6825b8528
Mayank <mayank99@users.noreply.github.com>
2023-02-27 14:03:36 -0500

    informationpanel ✅

--- apps/website/src/examples/index.tsx
+++ apps/website/src/examples/index.tsx
@@ -61,6 +61,8 @@ export { default as FooterMainExample } from './Footer.main';
 
 export { default as HeaderMainExample } from './Header.main';
 
+export { default as InformationPanelMainExample } from './InformationPanel.main';
+
 export { default as BlockquoteMainExample } from './Blockquote.main';
 export { default as ButtonMainExample } from './Button.main';
 export { default as ButtonDefaultExample } from './Button.default';
@@ -72,7 +74,6 @@ export { default as CheckboxMainExample } from './Checkbox.main';
 export { default as CarouselMainExample } from './Carousel.main';
 export { default as CodeMainExample } from './Code.main';
 export { default as NonIdealStateMainExample } from './NonIdealState.main';
-export { default as InformationPanelMainExample } from './InformationPanel.main';
 export { default as KeyboardMainExample } from './Keyboard.main';
 export { default as InputMainExample } from './Input.main';
 export { default as LabelMainExample } from './Label.main';
