32c424adaf1f03f275327257ac194d9506e22624
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-03-03 15:13:43 +0200

    added tab index to skip the anchor link

--- packages/itwinui-react/src/core/utils/components/LinkBox.tsx
+++ packages/itwinui-react/src/core/utils/components/LinkBox.tsx
@@ -21,7 +21,12 @@ export type PolymorphicActionProps = PolymorphicComponentProps<
 export const LinkBox = React.forwardRef((props, ref) => {
   const { as: Element = 'a', className, ...rest } = props;
   return (
-    <Element ref={ref} className={cx('iui-link-box', className)} {...rest} />
+    <Element
+      ref={ref}
+      className={cx('iui-link-box', className)}
+      tabIndex={-1}
+      {...rest}
+    />
   );
 }) as PolymorphicForwardRefComponent<'a', { children?: React.ReactNode }>;
 
