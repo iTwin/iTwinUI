39b8f25296df678a256b5fbcd6037f877721990b
Mayank <mayank99@users.noreply.github.com>
2023-03-06 16:13:03 -0500

    remove margins, add `iui-file-upload-template`, make heights match

--- packages/itwinui-react/src/core/FileUpload/FileUploadTemplate.tsx
+++ packages/itwinui-react/src/core/FileUpload/FileUploadTemplate.tsx
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import React from 'react';
+import cx from 'classnames';
 import { useTheme, SvgUpload } from '../utils';
 import '@itwin/itwinui-css/css/file-upload.css';
 
@@ -36,7 +37,7 @@ export type FileUploadTemplateProps = {
    * Optional children appended to the template.
    */
   children?: React.ReactNode;
-};
+} & React.ComponentProps<'div'>;
 
 /**
  * Default template to be used with the `FileUpload` wrapper component.
@@ -52,11 +53,13 @@ export const FileUploadTemplate = (props: FileUploadTemplateProps) => {
     label = 'Choose a file',
     subtitle = 'or drag & drop it here.',
     children,
+    className,
+    ...rest
   } = props;
   useTheme();
 
   return (
-    <>
+    <div className={cx('iui-file-upload-template', className)} {...rest}>
       <SvgUpload className='iui-icon' aria-hidden />
       <div className='iui-template-text'>
         <label className='iui-anchor'>
@@ -72,7 +75,7 @@ export const FileUploadTemplate = (props: FileUploadTemplateProps) => {
         <div>{subtitle}</div>
         {children}
       </div>
-    </>
+    </div>
   );
 };
 
