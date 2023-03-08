b67b3118bc45cc0a81ecd2023ca26b139a40fad9
Šarūnas <sgriskus2002@gmail.com>
2023-03-09 00:29:49 +0200

    clean-up after playground testing

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,48 +1,9 @@
-import { Carousel } from '@itwin/itwinui-react';
-import React from 'react';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
-  const [index, setIndex] = React.useState(0);
-
-  const handleOnKeyDown = React.useCallback((e: KeyboardEvent) => {
-    if (e.key === 'ArrowLeft') {
-      setIndex((prev) => prev - 1);
-    } else if (e.key === 'ArrowRight') {
-      setIndex((prev) => prev + 1);
-    }
-  }, []);
-
-  React.useEffect(() => {
-    window.addEventListener('keydown', handleOnKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleOnKeyDown);
-    };
-  }, [handleOnKeyDown]);
-
   return (
     <>
-      <Carousel activeSlideIndex={index}>
-        <Carousel.Slider>
-          {[...Array(50)].map((_, index) => (
-            <Carousel.Slide key={index}>
-              {
-                <div
-                  style={{
-                    height: 250,
-                    display: 'grid',
-                    placeItems: 'center',
-                    border: 'solid 1px',
-                  }}
-                >
-                  {index}
-                </div>
-              }
-            </Carousel.Slide>
-          ))}
-        </Carousel.Slider>
-        <Carousel.Navigation />
-      </Carousel>
+      <Button>Hello world</Button>
     </>
   );
 };
