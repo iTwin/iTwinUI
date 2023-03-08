d67a508b604d9b043f2d735f92df4e15e293e46e
Šarūnas <sgriskus2002@gmail.com>
2023-03-09 00:15:33 +0200

    changes for issue to be reproduced

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,9 +1,48 @@
-import { Button } from '@itwin/itwinui-react';
+import { Carousel } from '@itwin/itwinui-react';
+import React from 'react';
 
 const App = () => {
+  const [index, setIndex] = React.useState(0);
+
+  const handleOnKeyDown = React.useCallback((e: KeyboardEvent) => {
+    if (e.key === 'ArrowLeft') {
+      setIndex((prev) => prev - 1);
+    } else if (e.key === 'ArrowRight') {
+      setIndex((prev) => prev + 1);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleOnKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleOnKeyDown);
+    };
+  }, [handleOnKeyDown]);
+
   return (
     <>
-      <Button>Hello world</Button>
+      <Carousel activeSlideIndex={index}>
+        <Carousel.Slider>
+          {[...Array(50)].map((_, index) => (
+            <Carousel.Slide key={index}>
+              {
+                <div
+                  style={{
+                    height: 250,
+                    display: 'grid',
+                    placeItems: 'center',
+                    border: 'solid 1px',
+                  }}
+                >
+                  {index}
+                </div>
+              }
+            </Carousel.Slide>
+          ))}
+        </Carousel.Slider>
+        <Carousel.Navigation />
+      </Carousel>
     </>
   );
 };
