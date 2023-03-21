ea3413f3eb2035bd59731d7cf64febe2afd40bda
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2023-03-21 10:57:08 -0500

    I dunno maybe fix the test

--- apps/storybook/src/Tabs.test.ts
+++ apps/storybook/src/Tabs.test.ts
@@ -20,6 +20,7 @@ describe('Tabs', () => {
       cy.wait(1000); // wait for resize observer to be done
 
       if (testName === 'Overflow') {
+        cy.wait(5000);
         cy.get('.iui-button').click();
       }
 
