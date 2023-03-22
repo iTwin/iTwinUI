87c383a0bbf06d43ad0cf1207cb367f53f5baf3e
Mayank <9084735+mayank99@users.noreply.github.com>
2023-03-22 14:31:14 -0400

    add subcomponents

--- packages/itwinui-react/src/core/List/ListItem.tsx
+++ packages/itwinui-react/src/core/List/ListItem.tsx
@@ -11,14 +11,24 @@ import type {
 } from '../utils';
 import '@itwin/itwinui-css/css/menu.css';
 
-export const ListItem = React.forwardRef((props, ref) => {
-  const { as: Element = 'div', className, ...rest } = props;
+const ListItemComponent = React.forwardRef((props, ref) => {
+  const {
+    as: Element = 'div',
+    size = 'default',
+    disabled = false,
+    active = false,
+    className,
+    ...rest
+  } = props;
 
   useTheme();
 
   return (
     <Element
       className={cx('iui-list-item', className)}
+      data-iui-active={active ? 'true' : undefined}
+      data-iui-disabled={disabled ? 'true' : undefined}
+      data-iui-size={size === 'large' ? 'large' : undefined}
       ref={ref}
       role='listitem'
       {...rest}
@@ -26,6 +36,67 @@ export const ListItem = React.forwardRef((props, ref) => {
   );
 }) as PolymorphicForwardRefComponent<'div', ListItemOwnProps>;
 
-type ListItemOwnProps = {}; // eslint-disable-line @typescript-eslint/ban-types
+type ListItemOwnProps = {
+  size?: 'default' | 'large';
+  disabled?: boolean;
+  active?: boolean;
+};
+
+// ----------------------------------------------------------------------------
+
+const ListItemIcon = React.forwardRef((props, ref) => {
+  const { as: Element = 'div', className, ...rest } = props;
+
+  return (
+    <Element
+      className={cx('iui-list-item-icon', className)}
+      ref={ref}
+      {...rest}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'div', ListItemIconOwnProps>;
+
+type ListItemIconOwnProps = {}; // eslint-disable-line @typescript-eslint/ban-types
+
+// ----------------------------------------------------------------------------
+
+const ListItemContent = React.forwardRef((props, ref) => {
+  const { as: Element = 'div', className, ...rest } = props;
+
+  return (
+    <Element
+      className={cx('iui-list-item-content', className)}
+      ref={ref}
+      {...rest}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'div', ListItemContentOwnProps>;
+
+type ListItemContentOwnProps = {}; // eslint-disable-line @typescript-eslint/ban-types
+
+// ----------------------------------------------------------------------------
+
+const ListItemDescription = React.forwardRef((props, ref) => {
+  const { as: Element = 'div', className, ...rest } = props;
+
+  return (
+    <Element
+      className={cx('iui-list-item-description', className)}
+      ref={ref}
+      {...rest}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'div', LListItemDescriptionOwnProps>;
+
+type LListItemDescriptionOwnProps = {}; // eslint-disable-line @typescript-eslint/ban-types
+
+// ----------------------------------------------------------------------------
+// Exported compound component
+
+export const ListItem = Object.assign(ListItemComponent, {
+  Icon: ListItemIcon,
+  Content: ListItemContent,
+  Description: ListItemDescription,
+});
 
 export type ListItemProps = PolymorphicComponentProps<'div', ListItemOwnProps>;
