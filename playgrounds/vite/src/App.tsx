65fdf1f837e4dc24050ab7e46115ca52689600b8
Mayank <9084735+mayank99@users.noreply.github.com>
2023-03-22 13:57:30 -0400

    logical properties

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,9 +1,15 @@
-import { Button } from '@itwin/itwinui-react';
+import { Button, List, ListItem, LinkAction } from '@itwin/itwinui-react';
 
 const App = () => {
   return (
     <>
-      <Button>Hello world</Button>
+      <List>
+        <ListItem>
+          <LinkAction href='#'>item 1</LinkAction>
+        </ListItem>
+        <ListItem>item 2</ListItem>
+        <ListItem>item 3</ListItem>
+      </List>
     </>
   );
 };
