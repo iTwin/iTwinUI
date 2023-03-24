794e8676f7139682aec14b1deafb63094f690a27
Annie <22462084+LostABike@users.noreply.github.com>
2023-03-24 10:45:16 -0500

    just testing

--- packages/itwinui-react/src/core/Dialog/DialogMain.tsx
+++ packages/itwinui-react/src/core/Dialog/DialogMain.tsx
@@ -223,6 +223,14 @@ export const DialogMain = React.forwardRef<HTMLDivElement, DialogMainProps>(
       </div>
     );
 
+    const contentRef = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+      if (isOpen && contentRef.current) {
+        contentRef.current.focus();
+      }
+    }, [isOpen]);
+
     return (
       <CSSTransition
         in={isOpen}
