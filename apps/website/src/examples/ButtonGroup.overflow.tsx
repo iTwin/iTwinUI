332bddca8e41c7746abe8b87b43b1c379ca85675
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-03-28 17:05:35 +0300

    Updating button group docs

---
+++ apps/website/src/examples/ButtonGroup.overflow.tsx
@@ -0,0 +1,89 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { ButtonGroup, DropdownMenu, IconButton, MenuItem } from '@itwin/itwinui-react';
+import {
+  SvgAdd,
+  SvgEdit,
+  SvgDelete,
+  SvgUndo,
+  SvgMore,
+  SvgPlaceholder,
+} from '@itwin/itwinui-icons-react';
+
+export default () => {
+  const buttons = [
+    <IconButton>
+      <SvgAdd />
+    </IconButton>,
+    <IconButton isActive>
+      <SvgEdit />
+    </IconButton>,
+    <IconButton disabled>
+      <SvgDelete />
+    </IconButton>,
+    <IconButton>
+      <SvgUndo />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+    <IconButton>
+      <SvgPlaceholder />
+    </IconButton>,
+  ];
+  return (
+    <div style={{ maxWidth: '70%' }}>
+      <ButtonGroup
+        overflowButton={(overflowStart) => (
+          <DropdownMenu
+            menuItems={(close) =>
+              Array(buttons.length - overflowStart + 1)
+                .fill(null)
+                .map((_, _index) => {
+                  const index = overflowStart + _index;
+                  const onClick = () => {
+                    close();
+                  };
+                  return (
+                    <MenuItem key={index} onClick={onClick} icon={<SvgPlaceholder />}>
+                      Button #{index}
+                    </MenuItem>
+                  );
+                })
+            }
+          >
+            <IconButton>
+              <SvgMore />
+            </IconButton>
+          </DropdownMenu>
+        )}
+      >
+        {buttons}
+      </ButtonGroup>
+    </div>
+  );
+};
