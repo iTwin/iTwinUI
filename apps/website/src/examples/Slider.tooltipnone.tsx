feb3c74bdc7a401d47244fe65fdfe6cf15e359fd
adamhock <78564221+adamhock@users.noreply.github.com>
2023-03-29 16:41:25 -0400

    Fix custom slider with no tooltip

--- apps/website/src/examples/Slider.tooltipnone.tsx
+++ apps/website/src/examples/Slider.tooltipnone.tsx
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
-import { Slider, Body } from '@itwin/itwinui-react';
+import { Slider, Text } from '@itwin/itwinui-react';
 
 export default () => {
   const dateFormatter = React.useMemo(() => {
@@ -14,17 +14,20 @@ export default () => {
     });
   }, []);
 
-  const [currentDate, setCurrentDate] = React.useState(new Date(Date.UTC(2019, 0, 1)));
+  const [currentValue, setCurrentValue] = React.useState({
+    number: 1,
+    date: new Date(Date.UTC(2019, 0, 1)),
+  });
 
-  const updateDate = React.useCallback((values: ReadonlyArray<number>) => {
+  const updateValue = React.useCallback((values: ReadonlyArray<number>) => {
     const newDate = new Date(Date.UTC(2019, 0, values[0]));
-    setCurrentDate(newDate);
+    setCurrentValue({ number: values[0], date: newDate });
   }, []);
 
   return (
     <div style={{ width: 'min(100%, 300px)' }}>
       <Slider
-        values={[0]}
+        values={[currentValue.number]}
         min={1}
         max={365}
         minLabel={'Date'}
@@ -32,8 +35,8 @@ export default () => {
         tooltipProps={() => {
           return { visible: false };
         }}
-        onUpdate={updateDate}
-        onChange={updateDate}
+        onUpdate={updateValue}
+        onChange={updateValue}
         tickLabels={
           <div
             style={{
@@ -43,14 +46,9 @@ export default () => {
               marginTop: '20px',
             }}
           >
-            <Body
-              style={{
-                width: '60px',
-                marginRight: '6px',
-              }}
-            >
-              {dateFormatter.format(currentDate)}
-            </Body>
+            <Text variant='body' style={{ width: '60px', marginRight: '6px' }}>
+              {dateFormatter.format(currentValue.date)}
+            </Text>
           </div>
         }
       />
