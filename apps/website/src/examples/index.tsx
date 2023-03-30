d986538584242a58fb166a3294dc01a83eb94448
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-03-30 10:39:27 +0300

    website: Buttons documentation update (#1158)
    Co-authored-by: Jon Graft <849817+FlyersPh9@users.noreply.github.com>
    Co-authored-by: adamhock <78564221+adamhock@users.noreply.github.com>

--- apps/website/src/examples/index.tsx
+++ apps/website/src/examples/index.tsx
@@ -38,7 +38,11 @@ export { default as BreadcrumbsMainExample } from './Breadcrumbs.main';
 export { default as BreadcrumbsTruncationExample } from './Breadcrumbs.truncation';
 export { default as BreadcrumbsCustomOverflowDropdownExample } from './Breadcrumbs.customOverflowDropdown';
 
+export { default as ButtonSizeExample } from './Button.size';
 export { default as ButtonDefaultExample } from './Button.default';
+export { default as ButtonHighVisibilityExample } from './Button.highvisibility';
+export { default as ButtonBorderlessExample } from './Button.borderless';
+export { default as ButtonCTAExample } from './Button.cta';
 export { default as ButtonMainExample } from './Button.main';
 export { default as IconButtonMainExample } from './IconButton.main';
 export { default as IdeasButtonMainExample } from './IdeasButton.main';
