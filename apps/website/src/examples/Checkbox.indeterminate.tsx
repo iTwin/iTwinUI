351ebe393cedcb29fec8013d3cd5261ae48a2adc
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2023-03-30 10:24:13 -0400

    Indeterminate checkbox demos actually work.

--- apps/website/src/examples/Checkbox.indeterminate.tsx
+++ apps/website/src/examples/Checkbox.indeterminate.tsx
@@ -6,17 +6,56 @@ import * as React from 'react';
 import { Checkbox, Flex } from '@itwin/itwinui-react';
 
 export default () => {
+  const [option1, setOption1] = React.useState(false);
+  const [option2, setOption2] = React.useState(true);
+  const [allOptions, setAllOptions] = React.useState(false);
+  const [isIndeterminate, setIsIndeterminate] = React.useState(true);
+
+  React.useEffect(() => {
+    if (option1 && option2) {
+      setAllOptions(true);
+      setIsIndeterminate(false);
+      return;
+    }
+    if (option1 || option2) {
+      setAllOptions(false);
+      setIsIndeterminate(true);
+    } else {
+      setAllOptions(false);
+      setIsIndeterminate(false);
+    }
+  }, [option1, option2]);
+
+  const onAllChange = (value: boolean) => {
+    setAllOptions(value);
+    setIsIndeterminate(false);
+    setOption1(value);
+    setOption2(value);
+  };
+
   return (
     <Flex flexDirection='column' alignItems='flex-start'>
-      <Checkbox label='Option 1' indeterminate defaultChecked />
+      <Checkbox
+        label='Option 1'
+        onChange={(event) => onAllChange(event.target.checked)}
+        indeterminate={isIndeterminate}
+        checked={allOptions}
+      />
       <Flex
         flexDirection='column'
         alignItems='flex-start'
         style={{ marginLeft: 'var(--iui-size-l)' }}
       >
-        <Checkbox label='Option 1.1' />
-        <Checkbox label='Option 1.2' defaultChecked />
-        <Checkbox label='Option 1.3' />
+        <Checkbox
+          label='Option 1.1'
+          checked={option1}
+          onChange={(event) => setOption1(event.target.checked)}
+        />
+        <Checkbox
+          label='Option 1.2'
+          checked={option2}
+          onChange={(event) => setOption2(event.target.checked)}
+        />
       </Flex>
     </Flex>
   );
