90d9ed14273f2ec23f0def960607639c7141d8ae
Mayank <9084735+mayank99@users.noreply.github.com>
2023-04-07 13:08:28 -0400

    move tests

--- packages/itwinui-react/src/core/List/List.test.tsx
+++ packages/itwinui-react/src/core/List/List.test.tsx
@@ -24,71 +24,7 @@ it('should render in its most basic state', () => {
   });
 });
 
-describe('ListItem', () => {
-  it('should respect actionable prop', () => {
-    render(<ListItem actionable>Item</ListItem>);
-    expect(screen.getByRole('listitem')).toHaveAttribute(
-      'data-iui-actionable',
-      'true',
-    );
-  });
-
-  it('should respect active prop', () => {
-    render(<ListItem active>Item</ListItem>);
-    expect(screen.getByRole('listitem')).toHaveAttribute(
-      'data-iui-active',
-      'true',
-    );
-  });
-
-  it('should respect focused prop', () => {
-    render(<ListItem focused>Item</ListItem>);
-    expect(screen.getByRole('listitem')).toHaveAttribute(
-      'data-iui-focused',
-      'true',
-    );
-  });
-
-  it('should respect disabled prop', () => {
-    render(<ListItem disabled>Item</ListItem>);
-    expect(screen.getByRole('listitem')).toHaveAttribute(
-      'data-iui-disabled',
-      'true',
-    );
-  });
-
-  it('should respect size prop', () => {
-    render(<ListItem size='large'>Item</ListItem>);
-    expect(screen.getByRole('listitem')).toHaveAttribute(
-      'data-iui-size',
-      'large',
-    );
-  });
-
-  it('should work with subcomponents', () => {
-    render(
-      <ListItem>
-        <ListItem.Icon>icon</ListItem.Icon>
-        <ListItem.Content>
-          item
-          <ListItem.Description>description</ListItem.Description>
-        </ListItem.Content>
-      </ListItem>,
-    );
-
-    expect(screen.getByText('icon')).toHaveClass('iui-list-item-icon');
-    expect(screen.getByText('item')).toHaveClass('iui-list-item-content');
-    expect(screen.getByText('description')).toHaveClass(
-      'iui-list-item-description',
-    );
-  });
-
-  it('should respect other props', () => {
-    const { container } = render(
-      <ListItem as='div' role='option'>
-        Item
-      </ListItem>,
-    );
-    expect(container.querySelector('div')).toHaveAttribute('role', 'option');
-  });
+it('should be polymorphic', () => {
+  const { container } = render(<List as='div'>Item</List>);
+  expect(container.querySelector('div')).toHaveAttribute('role', 'list');
 });
