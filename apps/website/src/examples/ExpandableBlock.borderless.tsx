c0c341ef7d46477beec49b4e3dc2ab34142dbbec
adamhock <78564221+adamhock@users.noreply.github.com>
2023-04-10 14:18:21 -0400

    website: Update Expandable Block documentation (#1184)

--- apps/website/src/examples/ExpandableBlock.borderless.tsx
+++ apps/website/src/examples/ExpandableBlock.borderless.tsx
@@ -8,7 +8,9 @@ import { ExpandableBlock } from '@itwin/itwinui-react';
 export default () => {
   return (
     <div style={{ width: 'min(100%, 300px)' }}>
-      <ExpandableBlock title='Expandable Block'>Content in block!</ExpandableBlock>
+      <ExpandableBlock styleType='borderless' title='Borderless expandable block'>
+        Content in block!
+      </ExpandableBlock>
     </div>
   );
 };
