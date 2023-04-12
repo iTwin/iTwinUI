b985e80df6ec3e730bc24fc3bebb92146a2b5911
adamhock <78564221+adamhock@users.noreply.github.com>
2023-04-12 15:42:01 -0400

    Moved asButton to usage.

--- apps/website/src/examples/Anchor.main.tsx
+++ apps/website/src/examples/Anchor.main.tsx
@@ -6,5 +6,5 @@ import * as React from 'react';
 import { Anchor } from '@itwin/itwinui-react';
 
 export default () => {
-  return <Anchor href='https://www.example.com/'>www.example.com</Anchor>;
+  return <Anchor href='https://www.example.com/'>Example Site</Anchor>;
 };
