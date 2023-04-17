bd9581246b44e7f89c9fac5c98983a01ee81e9cb
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-04-17 10:50:05 +0300

    searchbox-comment solve

--- apps/storybook/src/SearchBox.stories.tsx
+++ apps/storybook/src/SearchBox.stories.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Story, Meta } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 import React from 'react';
 import {
   SearchBox,
@@ -10,6 +10,7 @@ import {
   IconButton,
   Text,
   Flex,
+  Divider,
 } from '@itwin/itwinui-react';
 import {
   SvgCaretDownSmall,
@@ -26,61 +27,84 @@ export default {
   title: 'Input/SearchBox',
 } as Meta<SearchBoxProps>;
 
-export const Basic: Story<SearchBoxProps> = () => {
+export const Basic: StoryFn<SearchBoxProps> = () => {
   return (
-    <Flex>
-      <SearchBox />
+    <Flex flexDirection='column'>
+      <SearchBox inputProps={{ placeholder: 'Basic search' }} />
       <SearchBox>
         <SearchBox.Button title='Search button' />
-        <SearchBox.Input />
-        <SearchBox.Button title='random'>button</SearchBox.Button>
+        <SearchBox.Input placeholder='Basic search with custom interactions' />
+        <Text
+          isMuted
+          variant='body'
+          as='p'
+          style={{ paddingRight: 'var(--iui-size-s)', alignSelf: 'center' }}
+        >
+          0/3
+        </Text>
+        <Divider orientation='vertical' />
+        <SearchBox.Button styleType='borderless'>
+          <SvgCaretUpSmall />
+        </SearchBox.Button>
+        <SearchBox.Button styleType='borderless'>
+          <SvgCaretDownSmall />
+        </SearchBox.Button>
       </SearchBox>
     </Flex>
   );
 };
 
-export const Expandable: Story<SearchBoxProps> = () => {
+export const Expandable: StoryFn<SearchBoxProps> = () => {
   return (
     <Flex flexDirection='column' alignItems='start'>
       <SearchBox expandable />
-      <SearchBox expandable isExpanded />
-      <SearchBox
-        expandable
-        inputProps={{ placeholder: 'Test' }}
-        onToggle={(isExpanding) => {
-          console.log(isExpanding);
-        }}
-      />
+      <SearchBox expandable>
+        <SearchBox.Icon />
+        <SearchBox.Input placeholder='Expandable search with custom interactions' />
+        <Text
+          isMuted
+          variant='body'
+          as='p'
+          style={{ paddingRight: 'var(--iui-size-s)', alignSelf: 'center' }}
+        >
+          0/3
+        </Text>
+        <Divider orientation='vertical' />
+        <SearchBox.Button styleType='borderless'>
+          <SvgCaretUpSmall />
+        </SearchBox.Button>
+        <SearchBox.Button styleType='borderless'>
+          <SvgCaretDownSmall />
+        </SearchBox.Button>
+      </SearchBox>
     </Flex>
   );
 };
 
-export const Small: Story<SearchBoxProps> = (args) => {
-  return (
-    <SearchBox {...args} size='small'>
-      <input type='search' placeholder='Search...' />
-      <IconButton size='small' styleType='borderless'>
-        <SvgCaretUpSmall />
-      </IconButton>
-      <IconButton size='small' styleType='borderless'>
-        <SvgCaretDownSmall />
-      </IconButton>
-    </SearchBox>
-  );
+export const Small: StoryFn<SearchBoxProps> = () => {
+  return <SearchBox size='small' inputProps={{ placeholder: 'Search...' }} />;
 };
 
-export const WithCustomAction: Story<SearchBoxProps> = (args) => {
+export const WithCustomAction: StoryFn<SearchBoxProps> = () => {
   const [expanded, setExpanded] = React.useState(false);
+
+  const handleToggle = (value: boolean) => {
+    console.log('handling toggle', value);
+    setExpanded(value);
+  };
+
+  const handleClose = () => {
+    console.log('handling close');
+    setExpanded(false);
+  };
 
   return (
     <SearchBox
       expandable
       isExpanded={expanded}
-      onFocus={() => setExpanded(true)}
-      // onToggle={(_expanded: boolean) => setExpanded(_expanded)}
-      {...args}
+      onOpen={() => handleToggle(true)}
     >
-      <IconButton styleType='borderless' onClick={() => setExpanded(false)}>
+      <IconButton styleType='borderless' onClick={handleClose}>
         <SvgCloseSmall />
       </IconButton>
       <input type='search' placeholder='Search...' />
