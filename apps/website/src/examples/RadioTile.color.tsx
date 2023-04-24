f925cbe91b0db866083f2ab093895c7946c39246
idakukimya <Ida.Kukimiya@bentley.com>
2023-04-24 14:13:23 -0400

    added miss copyright

--- apps/website/src/examples/RadioTile.color.tsx
+++ apps/website/src/examples/RadioTile.color.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import { RadioTileGroup, RadioTile } from '@itwin/itwinui-react';
 
