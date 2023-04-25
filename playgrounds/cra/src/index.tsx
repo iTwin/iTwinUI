2595d0a4b76d92d3dca3f8175de1f03fa0340c52
Mayank <9084735+mayank99@users.noreply.github.com>
2023-04-25 09:40:58 -0400

    fix and clean up cra playground code

--- playgrounds/cra/src/index.tsx
+++ playgrounds/cra/src/index.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
 import { IconButton, ThemeProvider } from '@itwin/itwinui-react';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 
 const AppWithWrapper = () => {
-  const [theme, setTheme] = React.useState(
-    matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
+  const [theme, setTheme] = React.useState<'light' | 'dark'>(() =>
+    matchMedia?.('(prefers-color-scheme: dark)').matches ? 'dark' : 'light',
   );
 
   return (
-    <ThemeProvider className='theme-provider' theme={theme}>
+    <ThemeProvider className='my-app-wrapper' theme={theme}>
       <IconButton
+        className='my-theme-toggle'
         styleType='borderless'
         aria-label='Toggle theme'
-        style={{ position: 'fixed', top: 0, right: 0 }}
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       >
         {theme === 'light' ? '🌞' : '🌙'}
