dce3843abe5e64e8338efa6846edc00d3c154172
Ida Kukimiya <kukimiya.ida@gmail.com>
2023-05-03 10:51:11 -0400

    Update apps/website/src/examples/InputGroup.radioGroup.tsx
    Co-authored-by: Leah Graham <37936169+elephantcatdog@users.noreply.github.com>

--- apps/website/src/examples/InputGroup.radioGroup.tsx
+++ apps/website/src/examples/InputGroup.radioGroup.tsx
@@ -3,36 +3,14 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { InputGroup, Radio } from '@itwin/itwinui-react';
-import { SvgSmileyHappy } from '@itwin/itwinui-react/cjs/core/utils';
-import { SvgSmileySad } from '@itwin/itwinui-icons-react';
-import * as React from 'react';
+import { SvgSmileyHappy, SvgSmileySad } from '@itwin/itwinui-icons-react';
 
-export default (args) => {
-  const option1Label = <SvgSmileyHappy />;
-  const option2Label = <SvgSmileySad />;
-  function action(arg0: string): React.ChangeEventHandler<HTMLInputElement> {
-    throw new Error('Function not implemented.');
-  }
-
+export default () => {
   return (
     <>
-      <InputGroup label='Radio group' message='Tell me how happy you are' {...args}>
-        <Radio
-          name='choice'
-          value='option1'
-          onChange={action('Clicked option 1!')}
-          label={option1Label}
-          disabled={args.disabled}
-          required={args.required}
-        />
-        <Radio
-          name='choice'
-          value='option2'
-          onChange={action('Clicked option 2!')}
-          label={option2Label}
-          disabled={args.disabled}
-          required={args.required}
-        />
+      <InputGroup label='Radio group' message='Tell me how happy you are'>
+        <Radio name='choice' value='option1' label={<SvgSmileyHappy />} />
+        <Radio name='choice' value='option2' label={<SvgSmileySad />} />
       </InputGroup>
     </>
   );
