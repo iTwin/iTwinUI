cc30bbc3e266cb057da80179dbd9db792ae101cd
Leah Graham <37936169+elephantcatdog@users.noreply.github.com>
2023-05-03 21:49:13 -0500

    Remove legacy support

--- packages/itwinui-css/backstop/scenarios/breadcrumbs.js
+++ packages/itwinui-css/backstop/scenarios/breadcrumbs.js
@@ -14,14 +14,6 @@ module.exports = [
     actions: [hover('#test-3'), focus('#test-4')],
     selectors: ['#demo-buttons'],
   }),
-  scenario('State hover and focus breadcrumb anchors', {
-    actions: [hover('#test-5'), focus('#test-6')],
-    selectors: ['#demo-anchors-2'],
-  }),
-  scenario('State hover and focus breadcrumb buttons', {
-    actions: [hover('#test-7'), focus('#test-8')],
-    selectors: ['#demo-buttons-2'],
-  }),
   scenario('State hover and focus overflow buttons', {
     actions: [hover('#overflow-hover'), focus('#overflow-focus')],
     selectors: ['#demo-buttons-3'],
