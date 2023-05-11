2e5767a7be080e420a861cb9b8e67eac97a50412
adamhock <78564221+adamhock@users.noreply.github.com>
2023-05-11 13:03:13 -0400

    Fixed indentation. Updated website.

--- apps/website/src/examples/TransferList.withtoolbar.tsx
+++ apps/website/src/examples/TransferList.withtoolbar.tsx
@@ -5,44 +5,128 @@
 import * as React from 'react';
 import { TransferList, IconButton } from '@itwin/itwinui-react';
 import {
-  SvgChevronDown,
-  SvgChevronUp,
+  SvgChevronLeftDouble,
+  SvgChevronRightDouble,
   SvgChevronRight,
   SvgChevronLeft,
 } from '@itwin/itwinui-icons-react';
 
 export default () => {
+  type TransferItemDataType = {
+    name: string;
+    active: boolean;
+  };
+
+  const [optionData, setOptionData] = React.useState([
+    { name: 'Option 1', active: false },
+    { name: 'Option 2', active: false },
+    { name: 'Option 3', active: false },
+    { name: 'Option 4', active: false },
+    { name: 'Option 5', active: false },
+    { name: 'Option 6', active: false },
+  ]);
+
+  const [appliedData, setAppliedData] = React.useState([{ name: 'Option 7', active: false }]);
+
+  const transfer = (
+    fromData: Array<TransferItemDataType>,
+    setFromData: (data: Array<TransferItemDataType>) => void,
+    setToData: (data: Array<TransferItemDataType>) => void,
+    sendAll?: boolean
+  ) => {
+    setToData((oldToData) => {
+      const newToData = [...oldToData];
+      const newFromData: Array<TransferItemDataType> = [];
+      fromData.forEach((item) => {
+        if (sendAll || item.active === true) {
+          const newItem = item;
+          newItem.active = false;
+          newToData.push(newItem);
+        } else {
+          newFromData.push(item);
+        }
+      });
+      setFromData(newFromData);
+      return newToData;
+    });
+  };
+
   return (
     <TransferList style={{ width: 500 }}>
       <TransferList.Area>
         <TransferList.Label>Options</TransferList.Label>
         <TransferList.List role={'listbox'}>
-          <TransferList.ListItem actionable>Option 1</TransferList.ListItem>
-          <TransferList.ListItem actionable>Option 2</TransferList.ListItem>
-          <TransferList.ListItem actionable>Option 3</TransferList.ListItem>
-          <TransferList.ListItem actionable>Option 4</TransferList.ListItem>
-          <TransferList.ListItem actionable>Option 5</TransferList.ListItem>
-          <TransferList.ListItem actionable>Option 6</TransferList.ListItem>
+          {optionData?.map((item, index) => {
+            return (
+              <TransferList.ListItem
+                actionable
+                active={item.active}
+                onActiveChange={(isActive: boolean) => {
+                  setOptionData((oldData) => {
+                    const newData = [...oldData];
+                    const newObject = { ...newData[index] };
+                    newObject.active = !isActive;
+                    newData[index] = newObject;
+                    return newData;
+                  });
+                }}
+              >
+                {item.name}
+              </TransferList.ListItem>
+            );
+          })}
         </TransferList.List>
       </TransferList.Area>
       <TransferList.Toolbar>
-        <IconButton styleType={'borderless'}>
-          <SvgChevronUp />
+        <IconButton
+          styleType={'borderless'}
+          onClick={() => {
+            transfer(optionData, setOptionData, setAppliedData, true);
+          }}
+        >
+          <SvgChevronRightDouble />
         </IconButton>
-        <IconButton styleType={'borderless'}>
-          <SvgChevronDown />
+        <IconButton
+          styleType={'borderless'}
+          onClick={() => transfer(optionData, setOptionData, setAppliedData, false)}
+        >
+          <SvgChevronRight />
         </IconButton>
-        <IconButton styleType={'borderless'}>
+        <IconButton
+          styleType={'borderless'}
+          onClick={() => transfer(appliedData, setAppliedData, setOptionData, false)}
+        >
           <SvgChevronLeft />
         </IconButton>
-        <IconButton styleType={'borderless'}>
-          <SvgChevronRight />
+        <IconButton
+          styleType={'borderless'}
+          onClick={() => transfer(appliedData, setAppliedData, setOptionData, true)}
+        >
+          <SvgChevronLeftDouble />
         </IconButton>
       </TransferList.Toolbar>
       <TransferList.Area>
         <TransferList.Label>Applied</TransferList.Label>
         <TransferList.List role={'listbox'}>
-          <TransferList.ListItem actionable>Option 7</TransferList.ListItem>
+          {appliedData.map((item, index) => {
+            return (
+              <TransferList.ListItem
+                actionable
+                active={item.active}
+                onActiveChange={(isActive: boolean) => {
+                  setAppliedData((oldData) => {
+                    const newData = [...oldData];
+                    const newObject = { ...newData[index] };
+                    newObject.active = !isActive;
+                    newData[index] = newObject;
+                    return newData;
+                  });
+                }}
+              >
+                {item.name}
+              </TransferList.ListItem>
+            );
+          })}
         </TransferList.List>
       </TransferList.Area>
     </TransferList>
