e7f5da85ed3b6c6a8f2b01e71475435e7639faa5
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-05-12 17:19:13 +0300

    remove unit test and add visual

--- apps/storybook/src/Tabs.test.ts
+++ apps/storybook/src/Tabs.test.ts
@@ -20,6 +20,18 @@ describe('Tabs', () => {
       cy.visit('iframe', { qs: { id } });
       cy.wait(1000); // wait for resize observer to be done
       cy.compareSnapshot(testName);
+
+      if (testName === 'Horizontal Overflow') {
+        const tabs = cy.get('li > button.iui-tab').should('have.length', 13);
+
+        tabs.last().focus();
+        cy.compareSnapshot(`${testName} (Scroll end)`);
+
+        // cy somehow loses tabs list and does not focus on first element so getting it again.
+        cy.focused().blur();
+        cy.get('li > button.iui-tab').first().focus();
+        cy.compareSnapshot(`${testName} (Scroll start)`);
+      }
     });
   });
 });
