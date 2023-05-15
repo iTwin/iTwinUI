4e1fcec19a3f1f357445e1ae0fe42b29d63702da
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-05-15 16:29:00 +0300

    Simplified main example and some mdx changes

--- apps/website/src/examples/Searchbox.main.tsx
+++ apps/website/src/examples/Searchbox.main.tsx
@@ -4,32 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import { SearchBox, Flex } from '@itwin/itwinui-react';
-import { SvgCloseSmall } from '@itwin/itwinui-icons-react';
 
 export default () => {
-  const [inputValue, setInputValue] = React.useState<string>('');
   return (
     <Flex style={{ width: '70%' }}>
-      <SearchBox aria-label='Search input'>
-        <SearchBox.Icon />
-        <SearchBox.Input
-          placeholder='Try typing...'
-          value={inputValue}
-          onChange={(e) => {
-            setInputValue(e.target.value);
-          }}
-        />
-        {inputValue !== '' ? (
-          <SearchBox.Button
-            label='Clear'
-            onClick={() => {
-              setInputValue('');
-            }}
-          >
-            <SvgCloseSmall />
-          </SearchBox.Button>
-        ) : null}
-      </SearchBox>
+      <SearchBox inputProps={{ placeholder: 'SearchBox component' }} />
     </Flex>
   );
 };
