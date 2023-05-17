3d60a206c1679efdb9e0007af5443838640e20fd
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-05-17 06:21:30 -0400

    Added React markdown to the output of SearchInput

--- apps/website/src/components/SearchInput/SearchInput.tsx
+++ apps/website/src/components/SearchInput/SearchInput.tsx
@@ -10,10 +10,19 @@ import {
 import { action } from 'nanostores';
 import React from 'react';
 
+import ReactMarkdown from 'react-markdown';
+// import ReactDom from 'react-dom';
+
 export const SearchInput = () => {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const sampleAnswer = `
+* To style an iTwinUI alert, you can use the following:
+  * Appearance:
+    * Warning: Used to inform the user of something that is not currently disruptive to their work, but may be soon or eventually.
+    * Informational: Default style when there is no defined status for the alert. Inform users about events that they should be aware of, but that are not disruptive to their work.`;
+
+  const [isModalOpen, setIsModalOpen] = React.useState(true);
   const [searchQuery, setSearchQuery] = React.useState('');
-  const [answer, setAnswer] = React.useState('');
+  const [answer, setAnswer] = React.useState(sampleAnswer);
   const [responseAwaiting, setResponseAwaiting] = React.useState(false);
 
   const fetchAnswer = async () => {
@@ -22,13 +31,17 @@ export const SearchInput = () => {
     }
     setResponseAwaiting(true);
 
-    // Call localhost:8000/answer?query=searchQuery
-    const response = await fetch(`http://localhost:8000/search?query=${searchQuery}`);
-    const data = await response.json();
-    console.log(data);
+    try {
+      // Call localhost:8000/answer?query=searchQuery
+      const response = await fetch(`http://localhost:8000/search?query=${searchQuery}`);
+      const data = await response.json();
+      console.log(data);
 
-    // // Set answer to the response
-    // setAnswer(data);
+      // Set answer to the response
+      setAnswer(data);
+    } catch (e) {
+      console.error('ERROR', e);
+    }
 
     setResponseAwaiting(false);
   };
@@ -41,6 +54,7 @@ export const SearchInput = () => {
         onChange={(e) => console.log(e.target.value)}
       />
       <Modal
+        style={{ width: '50vw', height: '80vh', display: 'flex', flexDirection: 'column' }}
         isOpen={isModalOpen}
         title={'AI Powered Search'}
         onClose={() => setIsModalOpen(false)}
@@ -51,17 +65,22 @@ export const SearchInput = () => {
         }}
         isDismissible={false}
       >
-        <ModalContent>{/* ChatGPT's conversation messages*/}</ModalContent>
-        <p>{answer}</p>
+        {/* <ModalContent style={{ flex: '1', overflowY: 'scroll' }}> */}
+        {/* ChatGPT's conversation messages*/}
+        {/* <p style={{ flex: '1' }}>{answer}</p> */}
+        <div style={{ flex: '1', overflowY: 'scroll' }}>
+          <ReactMarkdown className='prose lg:prose-xl'>{answer}</ReactMarkdown>
+        </div>
         <Input
           placeholder='Send a message...'
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
+        {/* </ModalContent> */}
         <ModalButtonBar>
           <Button onClick={() => setIsModalOpen(false)}>Close</Button>
           {responseAwaiting ? (
-            <ProgressRadial />
+            <ProgressRadial indeterminate={true} />
           ) : (
             <Button onClick={() => fetchAnswer()}>Send</Button>
           )}
