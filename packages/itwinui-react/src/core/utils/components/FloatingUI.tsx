9fa88948571bc212e54faf50b123e2470d33b33e
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-05-23 15:27:36 +0300

    Trying to make that work

--- packages/itwinui-react/src/core/utils/components/FloatingUI.tsx
+++ packages/itwinui-react/src/core/utils/components/FloatingUI.tsx
@@ -5,40 +5,29 @@ import {
   offset,
   flip,
   shift,
-  useClick,
   useDismiss,
   useRole,
   useInteractions,
-  useMergeRefs,
-  type Placement,
   FloatingPortal,
-  FloatingFocusManager,
-  useId,
+  useHover,
+  useFocus,
 } from '@floating-ui/react';
+import type { Placement } from '@floating-ui/react';
+import cx from 'classnames';
+import type { PolymorphicForwardRefComponent } from '../props.js';
 
-interface PopoverOptions {
-  initialOpen?: boolean;
+type PopoverNewProps = {
   placement?: Placement;
-  modal?: boolean;
-  open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-}
+  visible?: boolean;
+  toggleVisible?: (isVisible: boolean) => void;
+};
 
-export function usePopover({
-  initialOpen = false,
-  placement = 'bottom',
-  modal,
-  open: controlledOpen,
-  onOpenChange: setControlledOpen,
-}: PopoverOptions = {}) {
-  const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
-  const [labelId, setLabelId] = React.useState<string | undefined>();
-  const [descriptionId, setDescriptionId] = React.useState<
-    string | undefined
-  >();
+export const usePopoverNew = (props: PopoverNewProps) => {
+  const { placement, visible, toggleVisible } = props;
+  const [localVisible, setLocalIsVisible] = React.useState(visible);
 
-  const open = controlledOpen ?? uncontrolledOpen;
-  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+  const open = visible ?? localVisible;
+  const setOpen = toggleVisible ?? setLocalIsVisible;
 
   const data = useFloating({
     placement,
@@ -55,15 +44,16 @@ export function usePopover({
     ],
   });
 
-  const context = data.context;
-
-  const click = useClick(context, {
-    enabled: controlledOpen == null,
+  const hover = useHover(context, {
+    move: false,
+    enabled: visible == null,
+  });
+  const focus = useFocus(context, {
+    enabled: visible == null,
   });
   const dismiss = useDismiss(context);
-  const role = useRole(context);
-
-  const interactions = useInteractions([click, dismiss, role]);
+  const role = useRole(context, { role: 'tooltip' });
+  const interactions = useInteractions([hover, focus, dismiss, role]);
 
   return React.useMemo(
     () => ({
@@ -71,181 +61,43 @@ export function usePopover({
       setOpen,
       ...interactions,
       ...data,
-      modal,
-      labelId,
-      descriptionId,
-      setLabelId,
-      setDescriptionId,
     }),
-    [open, setOpen, interactions, data, modal, labelId, descriptionId],
-  );
-}
-
-type ContextType =
-  | (ReturnType<typeof usePopover> & {
-      setLabelId: React.Dispatch<React.SetStateAction<string | undefined>>;
-      setDescriptionId: React.Dispatch<
-        React.SetStateAction<string | undefined>
-      >;
-    })
-  | null;
-
-const PopoverContext = React.createContext<ContextType>(null);
-
-export const usePopoverContext = () => {
-  const context = React.useContext(PopoverContext);
-
-  if (context == null) {
-    throw new Error('Popover components must be wrapped in <Popover />');
-  }
-
-  return context;
-};
-
-export const Popover = ({
-  children,
-  modal = false,
-  ...restOptions
-}: {
-  children: React.ReactNode;
-} & PopoverOptions) => {
-  // This can accept any props as options, e.g. `placement`,
-  // or other positioning options.
-  const popover = usePopover({ modal, ...restOptions });
-  return (
-    <PopoverContext.Provider value={popover}>
-      {children}
-    </PopoverContext.Provider>
+    [open, setOpen, interactions, data],
   );
 };
 
-interface PopoverTriggerProps {
-  children: React.ReactNode;
-  asChild?: boolean;
-}
+// ------------------------------------------------------------------
+// Popover content component
 
-export const PopoverTrigger = React.forwardRef<
-  HTMLElement,
-  React.HTMLProps<HTMLElement> & PopoverTriggerProps
->(function PopoverTrigger({ children, asChild = false, ...props }, propRef) {
-  const context = usePopoverContext();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const childrenRef = (children as any).ref;
-  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+// eslint-disable-next-line @typescript-eslint/ban-types
+type PopoverContentOwnProps = {};
 
-  // `asChild` allows the user to pass any element as the anchor
-  if (asChild && React.isValidElement(children)) {
-    return React.cloneElement(
-      children,
-      context.getReferenceProps({
-        ref,
-        ...props,
-        ...children.props,
-        'data-state': context.open ? 'open' : 'closed',
-      }),
-    );
-  }
-
-  return (
-    <button
-      ref={ref}
-      type='button'
-      // The user can style the trigger based on the state
-      data-state={context.open ? 'open' : 'closed'}
-      {...context.getReferenceProps(props)}
-    >
-      {children}
-    </button>
-  );
-});
-
-export const PopoverContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLProps<HTMLDivElement>
->(function PopoverContent(props, propRef) {
-  const { context: floatingContext, ...context } = usePopoverContext();
-  const ref = useMergeRefs([context.refs.setFloating, propRef]);
-
-  if (!floatingContext.open) {
-    return null;
-  }
-
+export const PopoverContent = React.forwardRef((props, propRef) => {
+  const { as: Element = 'div', ...rest } = props;
   return (
     <FloatingPortal>
-      <FloatingFocusManager context={floatingContext} modal={context.modal}>
-        <div
-          ref={ref}
-          style={{
-            ...context.floatingStyles,
-            ...props.style,
-          }}
-          aria-labelledby={context.labelId}
-          aria-describedby={context.descriptionId}
-          {...context.getFloatingProps(props)}
-        >
-          {props.children}
-        </div>
-      </FloatingFocusManager>
+      <Element
+        ref={propRef}
+        className={cx('iui-popover', props.className)}
+        {...rest}
+      >
+        {props.children}
+      </Element>
     </FloatingPortal>
   );
-});
+}) as PolymorphicForwardRefComponent<'div', PopoverContentOwnProps>;
 
-export const PopoverHeading = React.forwardRef<
-  HTMLHeadingElement,
-  React.HTMLProps<HTMLHeadingElement>
->(function PopoverHeading({ children, ...props }, ref) {
-  const { setLabelId } = usePopoverContext();
-  const id = useId();
+// ------------------------------------------------------------------
+// Popover Trigger component
 
-  // Only sets `aria-labelledby` on the Popover root element
-  // if this component is mounted inside it.
-  React.useLayoutEffect(() => {
-    setLabelId(id);
-    return () => setLabelId(undefined);
-  }, [id, setLabelId]);
+// eslint-disable-next-line @typescript-eslint/ban-types
+type PopoverTriggerOwnProps = {};
 
+export const PopoverTrigger = React.forwardRef((props, propRef) => {
+  const { as: Element = 'div', ...rest } = props;
   return (
-    <h2 {...props} ref={ref} id={id}>
-      {children}
-    </h2>
+    <Element ref={propRef} {...rest}>
+      {props.children}
+    </Element>
   );
-});
-
-export const PopoverDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLProps<HTMLParagraphElement>
->(function PopoverDescription({ children, ...props }, ref) {
-  const { setDescriptionId } = usePopoverContext();
-  const id = useId();
-
-  // Only sets `aria-describedby` on the Popover root element
-  // if this component is mounted inside it.
-  React.useLayoutEffect(() => {
-    setDescriptionId(id);
-    return () => setDescriptionId(undefined);
-  }, [id, setDescriptionId]);
-
-  return (
-    <p {...props} ref={ref} id={id}>
-      {children}
-    </p>
-  );
-});
-
-export const PopoverClose = React.forwardRef<
-  HTMLButtonElement,
-  React.ButtonHTMLAttributes<HTMLButtonElement>
->(function PopoverClose(props, ref) {
-  const { setOpen } = usePopoverContext();
-  return (
-    <button
-      type='button'
-      ref={ref}
-      {...props}
-      onClick={(event) => {
-        props.onClick?.(event);
-        setOpen(false);
-      }}
-    />
-  );
-});
+}) as PolymorphicForwardRefComponent<'div', PopoverTriggerOwnProps>;
