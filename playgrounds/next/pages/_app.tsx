bbc091a0a15a2fea6d6864cb83173110dda19914
Mayank <9084735+mayank99@users.noreply.github.com>
2023-05-30 16:44:25 -0400

    update all templates

--- playgrounds/next/pages/_app.tsx
+++ playgrounds/next/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@itwin/itwinui-react';
+import '@itwin/itwinui-react/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,7 +16,10 @@ export default function App({ Component, pageProps }: AppProps) {
           padding: 2rem 1rem;
         }
       `}</style>
-      <Component {...pageProps} />
+
+      <ThemeProvider theme='dark'>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </main>
   );
 }
