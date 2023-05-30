55dec6aa8f38de0d9fd1a1f70cb7c9eac4160966
Mayank <9084735+mayank99@users.noreply.github.com>
2023-05-30 16:12:25 -0400

    use vite plugin instead of script

--- packages/itwinui-react/src/styles.js/vite.config.mjs
+++ packages/itwinui-react/src/styles.js/vite.config.mjs
@@ -1,5 +1,9 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +27,14 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    {
+      name: 'copy-files-after-build',
+      closeBundle: async () => {
+        await copyBuildOutput();
+      },
+    },
+  ],
   css: {
     modules: {
       // TODO: use proper hash in v4
@@ -32,3 +44,41 @@ export default defineConfig({
     },
   },
 });
+
+// ----------------------------------------------------------------------------
+
+const root = path.join(__dirname, '..', '..');
+const srcDir = path.resolve(root, 'src');
+const distDir = path.resolve(srcDir, 'styles.js', 'dist');
+const distEsmDir = path.resolve(distDir, 'esm');
+const distCjsDir = path.resolve(distDir, 'cjs');
+const outCjsDir = path.resolve(root, 'cjs');
+const outEsmDir = path.resolve(root, 'esm');
+
+const copyBuildOutput = async () => {
+  // copy styles.js from src/styles.js/dist into cjs/styles.js and esm/styles.js
+  await fs.copyFile(
+    path.join(distEsmDir, 'styles.js'),
+    path.join(outEsmDir, 'styles.js'),
+  );
+  await fs.copyFile(
+    path.join(distCjsDir, 'styles.js'),
+    path.join(outCjsDir, 'styles.js'),
+  );
+
+  // copy styles.js/dist/styles.css into <root>/styles.css
+  await fs.copyFile(
+    path.join(distDir, 'styles.css'),
+    path.join(root, 'styles.css'),
+  );
+
+  // copy styles.d.ts from src into cjs/styles.d.ts and esm/styles.d.ts
+  await fs.copyFile(
+    path.join(srcDir, 'styles.d.ts'),
+    path.join(outEsmDir, 'styles.d.ts'),
+  );
+  await fs.copyFile(
+    path.join(srcDir, 'styles.d.ts'),
+    path.join(outCjsDir, 'styles.d.ts'),
+  );
+};
