2b900ff4c044aa4dee608ba378a0cc3aa209dd0d
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-06 11:24:05 -0400

    create cjs and esm dirs if they don't exist

--- packages/itwinui-react/src/styles.js/vite.config.mjs
+++ packages/itwinui-react/src/styles.js/vite.config.mjs
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
@@ -56,28 +56,36 @@ const outCjsDir = path.resolve(root, 'cjs');
 const outEsmDir = path.resolve(root, 'esm');
 
 const copyBuildOutput = async () => {
+  // create cjs/ and esm/ directories if they don't exist
+  if (!fs.existsSync(outCjsDir)) {
+    await fs.promises.mkdir(outCjsDir);
+  }
+  if (!fs.existsSync(outEsmDir)) {
+    await fs.promises.mkdir(outEsmDir);
+  }
+
   // copy styles.js from src/styles.js/dist/ into cjs/ and esm/
-  await fs.copyFile(
+  await fs.promises.copyFile(
     path.join(distEsmDir, 'styles.js'),
     path.join(outEsmDir, 'styles.js'),
   );
-  await fs.copyFile(
+  await fs.promises.copyFile(
     path.join(distCjsDir, 'styles.js'),
     path.join(outCjsDir, 'styles.js'),
   );
 
   // copy styles.css from src/styles.js/dist/ into <root>/
-  await fs.copyFile(
+  await fs.promises.copyFile(
     path.join(distDir, 'styles.css'),
     path.join(root, 'styles.css'),
   );
 
   // copy styles.d.ts from src/ into cjs/ and esm/
-  await fs.copyFile(
+  await fs.promises.copyFile(
     path.join(srcDir, 'styles.d.ts'),
     path.join(outEsmDir, 'styles.d.ts'),
   );
-  await fs.copyFile(
+  await fs.promises.copyFile(
     path.join(srcDir, 'styles.d.ts'),
     path.join(outCjsDir, 'styles.d.ts'),
   );
