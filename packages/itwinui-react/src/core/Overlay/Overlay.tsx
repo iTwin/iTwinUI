7ace441893edeb862898fac86a32a592beb2840c
Malik Rashad <marashad001@gmail.com>
2023-06-07 11:01:26 -0500

    directory changes & code cleanup

--- packages/itwinui-react/src/core/Overlay/Overlay.tsx
+++ packages/itwinui-react/src/core/Overlay/Overlay.tsx
@@ -2,8 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export { ProgressLinear } from './ProgressLinear/index.js';
+import { polymorphic } from '../utils/index.js';
 
-export { ProgressRadial } from './ProgressRadial/index.js';
-
-export { Overlay } from '../Overlay/index.js';
+export const Overlay = polymorphic('iui-progress-indicator-overlay');
+Overlay.displayName = 'Overlay';
