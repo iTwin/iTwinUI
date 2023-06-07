7e18d3c0fb36fca184f2d7aebb146719bd9ead49
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-07 11:52:14 -0400

    move logic into Dialog

--- packages/itwinui-react/src/core/Modal/Modal.tsx
+++ packages/itwinui-react/src/core/Modal/Modal.tsx
@@ -3,8 +3,6 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { useGlobals, getDocument, useIsClient } from '../utils/index.js';
 import type { PolymorphicForwardRefComponent } from '../utils/index.js';
 import { Dialog } from '../Dialog/index.js';
 import type { DialogMainProps } from '../Dialog/DialogMain.js';
@@ -38,7 +36,7 @@ type ModalProps = {
    */
   onKeyDown?: React.KeyboardEventHandler;
   /**
-   * If true, the dialog be portaled into a <div> inside the nearest ThemeProvider.
+   * If true, the dialog will be portaled into a <div> inside the nearest `ThemeProvider`.
    *
    * Can be set to an object with a `to` property to portal into a specific element.
    *
@@ -85,37 +83,25 @@ export const Modal = React.forwardRef((props, forwardedRef) => {
     ...rest
   } = props;
 
-  const context = useGlobals();
-  const isClient = useIsClient();
-
-  const portalTo =
-    typeof portal !== 'boolean'
-      ? portal.to
-      : context?.portalContainerRef?.current ?? getDocument()?.body;
-
-  return isClient && portalTo ? (
-    ReactDOM.createPortal(
-      <Dialog
-        isOpen={isOpen}
-        closeOnEsc={closeOnEsc}
-        closeOnExternalClick={closeOnExternalClick}
-        isDismissible={isDismissible}
-        onClose={onClose}
-        preventDocumentScroll
-        trapFocus
-        setFocus
-        ref={forwardedRef}
-      >
-        <Dialog.Backdrop />
-        <Dialog.Main aria-modal {...rest}>
-          <Dialog.TitleBar titleText={title} />
-          {children}
-        </Dialog.Main>
-      </Dialog>,
-      portalTo,
-    )
-  ) : (
-    <></>
+  return (
+    <Dialog
+      isOpen={isOpen}
+      closeOnEsc={closeOnEsc}
+      closeOnExternalClick={closeOnExternalClick}
+      isDismissible={isDismissible}
+      onClose={onClose}
+      preventDocumentScroll
+      trapFocus
+      setFocus
+      ref={forwardedRef}
+      portal={portal}
+    >
+      <Dialog.Backdrop />
+      <Dialog.Main aria-modal {...rest}>
+        <Dialog.TitleBar titleText={title} />
+        {children}
+      </Dialog.Main>
+    </Dialog>
   );
 }) as PolymorphicForwardRefComponent<'div', ModalProps>;
 
