810f62c556c3bab7f6ba1afae405fdce9404147c
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-12 16:39:03 -0400

    revert playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,29 +1,9 @@
-import * as React from 'react';
-import { Button, useToaster } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
-  const toaster = useToaster();
-
-  React.useEffect(() => {
-    toaster.setSettings({ placement: 'bottom-start' });
-  }, []);
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toaster.positive('hello!', {
-            hasCloseButton: true,
-            link: {
-              title: 'ur mum',
-              onClick: () => console.log('hi'),
-            },
-          })
-        }
-      >
-        Hello world
-      </Button>
-      <Button onClick={() => toaster.closeAll()}>Close all</Button>
+      <Button>Hello world</Button>
     </>
   );
 };
