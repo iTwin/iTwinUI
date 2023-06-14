75452732f6eebc1c4bb2d800364d5522ffed6bc3
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-14 10:11:52 -0400

    remove dead code

--- packages/itwinui-react/src/core/utils/functions/dom.ts
+++ packages/itwinui-react/src/core/utils/functions/dom.ts
@@ -3,31 +3,6 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import styles from '../../../styles.js';
-
-/**
- * Get the container as a child of body, or create one if it doesn't exist.
- * Mostly used for dynamic components like Modal or Toast.
- *
- * @param containerId id of the container to find or create
- * @param ownerDocument Can be changed if the container should be in a different document (e.g. in popup).
- */
-export const getContainer = (
-  containerId: string,
-  ownerDocument = getDocument(),
-) => {
-  let container = ownerDocument?.getElementById(containerId) ?? undefined;
-  if (container == null && !!ownerDocument) {
-    container = ownerDocument.createElement('div');
-    container.setAttribute('id', containerId);
-    const root =
-      ownerDocument.querySelector(`.${styles['iui-root']}`) ??
-      ownerDocument.body;
-    root.appendChild(container);
-  }
-  return container;
-};
-
 /**
  * Get document if it is defined.
  * Used to support SSR/SSG applications.
