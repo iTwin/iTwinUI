cd911ce2a6937c682b0dc8aed94e467e1fbf81d6
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-15 08:40:43 -0400

    update some tests

--- packages/itwinui-react/src/core/ProgressIndicators/ProgressRadial/ProgressRadial.test.tsx
+++ packages/itwinui-react/src/core/ProgressIndicators/ProgressRadial/ProgressRadial.test.tsx
@@ -10,112 +10,56 @@ import { ProgressRadial } from './ProgressRadial.js';
 it('renders determinate ProgressRadial', () => {
   const { container } = render(<ProgressRadial value={43} />);
 
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  expect(container.querySelector('.iui-track')).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('50.16');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveClass('iui-determinate');
+  expect(spinner).toHaveStyle('--iui-progress-percentage: 43%');
 });
 
 it('should add className and style props correctly', () => {
   const { container } = render(
     <ProgressRadial className='custom-class' style={{ fill: 'hotpink' }} />,
   );
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.custom-class',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  expect(spinner.style.fill).toEqual('hotpink');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveClass('custom class');
+  expect(spinner).toHaveStyle('fill: hotpink');
 });
 
 it('renders indeterminate ProgressRadial', () => {
   const { container } = render(<ProgressRadial indeterminate={true} />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-indeterminate',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  expect(container.querySelector('.iui-track')).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('0');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveClass('iui-indeterminate');
 });
 
 it('renders positive ProgressRadial', () => {
   const { container } = render(<ProgressRadial status={'positive'} />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate.iui-positive',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('0');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveAttribute('data-iui-status', 'positive');
 });
 
 it('renders negative ProgressRadial', () => {
   const { container } = render(<ProgressRadial value={40} status='negative' />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate.iui-negative',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('52.8');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveStyle('--iui-progress-percentage: 40%');
+  expect(spinner).toHaveAttribute('data-iui-status', 'negative');
 });
 
 it('renders determinate ProgressRadial with max value', () => {
   const { container } = render(<ProgressRadial value={222} />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('0');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveStyle('--iui-progress-percentage: 100%');
 });
 
 it('renders determinate ProgressRadial with min value', () => {
   const { container } = render(<ProgressRadial value={-11} />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('88');
+  const spinner = container.querySelector('.iui-progress-indicator-radial');
+  expect(spinner).toHaveStyle('--iui-progress-percentage: 0%');
 });
 
-it('renders determinate ProgressRadial with no value', () => {
-  const { container } = render(<ProgressRadial />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('88');
-});
-
-it('renders determinate small  ProgressRadial', () => {
-  const { container } = render(<ProgressRadial size={'small'} value={50} />);
-
-  const spinner = container.querySelector(
-    '.iui-progress-indicator-radial.iui-determinate.iui-small',
-  ) as HTMLElement;
-  expect(spinner).toBeTruthy();
-  const spinnerFill = container.querySelector('.iui-fill') as SVGCircleElement;
-  expect(spinnerFill.style.strokeDashoffset).toBe('44');
-});
-
-it('should render all sizes of ProgressRadial', () => {
-  const sizes = ['small', 'x-small', 'large'] as const;
-
-  sizes.forEach((size) => {
+it.each(['small', 'x-small', 'large'] as const)(
+  'should render %s size of ProgressRadial',
+  (size) => {
     const { container } = render(<ProgressRadial size={size} />);
-    const spinner = container.querySelector(
-      `.iui-progress-indicator-radial.iui-determinate.iui-${size}`,
-    ) as HTMLElement;
-    expect(spinner).toBeTruthy();
-  });
-});
+    const spinner = container.querySelector('.iui-progress-indicator-radial');
+    expect(spinner).toHaveAttribute('data-iui-size', size);
+  },
+);
