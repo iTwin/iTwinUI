80ae22da48a689d63a2aee829a33d01a88b95f9b
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-06-16 16:00:09 -0400

    deleted vestigial test

--- examples/Alert.informationalComponent.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Component from './Alert.informational';
-
-describe('<Component />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Component />);
-  });
-});
