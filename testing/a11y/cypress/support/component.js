edb271dcadf1c8bdf4f2a3640586565f9fb74088
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-06-16 11:59:34 -0400

    testing node_modules refresh

--- testing/a11y/cypress/support/component.js
+++ testing/a11y/cypress/support/component.js
@@ -32,5 +32,5 @@ Cypress.Commands.add('mount', mount);
 
 // Importing CSS
 
-// import '@itwin/itwinui-css/global';
-// import '@itwin/itwinui-variables';
+import '@itwin/itwinui-css/global';
+import '@itwin/itwinui-variables';
