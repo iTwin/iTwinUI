65a17bdb7bd2e6553047e72fdef96b3865274712
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-06-20 16:16:34 -0400

    redundant spec deleted

--- testing/a11y/src/components.spec.tsx
+++
@@ -1,9 +0,0 @@
-// import * as React from 'react';
-// import * as allExamples from 'examples';
-// /*Other imports */
-
-// /*Test */
-// it ('mounts', () => {
-//   cy.mount(<Alert/>)
-//   cy.should('be.visible')
-// })
