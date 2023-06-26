c547322ebf623600b6d0be81a80f1051bad1f8df
Greta <83585998+gretanausedaite@users.noreply.github.com>
2023-06-26 09:43:06 +0300

    Adding a bit of new code

--- packages/itwinui-react/src/core/utils/components/Popover.tsx
+++ packages/itwinui-react/src/core/utils/components/Popover.tsx
@@ -4,13 +4,79 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as React from 'react';
-import cx from 'classnames';
-import Tippy from '@tippyjs/react';
+// import cx from 'classnames';
+// import Tippy from '@tippyjs/react';
 import type { TippyProps } from '@tippyjs/react';
 import type { Placement, Instance } from 'tippy.js';
-import { useMergedRefs, useIsClient } from '../hooks/index.js';
+// import { useMergedRefs, useIsClient } from '../hooks/index.js';
 export type PopoverInstance = Instance;
-import { ThemeContext } from '../../ThemeProvider/ThemeContext.js';
+// import { ThemeContext } from '../../ThemeProvider/ThemeContext.js';
+// import {
+//   useFloating,
+//   useClick,
+//   useDismiss,
+//   useRole,
+//   useInteractions,
+// } from '@floating-ui/react';
+// // import ReactDOM from 'react-dom';
+
+// type PopoverOptions = {
+//   initialOpen?: boolean;
+//   placement?: Placement;
+//   modal?: boolean;
+//   open?: boolean;
+//   onOpenChange?: (open: boolean) => void;
+// };
+
+// function usePopover({
+//   initialOpen = false,
+//   // placement = 'bottom',
+//   modal,
+//   open: controlledOpen,
+//   onOpenChange: setControlledOpen,
+// }: PopoverOptions = {}) {
+//   const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
+//   const [labelId, setLabelId] = React.useState<string | undefined>();
+//   const [descriptionId, setDescriptionId] = React.useState<
+//     string | undefined
+//   >();
+
+//   const open = controlledOpen ?? uncontrolledOpen;
+//   const setOpen = setControlledOpen ?? setUncontrolledOpen;
+
+//   const data = useFloating({
+//     // placement,
+//     open,
+//     onOpenChange: setOpen,
+//     // whileElementsMounted: autoUpdate,
+//     middleware: [],
+//   });
+
+//   const context = data.context;
+
+//   const click = useClick(context, {
+//     enabled: controlledOpen == null,
+//   });
+//   const dismiss = useDismiss(context);
+//   const role = useRole(context);
+
+//   const interactions = useInteractions([click, dismiss, role]);
+
+//   return React.useMemo(
+//     () => ({
+//       open,
+//       setOpen,
+//       ...interactions,
+//       ...data,
+//       modal,
+//       labelId,
+//       descriptionId,
+//       setLabelId,
+//       setDescriptionId,
+//     }),
+//     [open, setOpen, interactions, data, modal, labelId, descriptionId],
+//   );
+// }
 
 export type PopoverProps = {
   /**
@@ -36,85 +102,45 @@ export type PopoverProps = {
  * with pre-configured props and plugins (e.g. lazy mounting, focus, etc).
  * @private
  */
-export const Popover = React.forwardRef((props: PopoverProps, ref) => {
-  const [mounted, setMounted] = React.useState(false);
-  const themeInfo = React.useContext(ThemeContext);
-  const isDomAvailable = useIsClient();
+// export const Popover = React.forwardRef((props: PopoverProps, ref) => {
+export const Popover = React.forwardRef(() => {
+  // const { children } = props;
+  // const [mounted, setMounted] = React.useState(false);
+  // const themeInfo = React.useContext(ThemeContext);
+  // const isDomAvailable = useIsClient();
 
-  const tippyRef = React.useRef<Element>(null);
-  const refs = useMergedRefs(tippyRef, ref);
+  // const popover = usePopover();
 
-  // Plugin to allow lazy mounting. See https://github.com/atomiks/tippyjs-react#lazy-mounting
-  const lazyLoad = {
-    fn: () => ({
-      onShow: () => setMounted(true),
-      onHidden: () => setMounted(false),
-    }),
-  };
+  // const tippyRef = React.useRef<Element>(null);
+  // const refs = useMergedRefs(tippyRef, ref);
 
-  // Plugin to remove tabindex from tippy, to prevent focus ring from unintentionally showing.
-  const removeTabIndex = {
-    fn: () => ({
-      onCreate: (instance: Instance) => {
-        instance.popper.firstElementChild?.removeAttribute('tabindex');
-      },
-    }),
-  };
+  // // Plugin to remove tabindex from tippy, to prevent focus ring from unintentionally showing.
+  // const removeTabIndex = {
+  //   fn: () => ({
+  //     onCreate: (instance: Instance) => {
+  //       instance.popper.firstElementChild?.removeAttribute('tabindex');
+  //     },
+  //   }),
+  // };
 
-  const computedProps: Partial<TippyProps> = {
-    allowHTML: true,
-    animation: false,
-    appendTo: (el) =>
-      themeInfo?.portalContainerRef?.current || el.ownerDocument.body,
-    arrow: false,
-    duration: 0,
-    interactive: true,
-    role: '',
-    offset: [0, 0],
-    maxWidth: '',
-    zIndex: 99999,
-    ...props,
-    className: cx('iui-popover', props.className),
-    // add additional check for isDomAvailable when using in controlled mode,
-    // because rootRef is not available in first render
-    visible:
-      props.visible !== undefined ? props.visible && isDomAvailable : undefined,
-    plugins: [
-      lazyLoad,
-      removeTabIndex,
-      hideOnEscOrTab,
-      ...(props.plugins || []),
-    ],
-    popperOptions: {
-      strategy: 'fixed',
-      ...props.popperOptions,
-      modifiers: [
-        { name: 'flip' },
-        { name: 'preventOverflow', options: { padding: 0 } },
-        ...(props.popperOptions?.modifiers || []),
-      ],
-    },
-  };
-
-  if (props.render) {
-    const render = props.render;
-    computedProps.render = (...args) => (mounted ? render(...args) : '');
-  } else {
-    // Fixing issue where elements below Popover gets click events.
-    // Tippy uses react Portal, which propagates events by react tree, not dom tree.
-    // Read more: https://reactjs.org/docs/portals.html#event-bubbling-through-portals
-    const clonedContent = React.isValidElement(props.content)
-      ? React.cloneElement(props.content, {
-          onClick: (e: MouseEvent) => {
-            e.stopPropagation();
-            (props.content as JSX.Element).props.onClick?.(e);
-          },
-        })
-      : props.content;
-    computedProps.content = mounted ? clonedContent : '';
-  }
-
-  return <Tippy {...computedProps} ref={refs} />;
+  return (
+    <>
+      {/* {React.isValidElement(children)
+        ? React.cloneElement(
+            children,
+            popover.getReferenceProps({
+              ref: popover.refs.setReference,
+              ...children.props,
+            }),
+          )
+        : null} */}
+      {/* {popover.open
+        ? portalTo
+          ? ReactDOM.createPortal(contentBox, portalTo)
+          : contentBox
+        : null} */}
+    </>
+  );
 });
 
 /**
