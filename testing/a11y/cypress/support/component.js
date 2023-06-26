48170c2e8e19f0b2965d873a79977c203cc163d7
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-06-26 15:35:40 -0400

    fixed index.html and component.js

--- testing/a11y/cypress/support/component.js
+++ testing/a11y/cypress/support/component.js
@@ -20,18 +20,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
 import { mount } from 'cypress/react18';
 
 Cypress.Commands.add('mount', mount);
 
-// Example use:
-// cy.mount(<MyComponent />)\
-
-// Importing CSS
-
-import '@itwin/itwinui-css';
 import '@itwin/itwinui-react/styles.css';
-import '@itwin/itwinui-variables';
+
+import 'cypress-axe';
