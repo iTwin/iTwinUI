d1dc494c2378cab56ee04e6c2ba360bb0430a244
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-06-29 16:11:11 -0400

    Component accessibility testing (#1352)
    Co-authored-by: Mayank <9084735+mayank99@users.noreply.github.com>

--- examples/ComboBox.disabled.tsx
+++ examples/ComboBox.disabled.tsx
@@ -3,7 +3,8 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
-import { ComboBox, SelectOption } from '@itwin/itwinui-react';
+import type { SelectOption } from '@itwin/itwinui-react';
+import { ComboBox } from '@itwin/itwinui-react';
 
 export default () => {
   const [value, setValue] = React.useState('');
