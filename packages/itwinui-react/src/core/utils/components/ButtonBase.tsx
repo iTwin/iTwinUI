95762571d608af8d4290e8dc015fbdf217ae86b4
Mayank <9084735+mayank99@users.noreply.github.com>
2023-06-29 17:12:20 -0400

    create ButtonBase

---
+++ packages/itwinui-react/src/core/utils/components/ButtonBase.tsx
@@ -0,0 +1,29 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { Box } from './Box.js';
+import type { PolymorphicForwardRefComponent } from '../props.js';
+
+export const ButtonBase = React.forwardRef((props, forwardedRef) => {
+  const { onClick: onClickProp, ...rest } = props;
+
+  return (
+    <Box
+      as='button'
+      type={props.as === 'button' ? 'button' : undefined}
+      ref={forwardedRef}
+      onClick={(e) => {
+        if (!props.disabled) {
+          onClickProp?.(e);
+        }
+      }}
+      aria-disabled={
+        props.as === 'button' && props.disabled ? 'true' : undefined
+      }
+      {...rest}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'button'>;
+ButtonBase.displayName = 'ButtonBase';
