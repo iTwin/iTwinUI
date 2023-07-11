dba07c5136c87433a54458c2a434e9d2f68e334b
Mayank <9084735+mayank99@users.noreply.github.com>
2023-07-11 21:07:32 +0000

    jest workaround?

--- packages/itwinui-react/src/core/utils/functions/import.ts
+++ packages/itwinui-react/src/core/utils/functions/import.ts
@@ -3,9 +3,10 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-// work around for typescript trying to transpile dynamic import in CJS environment
-// see https://github.com/TypeStrong/ts-node/discussions/1290
-export const dynamicImport = new Function(
-  'specifier',
-  'return import(specifier)',
-);
+// workarounds for:
+// (1) typescript trying to transpile dynamic import in CJS environment (https://github.com/TypeStrong/ts-node/discussions/1290)
+// (2) jest causing segfault when encountering dynamic import (https://github.com/nodejs/node/issues/35889)
+export const dynamicImport =
+  typeof jest === undefined
+    ? new Function('specifier', 'return import(specifier)')
+    : (specifier: string) => import(specifier);
