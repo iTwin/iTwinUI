0a58db98690c5be26f9bde8d12b10fe4df81f49d
Mayank <9084735+mayank99@users.noreply.github.com>
2023-07-19 08:08:41 -0400

    add check for `isDirectory`

--- packages/itwinui-react/scripts/generateStyles.mjs
+++ packages/itwinui-react/scripts/generateStyles.mjs
@@ -15,6 +15,16 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const readPackageOnDisk = (pkg) =>
   fs.promises.readFile(require.resolve(pkg), 'utf-8');
 
+const removeIfExists = async (_path) => {
+  if (fs.existsSync(_path)) {
+    if (fs.lstatSync(_path).isDirectory()) {
+      await fs.promises.rmdir(_path);
+    } else {
+      await fs.promises.unlink(_path);
+    }
+  }
+};
+
 const rawCssText =
   '@charset "utf-8";\n' +
   (await readPackageOnDisk('@itwin/itwinui-variables')) +
@@ -37,18 +47,13 @@ if (!fs.existsSync(outCjsDir)) {
   await fs.promises.mkdir(outCjsDir);
 }
 
-if (fs.existsSync(outEsmPath)) {
-  await fs.promises.unlink(outEsmPath);
-}
-
-if (fs.existsSync(outCjsPath)) {
-  await fs.promises.unlink(outCjsPath);
-}
-
+await removeIfExists(outEsmPath);
 await fs.promises.writeFile(
   outEsmPath,
   `${copyrightBannerJs}\nexport default String.raw\`${rawCssText}\`;`,
 );
+
+await removeIfExists(outCjsPath);
 await fs.promises.writeFile(
   outCjsPath,
   `${copyrightBannerJs}\nmodule.exports=String.raw\`${rawCssText}\`;`,
