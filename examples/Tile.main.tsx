66e9dea358d34557a32278887a056d2f07541042
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-08-14 17:57:15 -0400

    Added aria-label to Tile.IconButton

--- examples/Tile.main.tsx
+++ examples/Tile.main.tsx
@@ -13,12 +13,12 @@ export default () => {
       <Tile.ThumbnailArea>
         <Tile.ThumbnailPicture url='https://itwinplatformcdn.azureedge.net/iTwinUI/stadium.png' />
         <Tile.TypeIndicator>
-          <Tile.IconButton>
+          <Tile.IconButton aria-label='Favorite'>
             <SvgStar />
           </Tile.IconButton>
         </Tile.TypeIndicator>
         <Tile.QuickAction>
-          <Tile.IconButton>
+          <Tile.IconButton aria-label='More info'>
             <SvgInfo />
           </Tile.IconButton>
         </Tile.QuickAction>
