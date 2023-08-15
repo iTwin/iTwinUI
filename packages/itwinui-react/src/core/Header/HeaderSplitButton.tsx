6d9635952406a2d3a166d92c255836648560c4db
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-08-15 13:18:58 -0400

    button label

--- packages/itwinui-react/src/core/Header/HeaderSplitButton.tsx
+++ packages/itwinui-react/src/core/Header/HeaderSplitButton.tsx
@@ -52,6 +52,7 @@ export const HeaderSplitButton = React.forwardRef((props, forwardedRef) => {
         onHide={React.useCallback(() => setIsMenuOpen(false), [])}
       >
         <ButtonBase
+          aria-label='dropdown menu button'
           className='iui-header-breadcrumb-button iui-header-breadcrumb-button-split'
           disabled={disabled}
         >
