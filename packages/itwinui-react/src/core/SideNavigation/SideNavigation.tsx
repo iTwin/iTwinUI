1ec314809b80b98500a62d0fdbea072a03a64225
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-08-15 16:31:19 -0400

    accessible label added to submenu expand button

--- packages/itwinui-react/src/core/SideNavigation/SideNavigation.tsx
+++ packages/itwinui-react/src/core/SideNavigation/SideNavigation.tsx
@@ -113,6 +113,7 @@ export const SideNavigation = React.forwardRef((props, forwardedRef) => {
 
   const ExpandButton = (
     <IconButton
+      label='Expand or collapse submenu'
       className='iui-sidenav-button iui-expand'
       onClick={React.useCallback(() => {
         _setIsExpanded((expanded) => !expanded);
