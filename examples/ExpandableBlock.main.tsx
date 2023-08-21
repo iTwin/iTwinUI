3e65703730591ffe57147e00884f2d8b73a8a3d7
Mayank <9084735+mayank99@users.noreply.github.com>
2023-08-21 15:56:51 -0400

    update docs

--- examples/ExpandableBlock.main.tsx
+++ examples/ExpandableBlock.main.tsx
@@ -8,10 +8,9 @@ import { ExpandableBlock } from '@itwin/itwinui-react';
 export default () => {
   return (
     <div style={{ width: 'min(100%, 300px)' }}>
-      <ExpandableBlock.Wrapper>
-        <ExpandableBlock.Trigger label='Expandable Block' />
-        <ExpandableBlock.Content>Content in block!</ExpandableBlock.Content>
-      </ExpandableBlock.Wrapper>
+      <ExpandableBlock title='Expandable Block'>
+        Content in block!
+      </ExpandableBlock>
     </div>
   );
 };
