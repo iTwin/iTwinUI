22303571c3ef9c4b04c18c53a272183c95b10003
Mayank <9084735+mayank99@users.noreply.github.com>
2023-08-28 13:15:37 -0400

    fix unit tests

--- packages/itwinui-react/src/core/Tooltip/Tooltip.test.tsx
+++ packages/itwinui-react/src/core/Tooltip/Tooltip.test.tsx
@@ -15,16 +15,16 @@ it('should toggle the visibility of tooltip on hover', async () => {
     </Tooltip>,
   );
 
-  expect(queryByText('some text')).toBeNull();
+  expect(queryByText('some text')).not.toBeVisible();
 
   fireEvent.mouseEnter(getByText('Hover Here'));
   act(() => void jest.advanceTimersByTime(50));
-  getByText('some text');
+  expect(getByText('some text')).toBeVisible();
 
   fireEvent.mouseLeave(getByText('Hover Here'));
   act(() => void jest.advanceTimersByTime(250));
 
-  expect(queryByText('some text')).not.toBeInTheDocument();
+  expect(queryByText('some text')).not.toBeVisible();
 
   jest.useRealTimers();
 });
@@ -39,14 +39,22 @@ it('should be visible', () => {
   getByText('some text');
 });
 
-it('should not be visible', () => {
-  const { queryByText } = render(
+it('should respect visible prop', () => {
+  const { queryByText, rerender } = render(
     <Tooltip content='some text' visible={false}>
       <div>Visible!</div>
     </Tooltip>,
   );
 
-  expect(queryByText('some text')).toBeNull();
+  expect(queryByText('some text')).not.toBeVisible();
+
+  rerender(
+    <Tooltip content='some text' visible>
+      <div>Not visible!</div>
+    </Tooltip>,
+  );
+
+  expect(queryByText('some text')).toBeVisible();
 });
 
 it('should allow button clicks and hovers', () => {
