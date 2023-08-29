756ed89d9bc7e69072095452f84f25c3a6b3d81f
Mayank <9084735+mayank99@users.noreply.github.com>
2023-08-29 12:08:20 -0400

    revert playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,14 +1,9 @@
-import { Button, Tooltip } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
   return (
     <>
-      <Tooltip
-        content='or not'
-        portal={{ to: () => document.querySelector('main') }}
-      >
-        <Button>Hello world</Button>
-      </Tooltip>
+      <Button>Hello world</Button>
     </>
   );
 };
