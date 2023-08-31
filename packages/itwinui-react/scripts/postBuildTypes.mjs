9ab12fad07dd2584de809b47c38017d0ba7aa5ba
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-08-31 11:16:05 -0400

    Removed code duplication

--- packages/itwinui-react/scripts/postBuildTypes.mjs
+++ packages/itwinui-react/scripts/postBuildTypes.mjs
@@ -4,38 +4,22 @@
  *--------------------------------------------------------------------------------------------*/
 import { exec } from 'node:child_process';
 
-// Since tsc does not include the react-table-types.d.ts file in the esm build,
+// Since tsc does not include the react-table-types.d.ts file in the cjs and esm builds,
 // we need to copy it manually.
-exec(
-  'copyfiles -u 1 src/core/Table/types/react-table-types.d.ts esm',
-  (error) => {
-    if (error) {
-      console.error(
-        'Error copying react-table-types.d.ts to esm build folder',
-        error,
-      );
-    } else {
-      console.log(
-        '✓ Finished copying react-table-types.d.ts to esm build folder',
-      );
-    }
-  },
-);
-
-// Since tsc does not include the react-table-types.d.ts file in the cjs build,
-// we need to copy it manually.
-exec(
-  'copyfiles -u 1 src/core/Table/types/react-table-types.d.ts cjs',
-  (error) => {
-    if (error) {
-      console.error(
-        'Error copying react-table-types.d.ts to cjs build folder',
-        error,
-      );
-    } else {
-      console.log(
-        '✓ Finished copying react-table-types.d.ts to cjs build folder',
-      );
-    }
-  },
-);
+['cjs', 'esm'].forEach((buildType) => {
+  exec(
+    `copyfiles -u 1 src/core/Table/types/react-table-types.d.ts ${buildType}`,
+    (error) => {
+      if (error) {
+        console.error(
+          `Error copying react-table-types.d.ts to ${buildType} build folder`,
+          error,
+        );
+      } else {
+        console.log(
+          `✓ Finished copying react-table-types.d.ts to ${buildType} build folder`,
+        );
+      }
+    },
+  );
+});
