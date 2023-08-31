c4ad330df2cda72fb646fbc5fa7829eaa958e88e
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-08-31 11:20:55 -0400

    Removed unnecessary comments

--- packages/itwinui-react/src/core/Table/types/index.ts
+++ packages/itwinui-react/src/core/Table/types/index.ts
@@ -2,16 +2,4 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-// import {
-//   // TableInterface,
-//   type TableBodyPropGetter,
-// } from './react-table-types.js';
-
-// import everything from './react-table-types.d.ts' and put it in a variable called TableTypes
 export type * as TableTypes from './react-table-types.js';
-// export { TableTypes };
-// export { TableTypes } from './react-table-types.js';
-// export * from './testing.js';
-
-// const testing: ReactTableTypes.TableBodyPropGetter<object> | undefined = undefined;
-// console.log(testing ?? 0 + 1);
