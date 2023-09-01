2dfb3bd67672ce38fc9d63002346aa072b733967
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2023-09-01 14:40:50 -0400

    Updating tests.

--- packages/itwinui-css/backstop/scenarios/slider.js
+++ packages/itwinui-css/backstop/scenarios/slider.js
@@ -6,27 +6,25 @@ const { scenario, hover, focus } = require('../scenarioHelper');
 
 module.exports = [
   // Slider types
-  scenario('Type Default', {
-    selectors: ['#demo-default'],
+  scenario('Type Horizontal', {
+    selectors: ['#demo-horizontal'],
     viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('Type Range', {
-    selectors: ['#demo-range'],
+  scenario('Type Horizontal extras', {
+    selectors: ['#demo-horizontal-extras'],
     viewports: [{ width: 800, height: 600 }],
   }),
-  scenario('Type Default Vertical', {
-    selectors: ['#demo-default-vertical'],
-    viewports: [{ width: 800, height: 600 }],
+  scenario('Type Vertical', {
+    selectors: ['#demo-vertical'],
   }),
-  scenario('Type Range Vertical', {
-    selectors: ['#demo-range-vertical'],
-    viewports: [{ width: 800, height: 600 }],
+  scenario('Type Vertical extras', {
+    selectors: ['#demo-vertical-extras'],
   }),
 
   // Hover & focus states
   scenario('State hover & focus', {
     actions: [hover('#test-slider-2'), focus('#test-slider-1')],
-    selectors: ['#demo-default'],
+    selectors: ['#demo-horizontal'],
     viewports: [{ width: 800, height: 600 }],
   }),
 ];
