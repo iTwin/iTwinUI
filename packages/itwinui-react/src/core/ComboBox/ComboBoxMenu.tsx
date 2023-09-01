a02e1bfa02a47f6de483c240f21b9bd918533b61
Mayank <9084735+mayank99@users.noreply.github.com>
2023-09-01 14:55:32 -0400

    fix virtual scroll

--- packages/itwinui-react/src/core/ComboBox/ComboBoxMenu.tsx
+++ packages/itwinui-react/src/core/ComboBox/ComboBoxMenu.tsx
@@ -5,14 +5,13 @@
 import cx from 'classnames';
 import * as React from 'react';
 import { Menu } from '../Menu/index.js';
-import { Surface } from '../Surface/index.js';
 import {
   useSafeContext,
   useMergedRefs,
   useVirtualization,
-  mergeRefs,
   getWindow,
   Portal,
+  Box,
 } from '../utils/index.js';
 import type { PolymorphicForwardRefComponent } from '../utils/index.js';
 import { ComboBoxStateContext, ComboBoxRefsContext } from './helpers.js';
@@ -26,105 +25,91 @@ type ComboBoxMenuProps = Omit<
 const isOverflowOverlaySupported = () =>
   getWindow()?.CSS?.supports?.('overflow: overlay');
 
-const VirtualizedComboBoxMenu = React.forwardRef(
-  (props: ComboBoxMenuProps, forwardedRef: React.Ref<HTMLElement>) => {
-    const { children, className, style, ...rest } = props;
-    const { id, filteredOptions, getMenuItem, focusedIndex } =
-      useSafeContext(ComboBoxStateContext);
-    const { menuRef } = useSafeContext(ComboBoxRefsContext);
+const VirtualizedComboBoxMenu = (props: React.ComponentProps<'div'>) => {
+  const { children, ...rest } = props;
+  const { filteredOptions, getMenuItem, focusedIndex } =
+    useSafeContext(ComboBoxStateContext);
+  const { menuRef } = useSafeContext(ComboBoxRefsContext);
 
-    const virtualItemRenderer = React.useCallback(
-      (index: number) =>
-        filteredOptions.length > 0
-          ? getMenuItem(filteredOptions[index], index)
-          : (children as JSX.Element), // Here is expected empty state content
-      [filteredOptions, getMenuItem, children],
+  const virtualItemRenderer = React.useCallback(
+    (index: number) =>
+      filteredOptions.length > 0
+        ? getMenuItem(filteredOptions[index], index)
+        : (children as JSX.Element), // Here is expected empty state content
+    [filteredOptions, getMenuItem, children],
+  );
+
+  const focusedVisibleIndex = React.useMemo(() => {
+    const currentElement = menuRef.current?.querySelector(
+      `[data-iui-index="${focusedIndex}"]`,
     );
+    if (!currentElement) {
+      return focusedIndex;
+    }
 
-    const focusedVisibleIndex = React.useMemo(() => {
-      const currentElement = menuRef.current?.querySelector(
-        `[data-iui-index="${focusedIndex}"]`,
-      );
-      if (!currentElement) {
-        return focusedIndex;
-      }
-
-      return Number(
-        currentElement.getAttribute('data-iui-filtered-index') ?? focusedIndex,
-      );
-    }, [focusedIndex, menuRef]);
-
-    const { outerProps, innerProps, visibleChildren } = useVirtualization({
-      // 'Fool' VirtualScroll by passing length 1
-      // whenever there is no elements, to show empty state message
-      itemsLength: filteredOptions.length || 1,
-      itemRenderer: virtualItemRenderer,
-      scrollToIndex: focusedVisibleIndex,
-    });
-
-    const surfaceStyles = {
-      // set as constant because we don't want it shifting when items are unmounted
-      maxInlineSize: 0,
-
-      // max-height must be on the outermost element for virtual scroll
-      maxBlockSize: 'calc((var(--iui-component-height) - 1px) * 8.5)',
-      overflowY: isOverflowOverlaySupported() ? 'overlay' : 'auto',
-      ...style,
-    } as React.CSSProperties;
-
-    return (
-      <Surface style={surfaceStyles} {...outerProps}>
-        <Menu
-          id={`${id}-list`}
-          setFocus={false}
-          role='listbox'
-          ref={mergeRefs(menuRef, innerProps.ref, forwardedRef)}
-          className={className}
-          style={innerProps.style}
-          {...rest}
-        >
-          {visibleChildren}
-        </Menu>
-      </Surface>
+    return Number(
+      currentElement.getAttribute('data-iui-filtered-index') ?? focusedIndex,
     );
-  },
-);
+  }, [focusedIndex, menuRef]);
+
+  const { outerProps, innerProps, visibleChildren } = useVirtualization({
+    // 'Fool' VirtualScroll by passing length 1
+    // whenever there is no elements, to show empty state message
+    itemsLength: filteredOptions.length || 1,
+    itemRenderer: virtualItemRenderer,
+    scrollToIndex: focusedVisibleIndex,
+  });
+
+  return (
+    <Box as='div' {...outerProps} {...rest}>
+      <div {...innerProps} ref={innerProps.ref}>
+        {visibleChildren}
+      </div>
+    </Box>
+  );
+};
 
 export const ComboBoxMenu = React.forwardRef((props, forwardedRef) => {
-  const { className, children, ...rest } = props;
+  const { className, children, style, ...rest } = props;
   const { id, enableVirtualization, popover } =
     useSafeContext(ComboBoxStateContext);
   const { menuRef } = useSafeContext(ComboBoxRefsContext);
 
-  const refs = useMergedRefs(
-    popover.refs.setFloating,
-    forwardedRef,
-    enableVirtualization ? null : menuRef,
-  );
+  const refs = useMergedRefs(popover.refs.setFloating, forwardedRef, menuRef);
 
   return (
     popover.open && (
       <Portal portal>
-        {!enableVirtualization ? (
-          <Menu
-            id={`${id}-list`}
-            setFocus={false}
-            role='listbox'
-            ref={refs}
-            className={cx('iui-scroll', className)}
-            {...popover.getFloatingProps(rest)}
-          >
-            {children}
-          </Menu>
-        ) : (
-          <VirtualizedComboBoxMenu
-            as='div'
-            ref={refs}
-            {...popover.getFloatingProps(props)}
-          >
-            {children}
-          </VirtualizedComboBoxMenu>
-        )}
+        <Menu
+          id={`${id}-list`}
+          setFocus={false}
+          role='listbox'
+          ref={refs}
+          className={cx('iui-scroll', className)}
+          {...popover.getFloatingProps({
+            style: !enableVirtualization
+              ? style
+              : ({
+                  // set as constant because we don't want it shifting when items are unmounted
+                  maxInlineSize: 0,
+
+                  // max-height must be on the outermost element for virtual scroll
+                  maxBlockSize:
+                    'calc((var(--iui-component-height) - 1px) * 8.5)',
+                  overflowY: isOverflowOverlaySupported() ? 'overlay' : 'auto',
+                  ...style,
+                  // ...outerProps.style,
+                } as React.CSSProperties),
+            ...rest,
+          })}
+        >
+          {!enableVirtualization ? (
+            children
+          ) : (
+            <VirtualizedComboBoxMenu>{children}</VirtualizedComboBoxMenu>
+          )}
+        </Menu>
+        )
       </Portal>
     )
   );
