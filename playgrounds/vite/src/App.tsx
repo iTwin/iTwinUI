7bb6598c0201ea316a5b9da2f655e866a146ca1c
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-09-05 12:49:25 -0400

    Testing change

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,6 +1,23 @@
-import { Button } from '@itwin/itwinui-react';
+import { Button, Table, TableTypes } from '@itwin/itwinui-react';
 
 const App = () => {
+  const columns: TableTypes.Column<{
+    header1: string;
+    header2: string;
+  }>[] = [
+    {
+      Header: 'Header 1',
+      accessor: 'header1',
+    },
+    {
+      Header: 'Header 2',
+      accessor: 'header2',
+    },
+  ] as TableTypes.Column<{
+    header1: string;
+    header2: string;
+  }>[];
+
   return (
     <>
       <Button>Hello world</Button>
