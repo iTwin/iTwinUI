2c3150234dde397050c5a074d2582de019c9c301
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-09-06 10:09:46 -0400

    Revert playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,12 +1,6 @@
 import { Button } from '@itwin/itwinui-react';
-import { TableTrial } from '@itwin/itwinui-react/table';
-// import { Anchor } from '@itwin/itwinui-react/cjs';
-// import { Badge } from '@itwin/itwinui-react/esm';
-// import {  } from '@itwin/itwinui-react/table';
 
 const App = () => {
-  console.log(TableTrial);
-
   return (
     <>
       <Button>Hello world</Button>
