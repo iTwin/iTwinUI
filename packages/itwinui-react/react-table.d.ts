ddce4b56827709a6b093bc9c78ba7f1dd782a717
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-09-06 12:39:00 -0400

    Added the fallback react-table file

---
+++ packages/itwinui-react/react-table.d.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+// This is a fallback for those using old node versions who cannot resolve @itwin/itwinui-react/react-table to the esm/cjs versions.
+export * from './esm/react-table/react-table-types.js';
