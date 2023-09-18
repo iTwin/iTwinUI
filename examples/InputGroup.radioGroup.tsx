ec2d8d51f37c97693660dbe7f799666e3da521a6
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2023-09-18 14:42:42 -0400

    Fixed a11y violations for InputGroup examples (#1482)

--- examples/InputGroup.radioGroup.tsx
+++ examples/InputGroup.radioGroup.tsx
@@ -3,14 +3,13 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { InputGroup, Radio } from '@itwin/itwinui-react';
-import { SvgSmileyHappy, SvgSmileySad } from '@itwin/itwinui-icons-react';
 
 export default () => {
   return (
     <>
       <InputGroup label='Radio group' message='Tell me how happy you are'>
-        <Radio name='choice' value='option1' label={<SvgSmileyHappy />} />
-        <Radio name='choice' value='option2' label={<SvgSmileySad />} />
+        <Radio name='choice' value='option1' label={'Very Happy'} />
+        <Radio name='choice' value='option2' label={'Not Happy'} />
       </InputGroup>
     </>
   );
