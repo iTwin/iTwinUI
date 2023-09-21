6d481eb6950c78616ab136f2149e26f58da80eac
Mayank <9084735+mayank99@users.noreply.github.com>
2023-09-21 16:45:43 -0400

    remove one more index file

--- packages/itwinui-react/src/index.ts
+++ packages/itwinui-react/src/index.ts
@@ -2,4 +2,222 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export * from './core/index.js';
+export { Alert } from './core/Alert/index.js';
+
+export { Avatar } from './core/Avatar/index.js';
+
+export { AvatarGroup } from './core/AvatarGroup/index.js';
+
+export { Backdrop } from './core/Backdrop/index.js';
+
+export { Badge } from './core/Badge/index.js';
+
+export { Breadcrumbs } from './core/Breadcrumbs/index.js';
+
+export {
+  Button,
+  DropdownButton,
+  IconButton,
+  IdeasButton,
+  SplitButton,
+} from './core/Buttons/index.js';
+
+export { ButtonGroup } from './core/ButtonGroup/index.js';
+
+export { Carousel } from './core/Carousel/index.js';
+
+export { Checkbox } from './core/Checkbox/index.js';
+
+export {
+  ColorPicker,
+  ColorSwatch,
+  ColorBuilder,
+  ColorInputPanel,
+  ColorPalette,
+} from './core/ColorPicker/index.js';
+
+export { ComboBox } from './core/ComboBox/index.js';
+
+export {
+  DatePicker,
+  generateLocalizedStrings,
+} from './core/DatePicker/index.js';
+
+export { Dialog } from './core/Dialog/index.js';
+
+export { DropdownMenu } from './core/DropdownMenu/index.js';
+
+export { ErrorPage, NonIdealState } from './core/NonIdealState/index.js';
+export type {
+  ErrorPageType,
+  ErrorTypeTranslations,
+} from './core/NonIdealState/index.js';
+
+export { ExpandableBlock } from './core/ExpandableBlock/index.js';
+
+export { Fieldset } from './core/Fieldset/index.js';
+
+export {
+  FileUpload,
+  FileUploadTemplate,
+  FileUploadCard,
+  FileEmptyCard,
+} from './core/FileUpload/index.js';
+
+export { Footer, defaultFooterElements } from './core/Footer/index.js';
+export type { FooterElement, TitleTranslations } from './core/Footer/index.js';
+
+export {
+  Header,
+  HeaderBreadcrumbs,
+  HeaderButton,
+  HeaderLogo,
+} from './core/Header/index.js';
+
+export { List, ListItem } from './core/List/index.js';
+
+export { TransferList } from './core/TransferList/index.js';
+
+export { Tabs, Tab } from './core/Tabs/index.js';
+
+export {
+  InformationPanel,
+  InformationPanelWrapper,
+  InformationPanelHeader,
+  InformationPanelBody,
+  InformationPanelContent,
+} from './core/InformationPanel/index.js';
+
+export { Input } from './core/Input/index.js';
+
+export { InputWithDecorations } from './core/InputWithDecorations/InputWithDecorations.js';
+
+export { InputGrid } from './core/InputGrid/InputGrid.js';
+
+export { Label } from './core/Label/index.js';
+
+export { LabeledInput } from './core/LabeledInput/index.js';
+
+export { InputGroup } from './core/InputGroup/index.js';
+
+export { LabeledSelect } from './core/LabeledSelect/index.js';
+
+export { LabeledTextarea } from './core/LabeledTextarea/index.js';
+
+export {
+  MenuItem,
+  MenuDivider,
+  MenuExtraContent,
+  MenuItemSkeleton,
+} from './core/Menu/index.js';
+
+export { Modal, ModalButtonBar, ModalContent } from './core/Modal/index.js';
+
+export { NotificationMarker } from './core/NotificationMarker/index.js';
+
+export { Overlay } from './core/Overlay/index.js';
+
+export {
+  ProgressLinear,
+  ProgressRadial,
+} from './core/ProgressIndicators/index.js';
+
+export { Radio } from './core/Radio/index.js';
+
+export { RadioTile, RadioTileGroup } from './core/RadioTiles/index.js';
+
+export { Select } from './core/Select/index.js';
+export type {
+  SelectOption,
+  ItemRendererProps,
+  SelectValueChangeEvent,
+} from './core/Select/index.js';
+
+export {
+  SideNavigation,
+  SidenavButton,
+  SidenavSubmenu,
+  SidenavSubmenuHeader,
+} from './core/SideNavigation/index.js';
+
+export { SkipToContentLink } from './core/SkipToContentLink/index.js';
+
+export { Slider } from './core/Slider/index.js';
+
+export { StatusMessage } from './core/StatusMessage/index.js';
+
+export { Surface } from './core/Surface/index.js';
+
+export {
+  Table,
+  tableFilters,
+  BaseFilter,
+  FilterButtonBar,
+  DefaultCell,
+  EditableCell,
+  TablePaginator,
+  ActionColumn,
+  ExpanderColumn,
+  SelectionColumn,
+} from './core/Table/index.js';
+export type {
+  TableFilterValue,
+  DateRangeFilterOptions,
+  TablePaginatorRendererProps,
+} from './core/Table/index.js';
+
+export { Tag, TagContainer } from './core/Tag/index.js';
+
+export { Textarea } from './core/Textarea/index.js';
+
+export { Tile } from './core/Tile/index.js';
+
+export { TimePicker } from './core/TimePicker/index.js';
+export type { MeridiemType } from './core/TimePicker/index.js';
+
+export { useToaster } from './core/Toast/index.js';
+
+export { ThemeProvider } from './core/ThemeProvider/index.js';
+export type { ThemeType } from './core/ThemeProvider/index.js';
+
+export { ToggleSwitch } from './core/ToggleSwitch/index.js';
+
+export { Tooltip } from './core/Tooltip/index.js';
+
+export { Tree, TreeNode, TreeNodeExpander } from './core/Tree/index.js';
+export type { NodeData, NodeRenderProps } from './core/Tree/index.js';
+
+export {
+  Anchor,
+  Blockquote,
+  Code,
+  Kbd,
+  KbdKeys,
+  Text,
+} from './core/Typography/index.js';
+
+export { Stepper, WorkflowDiagram } from './core/Stepper/index.js';
+export type {
+  StepProperties,
+  StepperLocalization,
+} from './core/Stepper/index.js';
+
+export { SearchBox } from './core/SearchBox/index.js';
+
+export {
+  getUserColor,
+  ColorValue,
+  MiddleTextTruncation,
+} from './core/utils/index.js';
+
+export { LinkBox, LinkAction } from './core/LinkAction/LinkAction.js';
+
+export { VisuallyHidden } from './core/VisuallyHidden/VisuallyHidden.js';
+
+export { Icon } from './core/Icon/Icon.js';
+
+export { Flex } from './core/Flex/Flex.js';
+
+export { Popover } from './core/Popover/Popover.js';
+
+export { Divider } from './core/Divider/Divider.js';
