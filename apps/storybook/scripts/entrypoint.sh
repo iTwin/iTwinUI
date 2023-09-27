c8d99562bcd172939b6096e4914d3108dc235e42
Mayank <9084735+mayank99@users.noreply.github.com>
2023-09-27 13:08:53 -0400

    use npm

--- apps/storybook/scripts/entrypoint.sh
+++ apps/storybook/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # `&` is used to run the process in the background
-yarn serve apps/storybook/storybook-static --listen 6006 &
-yarn workspace storybook cypress run "$@"
+npx serve apps/storybook/storybook-static --listen 6006 &
+npx cypress run "$@" --workspace=storybook
