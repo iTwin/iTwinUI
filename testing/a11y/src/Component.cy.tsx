7c5d0423ac28a3741aa44999391ee3b79d7fffd8
Mayank <9084735+mayank99@users.noreply.github.com>
2023-10-16 17:35:26 -0400

    ignore InputStatusExample in Ci

--- testing/a11y/src/Component.cy.tsx
+++ testing/a11y/src/Component.cy.tsx
@@ -9,12 +9,9 @@ import { ThemeProvider } from '@itwin/itwinui-react';
 // We are disabling few rules on certain elements to ignore false positives.
 const axeConfigPerExample = (example) => {
   switch (example) {
-    // disabled elements do not need to meet contrast requirements
-    case 'TileLoadingExample': {
-      return {
-        rules: [{ id: 'color-contrast', enabled: false }],
-      };
-    }
+    case 'TileLoadingExample': // disabled elements do not need to meet contrast requirements
+    case 'InputStatusExample': // false positive only in CI
+      return { rules: [{ id: 'color-contrast', enabled: false }] };
   }
 };
 
