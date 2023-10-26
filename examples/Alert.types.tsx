9954d8668ab9ea34affa99ae0dc44d1541ffd179
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2023-10-26 10:09:04 -0400

    removed extra examples, updated text.

---
+++ examples/Alert.types.tsx
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { Alert } from '@itwin/itwinui-react';
+
+export default () => {
+  const types = ['positive', 'warning', 'negative'];
+  return (
+    <>
+      {types.map((type, index) => (
+        <Alert.Wrapper
+          key={index}
+          type={type}
+          style={{ minWidth: 'min(100%, 350px)' }}
+        >
+          <Alert.Icon />
+          <Alert.Message>
+            This is a {type} alert
+            <Alert.Action onClick={() => console.log('Clicked more info!')}>
+              Learn more
+            </Alert.Action>
+          </Alert.Message>
+          <Alert.CloseButton onClick={() => console.log('CLOSED')} />
+        </Alert.Wrapper>
+      ))}
+    </>
+  );
+};
