52b491ca97253c4feb2dadbce9ff4b2c9f999a9d
Mayank <9084735+mayank99@users.noreply.github.com>
2023-11-02 18:22:49 -0400

    use `serve` instead of `preview` to get around docker

--- apps/storybook/scripts/entrypoint.sh
+++ apps/storybook/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # `&` is used to run the process in the background
-yarn workspace storybook preview &
+yarn serve apps/storybook/build --listen 6006 -L &
 yarn workspace storybook cypress run "$@"
