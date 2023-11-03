4053c924649f94500f006d705d4791e25ab3aaab
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2023-11-03 17:56:45 -0400

    Updated Alert isSticky Example and some info.

--- examples/Alert.sticky.tsx
+++ examples/Alert.sticky.tsx
@@ -7,7 +7,14 @@ import { Alert } from '@itwin/itwinui-react';
 
 export default () => {
   return (
-    <div>
+    <div
+      style={{
+        height: '150px',
+        overflow: 'overlay',
+        textAlign: 'justify',
+        border: 'solid 0.5px',
+      }}
+    >
       <Alert.Wrapper isSticky>
         <Alert.Icon />
         <Alert.Message>
@@ -18,7 +25,31 @@ export default () => {
         </Alert.Message>
         <Alert.CloseButton onClick={() => console.log('CLOSED')} />
       </Alert.Wrapper>
-      <p>Page content.</p>
+      <p
+        style={{
+          margin: 0,
+          padding: '8px',
+        }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+      </p>
     </div>
   );
 };
