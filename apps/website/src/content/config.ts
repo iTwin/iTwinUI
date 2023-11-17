f98f67f9eef767dc52f0dac82008e117833eb826
Mayank <9084735+mayank99@users.noreply.github.com>
2023-11-17 12:26:21 -0500

    introduce content collections

--- apps/website/src/content/config.ts
+++ apps/website/src/content/config.ts
@@ -2,5 +2,16 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+import { z, defineCollection } from 'astro:content';
+
+const docs = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    // description: z.string().optional(),
+    // thumbnail: z.string().optional(),
+    group: z.string().optional(),
+  }),
+});
+
+export const collections = { docs };
