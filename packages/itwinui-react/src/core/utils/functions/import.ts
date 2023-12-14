ac7bebba7b8ba409f7cc10c768326a75e42a53c9
Mayank <9084735+mayank99@users.noreply.github.com>
2023-12-14 13:53:05 -0500

    remove `dynamicImport` hack

--- packages/itwinui-react/src/core/utils/functions/import.ts
+++ packages/itwinui-react/src/core/utils/functions/import.ts
@@ -4,17 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 /**
- * Wrapper around native `import()` with workarounds for:
- *   1. typescript trying to transpile dynamic import in CJS environment (https://github.com/microsoft/TypeScript/issues/43329)
- *   2. jest/node causing segfault when encountering dynamic import (https://github.com/nodejs/node/issues/35889)
- */
-export const dynamicImport =
-  typeof jest === undefined
-    ? new Function('specifier', 'return import(specifier)')
-    : (specifier: string) =>
-        import(/* webpackIgnore: true */ /* @vite-ignore */ specifier);
-
-/**
  * Wrapper around native CSS module scripts (import attributes) for dynamic imports.
  * In unsupported browsers, it gracefully degrades to import assertions, and then `fetch`.
  *
