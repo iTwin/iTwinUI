e209f9eb1fe407dbeffd6fc65c8d75fe9bb73e2b
Mayank <9084735+mayank99@users.noreply.github.com>
2024-01-03 09:01:52 -0500

    fix lint

--- examples/LinkAction.main.tsx
+++ examples/LinkAction.main.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import { LinkAction, LinkBox, Surface } from '@itwin/itwinui-react';
 
