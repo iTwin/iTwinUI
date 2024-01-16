9f753c4a4d82739e3c63d6d75c5aa7fb7ce2ebbd
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2024-01-16 16:02:22 -0500

    installed vitest and added config file.

---
+++ packages/itwinui-react/vitest.config.ts
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    sequence: {
+      hooks: 'list',
+    },
+    include: ['**/*[.-]{test,spec}.?(c|m)[jt]s?(x)'],
+  },
+});
