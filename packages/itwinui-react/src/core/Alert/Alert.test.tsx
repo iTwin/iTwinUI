9d3ad36cb9a81058f39d07f31fb69872c3818e35
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2024-01-18 09:41:52 -0500

    Added more things inside config file, fixed Alert unit tests.

--- packages/itwinui-react/src/core/Alert/Alert.test.tsx
+++ packages/itwinui-react/src/core/Alert/Alert.test.tsx
@@ -5,6 +5,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import * as React from 'react';
 import { userEvent } from '@testing-library/user-event';
+import { vi } from 'vitest';
 
 import { Alert } from './Alert.js';
 
@@ -14,14 +15,15 @@ it('renders correctly in its default state', () => {
       <Alert.Message>This is an alert.</Alert.Message>
     </Alert.Wrapper>,
   );
-
-  expect(container.querySelector('.iui-alert')).toBeTruthy();
-  expect(container.querySelector('.iui-svg-icon')).toBeFalsy();
-  const message = container.querySelector('.iui-alert-message') as HTMLElement;
+  expect(container.querySelector('._iui3-alert')).toBeTruthy();
+  expect(container.querySelector('._iui3-svg-icon')).toBeFalsy();
+  const message = container.querySelector(
+    '._iui3-alert-message',
+  ) as HTMLElement;
   expect(message).toBeTruthy();
   expect(message.querySelector('a')).toBeNull();
   expect(message.textContent).toBe('This is an alert.');
-  expect(container.querySelector('button > .iui-button-icon')).toBeNull();
+  expect(container.querySelector('button > ._iui3-button-icon')).toBeNull();
 });
 
 it('renders clickable text with href correctly', () => {
@@ -37,9 +39,9 @@ it('renders clickable text with href correctly', () => {
     </Alert.Wrapper>,
   );
 
-  expect(container.querySelector('.iui-alert')).toBeTruthy();
+  expect(container.querySelector('._iui3-alert')).toBeTruthy();
   const link = container.querySelector(
-    '.iui-alert-message > a.iui-alert-link',
+    '._iui3-alert-message > a._iui3-alert-link',
   ) as HTMLAnchorElement;
   expect(link).toBeTruthy();
   expect(link.textContent).toBe('I am a clickable text');
@@ -55,7 +57,7 @@ it('takes a className and style correctly', () => {
     </Alert.Wrapper>,
   );
 
-  const alert = container.querySelector('.iui-alert') as HTMLElement;
+  const alert = container.querySelector('._iui3-alert') as HTMLElement;
   expect(alert).toBeTruthy();
   expect(alert.className).toContain('custom-alert');
   expect(alert.style.width).toBe('100px');
@@ -69,7 +71,7 @@ it('renders sticky alert correctly', () => {
     </Alert.Wrapper>,
   );
 
-  const alert = container.querySelector('.iui-alert') as HTMLElement;
+  const alert = container.querySelector('._iui3-alert') as HTMLElement;
   expect(alert).toBeTruthy();
   expect(alert).toHaveAttribute('data-iui-variant', 'sticky');
   getByText('This is sticky alert.');
@@ -85,10 +87,12 @@ it('renders alert action as button', () => {
     </Alert.Wrapper>,
   );
 
-  const alert = container.querySelector('.iui-alert') as HTMLElement;
+  const alert = container.querySelector('._iui3-alert') as HTMLElement;
   expect(alert).toBeTruthy();
 
-  const alertButton = container.querySelector('.iui-alert-link') as HTMLElement;
+  const alertButton = container.querySelector(
+    '._iui3-alert-link',
+  ) as HTMLElement;
   expect(alertButton).toBeInstanceOf(HTMLButtonElement);
 });
 
@@ -98,7 +102,7 @@ it('renders alert action as button', () => {
   >
 ).forEach((type) => {
   it(`renders ${type} correctly`, () => {
-    const closeMock = jest.fn();
+    const closeMock = vi.fn();
     const { container, getByText } = render(
       <Alert.Wrapper type={type}>
         <Alert.Icon />
@@ -106,19 +110,19 @@ it('renders alert action as button', () => {
         <Alert.CloseButton onClick={closeMock} />
       </Alert.Wrapper>,
     );
-    const alert = container.querySelector('.iui-alert') as HTMLElement;
+    const alert = container.querySelector('._iui3-alert') as HTMLElement;
     expect(alert).toBeTruthy();
     expect(alert).toHaveAttribute('data-iui-status', `${type}`);
 
-    const icon = container.querySelector('.iui-svg-icon') as HTMLElement;
+    const icon = container.querySelector('._iui3-svg-icon') as HTMLElement;
     expect(icon).toBeTruthy();
     expect(icon).toHaveAttribute('data-iui-icon-color', `${type}`);
 
     expect(
-      container.querySelector('.iui-alert-message > .iui-alert-link'),
+      container.querySelector('._iui3-alert-message > .iui-alert-link'),
     ).toBeNull();
     const close = container.querySelector(
-      'button > .iui-button-icon',
+      'button > ._iui3-button-icon',
     ) as HTMLElement;
     expect(close).toBeTruthy();
     fireEvent.click(close);
@@ -128,7 +132,7 @@ it('renders alert action as button', () => {
 });
 
 it('should support legacy api', async () => {
-  const onClose = jest.fn();
+  const onClose = vi.fn();
 
   render(
     <div data-testid='1'>
--- packages/itwinui-react/vitest.config.ts
+++ packages/itwinui-react/vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: 'setupTests.ts',
     sequence: {
       hooks: 'list',
     },
