2e3cd93bf5832a649ef3168d0fda9a64796de56a
Mayank <9084735+mayank99@users.noreply.github.com>
2024-01-23 09:18:57 -0500

    use `LinkAction` inside `ExpandableBlock` (#1794)
    Instead of the entire trigger area being used as a button, only the main label will now be wrapped in a button and its hit target will be expanded using LinkAction. The main benefit is that it allows users to nest secondary actions.

---
+++ .changeset/lemon-snails-ring.md
@@ -0,0 +1,5 @@
+---
+'@itwin/itwinui-react': minor
+---
+
+`ExpandableBlock` now internally uses `LinkAction` for the trigger element. This slightly improves the accessibility, and opens up the possibility of including secondary actions.
---
+++ examples/ExpandableBlock.actions.tsx
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { ExpandableBlock, IconButton } from '@itwin/itwinui-react';
+import { SvgMoreVertical } from '@itwin/itwinui-icons-react';
+
+export default () => {
+  return (
+    <div style={{ width: 'min(100%, 300px)' }}>
+      <ExpandableBlock.Wrapper>
+        <ExpandableBlock.Trigger
+          label='My disclosure'
+          endIcon={
+            <IconButton
+              label='More options'
+              styleType='borderless'
+              size='small'
+              onClick={() => console.log('Clicked more options')}
+            >
+              <SvgMoreVertical />
+            </IconButton>
+          }
+        />
+
+        <ExpandableBlock.Content>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+          doloremque sint nobis harum. Harum atque, minima in aliquid nostrum
+          corporis.
+        </ExpandableBlock.Content>
+      </ExpandableBlock.Wrapper>
+    </div>
+  );
+};
