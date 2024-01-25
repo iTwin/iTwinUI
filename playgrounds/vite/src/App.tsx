bdcf5abc7c9d639635d2b750086af6d2944cbc49
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-01-25 15:26:37 -0500

    From repro sandbox

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,11 +1,51 @@
-import { Button } from '@itwin/itwinui-react';
+// import "./App.scss";
+import { Table } from '@itwin/itwinui-react';
+import { useState, useMemo } from 'react';
 
-const App = () => {
-  return (
-    <>
-      <Button>Hello world</Button>
-    </>
+export default function App() {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Header',
+        columns: [
+          {
+            id: 'id1',
+            accessor: 'id1',
+            Header: 'id1',
+            width: '20%',
+          },
+          {
+            id: 'id2',
+            accessor: 'id2',
+            Header: 'id2',
+            width: '20%',
+          },
+          {
+            id: 'id3',
+            accessor: 'id3',
+            Header: 'id3',
+            width: '60%',
+          },
+        ],
+      },
+    ],
+    [],
   );
-};
 
-export default App;
+  const data = [
+    { name: 'Name1', description: 'Description1' },
+    { name: 'Name2', description: 'Description2' },
+    { name: 'Name3', description: 'Description3' },
+  ];
+
+  return (
+    <div className='container'>
+      <Table
+        isResizable
+        columns={columns}
+        data={data}
+        emptyTableContent='empty'
+      />
+    </div>
+  );
+}
