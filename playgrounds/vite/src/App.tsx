7e6bba48ab4dd5a3ad554a11e29e5ba7c1194fe7
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-01-25 18:04:41 -0500

    WIP testing/debug

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,5 +1,6 @@
 // import "./App.scss";
-import { Table } from '@itwin/itwinui-react';
+import { Flex, Table } from '@itwin/itwinui-react';
+import { Column } from '@itwin/itwinui-react/react-table';
 import { useState, useMemo } from 'react';
 
 export default function App() {
@@ -39,13 +40,97 @@ export default function App() {
   ];
 
   return (
-    <div className='container'>
-      <Table
-        isResizable
-        columns={columns}
-        data={data}
-        emptyTableContent='empty'
-      />
-    </div>
+    <Flex flexDirection='column' alignItems='stretch'>
+      {/* <div className='container'>
+        <Table
+          isResizable
+          columns={columns}
+          data={data}
+          emptyTableContent='empty'
+        />
+      </div> */}
+
+      {/* should resize only the current column when resize mode is expand */}
+      {renderComponent({
+        columns: [
+          {
+            id: 'name',
+            Header: 'Name',
+            accessor: 'name',
+          },
+          {
+            id: 'description',
+            Header: 'description',
+            accessor: 'description',
+          },
+          {
+            id: 'view',
+            Header: 'view',
+            Cell: () => <>View</>,
+          },
+        ],
+        isResizable: true,
+        columnResizeMode: 'expand',
+      })}
+    </Flex>
   );
 }
+
+const renderComponent = (() => {
+  type TestDataType = {
+    name: string;
+    description: string;
+    accessor?: string;
+    subRows?: TestDataType[];
+    booleanValue?: boolean;
+  };
+  type TableProps = React.ComponentPropsWithoutRef<typeof Table<TestDataType>>;
+
+  const columns = (
+    onViewClick: () => void = () => {},
+  ): Column<TestDataType>[] => [
+    {
+      id: 'name',
+      Header: 'Name',
+      accessor: 'name',
+      width: 90,
+    },
+    {
+      id: 'description',
+      Header: 'Description',
+      accessor: 'description',
+      maxWidth: 200,
+    },
+    {
+      id: 'view',
+      Header: 'View',
+      Cell: () => {
+        return <span onClick={onViewClick}>View</span>;
+      },
+    },
+  ];
+
+  const mockedData = (count = 3): TestDataType[] =>
+    [...new Array(count)].map((_, index) => ({
+      name: `Name${index + 1}`,
+      description: `Description${index + 1}`,
+    }));
+
+  function renderComponent(
+    initialsProps?: Partial<TableProps>,
+    onViewClick?: () => void,
+    renderContainer?: HTMLElement,
+  ) {
+    const defaultProps: TableProps = {
+      columns: columns(onViewClick),
+      data: mockedData(),
+      emptyTableContent: 'Empty table',
+      emptyFilteredTableContent: 'No results. Clear filter.',
+    };
+
+    const props = { ...defaultProps, ...initialsProps };
+    return <Table {...props} />;
+  }
+
+  return renderComponent;
+})();
