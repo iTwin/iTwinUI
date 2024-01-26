30f14d9ebbd37f228ce088ed207ab042638c2688
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2024-01-26 10:00:59 -0500

    upgraded jest-dom version.

--- packages/itwinui-react/setupTests.ts
+++ packages/itwinui-react/setupTests.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/vitest';
 import { TextEncoder } from 'util';
 
 window.HTMLElement.prototype.scrollIntoView = () => {};
