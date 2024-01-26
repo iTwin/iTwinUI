8e20da27e10d66df1d496165a53e89b0adfac925
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-01-26 15:17:29 -0500

    Repro code

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,11 +1,33 @@
-import { Button } from '@itwin/itwinui-react';
+import React from 'react';
+import { Button, ButtonGroup } from '@itwin/itwinui-react';
 
-const App = () => {
+function App() {
   return (
     <>
-      <Button>Hello world</Button>
+      {/* below div is only a visual helper for the 20% width */}
+      <div
+        style={{
+          position: 'absolute',
+          width: '20%',
+          height: '100%',
+          borderRight: '1px solid green',
+        }}
+      ></div>
+      <ButtonGroup
+        style={{ width: '20%' }}
+        overflowButton={() => (
+          <Button size='small' styleType='borderless'>
+            More
+          </Button>
+        )}
+      >
+        <Button>Long enough button</Button>
+      </ButtonGroup>
+      <br />
+      Resize window so the button is more or less than 20% of the window (green
+      line).
     </>
   );
-};
+}
 
 export default App;
