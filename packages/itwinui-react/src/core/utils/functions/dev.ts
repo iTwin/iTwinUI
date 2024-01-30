4ce29730791a50332d2e5d250dffcde32e7d0e8d
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2024-01-30 15:41:06 -0500

    Added PermaLink.

--- packages/itwinui-react/src/core/utils/functions/dev.ts
+++ packages/itwinui-react/src/core/utils/functions/dev.ts
@@ -5,10 +5,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 const isJest = typeof (globalThis as any).jest !== 'undefined';
+
+// See https://github.com/JoshuaKGoldberg/console-fail-test/blob/4738d5f3e506735c2c16f52588a9cfade58b9569/src/environments/mocha.ts#L19-L28
 const isMocha =
   typeof (globalThis as any).beforeEach !== 'undefined' &&
   `${(globalThis as any).beforeEach}`.replace(/\s/g, '') ===
     'function(name,fn){suites[0].beforeEach(name,fn);}';
+
+// See https://github.com/iTwin/iTwinUI/blob/bb1c2260568ce58b4676f2b18081ec7b825812cd/packages/itwinui-react/src/core/utils/functions/dev.ts#L13
 const isVitest = (typeof globalThis as any).__vitest_index__ !== 'undefined';
 
 const isUnitTest = isJest || isVitest || isMocha;
