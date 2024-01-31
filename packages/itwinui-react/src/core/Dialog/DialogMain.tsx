04804f7e841e2be6e075f65c0ab5f9f9dba291c0
Siddhant Rawal <128523557+siddhantrawal@users.noreply.github.com>
2024-01-31 13:58:18 -0500

    updating timeout.

--- packages/itwinui-react/src/core/Dialog/DialogMain.tsx
+++ packages/itwinui-react/src/core/Dialog/DialogMain.tsx
@@ -209,7 +209,7 @@ export const DialogMain = React.forwardRef((props, ref) => {
         enterActive: styles['iui-dialog-animation-enter-active'],
         enterDone: styles['iui-dialog-visible'],
       }}
-      timeout={{ enter: 100, exit: 600 }}
+      timeout={100}
       // Focuses dialog when opened
       onEntered={() => {
         previousFocusedElement.current = dialogRef.current?.ownerDocument
