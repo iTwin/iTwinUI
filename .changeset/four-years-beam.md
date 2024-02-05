6ae00ea13fb8b967e476fbbd0022f7790e58b7b6
Mayank <9084735+mayank99@users.noreply.github.com>
2024-02-05 17:28:50 -0500

    allow tags to be used as buttons (#1815)

---
+++ .changeset/four-years-beam.md
@@ -0,0 +1,5 @@
+---
+'@itwin/itwinui-css': minor
+---
+
+Removed unnecessary `Tag` styles, and added support for tags to be used as `<button>` elements.
--- packages/itwinui-react/src/core/Tag/Tag.tsx
+++ packages/itwinui-react/src/core/Tag/Tag.tsx
@@ -4,28 +4,64 @@
  *--------------------------------------------------------------------------------------------*/
 import cx from 'classnames';
 import * as React from 'react';
-import { SvgCloseSmall, Box } from '../utils/index.js';
+import { SvgCloseSmall, Box, ButtonBase } from '../utils/index.js';
 import type { PolymorphicForwardRefComponent } from '../utils/index.js';
 import { IconButton } from '../Buttons/IconButton.js';
 
 type TagProps = {
   /**
-   * Callback function that handles click on close icon.
-   * Close icon is shown only when this function is passed.
-   * Use only with 'default' Tag.
-   */
-  onRemove?: React.MouseEventHandler;
-  /**
    * Text inside the tag.
    */
   children: React.ReactNode;
-  /**
-   * Variant of tag.
-   * Basic tags don't have an outline.
-   * @default 'default'
-   */
-  variant?: 'default' | 'basic';
-};
+} & (
+  | {
+      /**
+       * Variant of tag.
+       * Basic tags don't have an outline.
+       * @default 'default'
+       */
+      variant?: 'default';
+      /**
+       * Props for customizing the label.
+       *
+       * Only relevant for the 'default' Tag.
+       */
+      labelProps?: React.ComponentPropsWithRef<'span'>;
+    }
+  | {
+      variant?: 'basic';
+      labelProps?: never;
+    }
+) &
+  (
+    | {
+        /**
+         * Callback invoked when the tag is clicked.
+         *
+         * When this prop is passed, the tag will be rendered as a button.
+         *
+         * This prop is mutually exclusive with `onRemove`.
+         */
+        onClick?: React.MouseEventHandler;
+        onRemove?: never;
+        removeButtonProps?: never;
+      }
+    | {
+        onClick?: never;
+        /**
+         * Callback function that handles click on the remove ("❌") button.
+         * If not passed, the remove button will not be shown.
+         *
+         * This prop is mutually exclusive with the `onClick` prop, because
+         * the tag will be rendered as a button when `onClick` is passed.
+         */
+        onRemove?: React.MouseEventHandler;
+        /**
+         * Props for customizing the remove ("❌") button.
+         */
+        removeButtonProps?: React.ComponentPropsWithRef<'button'>;
+      }
+  );
 
 /**
  * Tag for showing categories, filters etc.
@@ -34,11 +70,19 @@ type TagProps = {
  * <Tag variant='basic'>Basic tag</Tag>
  */
 export const Tag = React.forwardRef((props, forwardedRef) => {
-  const { className, variant = 'default', children, onRemove, ...rest } = props;
+  const {
+    className,
+    variant = 'default',
+    children,
+    onRemove,
+    labelProps,
+    removeButtonProps,
+    ...rest
+  } = props;
 
   return (
     <Box
-      as='span'
+      as={!!props.onClick ? ButtonBase : 'span'}
       className={cx(
         {
           'iui-tag-basic': variant === 'basic',
@@ -46,11 +90,16 @@ export const Tag = React.forwardRef((props, forwardedRef) => {
         },
         className,
       )}
-      ref={forwardedRef}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any -- ref's type doesn't matter internally
+      ref={forwardedRef as any}
       {...rest}
     >
       {variant === 'default' ? (
-        <Box as='span' className='iui-tag-label'>
+        <Box
+          as='span'
+          {...labelProps}
+          className={cx('iui-tag-label', labelProps?.className)}
+        >
           {children}
         </Box>
       ) : (
@@ -62,7 +111,8 @@ export const Tag = React.forwardRef((props, forwardedRef) => {
           size='small'
           onClick={onRemove}
           aria-label='Delete tag'
-          className='iui-tag-button'
+          {...removeButtonProps}
+          className={cx('iui-tag-button', removeButtonProps?.className)}
         >
           <SvgCloseSmall aria-hidden />
         </IconButton>
