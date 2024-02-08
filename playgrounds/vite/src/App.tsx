078480727fd85d6403ee7d64f04569e4f0dee3c4
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-08 16:31:05 -0500

    Reverting playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,21 +1,11 @@
-import * as React from 'react';
-import { Button, Modal, ModalContent } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function Home() {
+const App = () => {
   return (
     <>
-      <p>Hello</p>
-      <Modal isOpen={true} title='This is the title'>
-        <ModalContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </ModalContent>
-      </Modal>
+      <Button>Hello world</Button>
     </>
   );
-}
+};
+
+export default App;
