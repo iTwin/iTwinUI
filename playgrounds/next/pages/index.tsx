dce1e2b1ce3edfe6a2172e743a44b6d91f75b7ff
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-09 12:57:54 -0500

    Revert playgrounds

--- playgrounds/next/pages/index.tsx
+++ playgrounds/next/pages/index.tsx
@@ -1,47 +1,9 @@
-import * as React from 'react';
-import { Dialog, Button } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-
+export default function Home() {
   return (
     <>
-      <Button styleType='high-visibility' onClick={() => setIsOpen(true)}>
-        Open dialog
-      </Button>
-      {isOpen && (
-        <Dialog
-          isOpen={true}
-          onClose={() => setIsOpen(false)}
-          closeOnEsc
-          closeOnExternalClick
-          preventDocumentScroll
-          trapFocus
-          setFocus
-          isDismissible
-          portal
-        >
-          <Dialog.Backdrop />
-          <Dialog.Main>
-            <Dialog.TitleBar titleText='Dialog' />
-            <Dialog.Content>
-              A dialog informs users about a task and can contain critical
-              information, require decisions, or involve multiple tasks. Dialogs
-              appear in front of app content to provide critical information or
-              ask for a decision.
-            </Dialog.Content>
-            <Dialog.ButtonBar>
-              <Button
-                styleType='high-visibility'
-                onClick={() => setIsOpen(false)}
-              >
-                Primary
-              </Button>
-              <Button onClick={() => setIsOpen(false)}>Secondary</Button>
-            </Dialog.ButtonBar>
-          </Dialog.Main>
-        </Dialog>
-      )}
+      <Button>Hello world</Button>
     </>
   );
-};
+}
