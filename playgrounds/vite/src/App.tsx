0507fb3cd1e647704bc3468f08bca1bfe62bc394
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-12 15:22:31 -0500

    Testing progress so far

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,9 +1,58 @@
-import { Button } from '@itwin/itwinui-react';
+import { Button, LabeledInput, SearchBox } from '@itwin/itwinui-react';
 
 const App = () => {
   return (
     <>
       <Button>Hello world</Button>
+      {/* End icon has a 9px padding (.75rem) */}
+      <SearchBox>
+        <SearchBox.CollapsedState />
+        <SearchBox.ExpandedState>
+          <SearchBox.Icon />
+          {/* <span>something</span> */}
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Input />
+          <SearchBox.CollapseButton />
+          <SearchBox.CollapseButton />
+        </SearchBox.ExpandedState>
+      </SearchBox>
+      <SearchBox>
+        <SearchBox.CollapsedState />
+        <SearchBox.ExpandedState>
+          <SearchBox.Icon />
+          <SearchBox.Input />
+          <SearchBox.CollapseButton />
+        </SearchBox.ExpandedState>
+      </SearchBox>
+      <SearchBox size='small'>
+        <SearchBox.CollapsedState />
+        <SearchBox.ExpandedState>
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Input />
+          <SearchBox.CollapseButton />
+          <SearchBox.CollapseButton />
+        </SearchBox.ExpandedState>
+      </SearchBox>
+      <SearchBox size='large'>
+        <SearchBox.CollapsedState />
+        <SearchBox.ExpandedState>
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Icon />
+          <SearchBox.Input />
+          <SearchBox.CollapseButton />
+          <SearchBox.CollapseButton />
+        </SearchBox.ExpandedState>
+      </SearchBox>
+
+      <LabeledInput label='Label' status='positive' />
+      <LabeledInput label='Label' status='positive' size='small' />
     </>
   );
 };
