6dc667b0f9436d3fe5ff140217346699676e36e5
Mayank <9084735+mayank99@users.noreply.github.com>
2024-02-15 17:48:25 -0500

    add `lines` prop

--- packages/itwinui-react/src/core/utils/components/LineClamp.tsx
+++ packages/itwinui-react/src/core/utils/components/LineClamp.tsx
@@ -2,7 +2,22 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { polymorphic } from '../functions/polymorphic.js';
+import * as React from 'react';
+import cx from 'classnames';
+import type { PolymorphicForwardRefComponent } from '../props.js';
+import { Box } from './Box.js';
 
 /** @private */
-export const LineClamp = polymorphic.div('iui-line-clamp');
+export const LineClamp = React.forwardRef((props, forwardedRef) => {
+  const { lines, ...rest } = props;
+  return (
+    <Box
+      ref={forwardedRef}
+      {...rest}
+      style={
+        { '--iui-line-clamp': lines, ...props.style } as React.CSSProperties
+      }
+      className={cx('iui-line-clamp', props.className)}
+    />
+  );
+}) as PolymorphicForwardRefComponent<'div', { lines?: number }>;
