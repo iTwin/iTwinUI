7056ceb8e3d1b66dea46319261a1969d8c0ec9a2
Mayank <9084735+mayank99@users.noreply.github.com>
2024-02-22 10:12:04 -0500

    migrate from `yarn` to `pnpm`

--- apps/react-workshop/scripts/entrypoint.sh
+++ apps/react-workshop/scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # `&` is used to run the process in the background
-yarn serve apps/react-workshop/build --listen 6006 -L &
-yarn workspace react-workshop cypress run "$@"
+pnpm exec serve apps/react-workshop/build --listen 6006 -L &
+pnpm --filter react-workshop cypress run "$@"
