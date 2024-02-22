3cf28716f1ea21d2fa8beb42686122a5d02e6489
Mayank <9084735+mayank99@users.noreply.github.com>
2024-02-22 10:33:23 -0500

    fix ci tests

--- apps/react-workshop/scripts/entrypoint.sh
+++ apps/react-workshop/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # `&` is used to run the process in the background
-corepack enable pnpm
+corepack enable pnpm &&
 pnpm exec serve apps/react-workshop/build --listen 6006 -L &
 pnpm --filter react-workshop cypress run "$@"
