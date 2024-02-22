e22a3efc4eec1f66c7ffc2c4fda6a3181ca8b310
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-22 10:15:15 -0500

    Revert unnecessary changes

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,49 +1,11 @@
-import * as React from 'react';
-import { Dialog, Button } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-
+const App = () => {
   return (
     <>
-      <Button styleType='high-visibility' onClick={() => setIsOpen(true)}>
-        Open dialog
-      </Button>
-      {/* {isOpen && ( */}
-      <Dialog
-        // isOpen={true}
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        closeOnEsc
-        closeOnExternalClick
-        preventDocumentScroll
-        trapFocus
-        setFocus
-        isDismissible
-        portal
-        renderWrapperWhenClosed={false}
-      >
-        <Dialog.Backdrop />
-        <Dialog.Main>
-          <Dialog.TitleBar titleText='Dialog' />
-          <Dialog.Content>
-            A dialog informs users about a task and can contain critical
-            information, require decisions, or involve multiple tasks. Dialogs
-            appear in front of app content to provide critical information or
-            ask for a decision.
-          </Dialog.Content>
-          <Dialog.ButtonBar>
-            <Button
-              styleType='high-visibility'
-              onClick={() => setIsOpen(false)}
-            >
-              Primary
-            </Button>
-            <Button onClick={() => setIsOpen(false)}>Secondary</Button>
-          </Dialog.ButtonBar>
-        </Dialog.Main>
-      </Dialog>
-      {/* )} */}
+      <Button>Hello world</Button>
     </>
   );
 };
+
+export default App;
