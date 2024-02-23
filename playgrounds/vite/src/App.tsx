6fb27a73395d1102d5bd786aee907dda4291310b
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-23 17:14:39 -0500

    Reverted playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,96 +1,11 @@
-import * as React from 'react';
-import {
-  Checkbox,
-  Divider,
-  InputGroup,
-  LabeledSelect,
-  Radio,
-  StatusMessage,
-} from '@itwin/itwinui-react';
-import { SvgCheckmark, SvgStar } from '@itwin/itwinui-icons-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function App() {
-  const CustomDivider = () => (
-    <Divider
-      style={{
-        margin: '10px',
-      }}
-    />
-  );
-
+const App = () => {
   return (
     <>
-      {/* ✅ LabeledSelect */}
-      <LabeledSelect
-        label='Select Label'
-        options={[
-          { value: 1, label: 'Item #1' },
-          { value: 2, label: 'Item #2' },
-          { value: 3, label: 'Item #3' },
-        ]}
-        status='positive'
-        svgIcon={<SvgStar />}
-        message={'Help message'}
-      />
-
-      <LabeledSelect
-        label='Select Label'
-        options={[
-          { value: 1, label: 'Item #1' },
-          { value: 2, label: 'Item #2' },
-          { value: 3, label: 'Item #3' },
-        ]}
-        status='positive'
-        message={
-          <StatusMessage status='positive' startIcon={<SvgStar />}>
-            Help message
-          </StatusMessage>
-        }
-      />
-
-      <CustomDivider />
-
-      {/* ❌ InputGroup */}
-      <InputGroup
-        label='Checkbox group'
-        svgIcon={<SvgStar />}
-        status='positive'
-        message='Help message'
-      >
-        <Checkbox label='Select all' />
-        <Checkbox label='Football' />
-        <Checkbox label='Hockey' />
-      </InputGroup>
-
-      {/* ❌ Passing <StatusMessage startIcon={…} /> to the message of <InputGroup status="…" /> shows duplicate status icons */}
-      <InputGroup
-        label='Checkbox group'
-        status='positive'
-        svgIcon={<SvgCheckmark />}
-        message='Help message'
-        // message={
-        //   <StatusMessage startIcon={<SvgStar />} status='positive'>
-        //     Help message
-        //   </StatusMessage>
-        // }
-      >
-        <Checkbox label='Select all' />
-        <Checkbox label='Football' />
-        <Checkbox label='Hockey' />
-      </InputGroup>
-
-      <InputGroup
-        label='some label'
-        message={
-          <StatusMessage>
-            <div className='my-message'>Message</div>
-          </StatusMessage>
-        }
-      >
-        <Radio />
-        <Radio />
-        <Radio />
-      </InputGroup>
+      <Button>Hello world</Button>
     </>
   );
-}
+};
+
+export default App;
