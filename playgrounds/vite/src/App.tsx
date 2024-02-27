e74a808356bd02dd3ae85b6203ec17c28fb07bc0
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-02-27 13:34:02 -0500

    Working for basic cases

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,9 +1,60 @@
-import { Button } from '@itwin/itwinui-react';
+import { SvgSearch, SvgStar } from '@itwin/itwinui-icons-react';
+import {
+  Button,
+  Icon,
+  IconButton,
+  Input,
+  InputWithDecorations,
+} from '@itwin/itwinui-react';
 
 const App = () => {
+  const dummyValue = Array.from({ length: 100 }, () => 'ABC').join('');
+  const dummyStyle = {
+    // backgroundColor: 'hotpink',
+  };
+
   return (
     <>
       <Button>Hello world</Button>
+      <InputWithDecorations>
+        <Icon padded>
+          <SvgStar />
+        </Icon>
+        <Input value={dummyValue} style={dummyStyle} />
+        <IconButton styleType='borderless'>
+          <SvgSearch />
+        </IconButton>
+      </InputWithDecorations>
+
+      <InputWithDecorations>
+        <Icon padded>
+          <SvgStar />
+        </Icon>
+        <Input value={dummyValue} style={dummyStyle} />
+        <IconButton onClick={() => console.log('pressed')}>
+          <SvgSearch />
+        </IconButton>
+      </InputWithDecorations>
+
+      <InputWithDecorations>
+        <Icon>
+          <SvgStar />
+        </Icon>
+        <Input value={dummyValue} style={dummyStyle} />
+        <IconButton onClick={() => console.log('pressed')}>
+          <SvgSearch />
+        </IconButton>
+      </InputWithDecorations>
+
+      <InputWithDecorations>
+        <Icon padded>
+          <SvgStar />
+        </Icon>
+        <Input value={dummyValue} style={dummyStyle} />
+        <IconButton onClick={() => console.log('pressed')}>
+          <SvgSearch />
+        </IconButton>
+      </InputWithDecorations>
     </>
   );
 };
