5df0df95799cbdad3681764c4a740dba6063f1c4
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2024-03-01 15:55:03 -0500

    fix spacing

--- scripts/testAll.js
+++ scripts/testAll.js
@@ -1,46 +1,65 @@
-const { execSync } = require( 'child_process');
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+const { execSync } = require('child_process');
 
 const componentName = process.argv[2];
 
 const runCommand = (type, command) => {
-  console.log(`${type} tests are running`)
+  console.log(`${type} tests are running`);
 
   if (!componentName) {
-    console.error('Please provide a component name')
-    process.exit(1)
-  };
+    console.error('Please provide a component name');
+    process.exit(1);
+  }
 
   try {
-    execSync(command)
-    return "\x1b[32mSuccess\x1b[0m"
-
-  } catch(error){
-    console.log(`${type} test failed`)
-    console.error(error)
-    return "\x1b[31mFailure\x1b[0m"
-  };
+    execSync(command);
+    return '\x1b[32mSuccess\x1b[0m';
+  } catch (error) {
+    console.log(`${type} test failed`);
+    console.error(error);
+    return '\x1b[31mFailure\x1b[0m';
+  }
 };
 
+/*
+figure out commit bug
+figure out how to find the correct file when needed for unit testing
+why doesnt react visual testing always work?
+how important are capitals and how can i fix that
+ get more detailed information for errors and postive
+ */
 
 const runTests = () => {
-  const formattedName = componentName
+  const formattedName = componentName;
 
-  const reactResult = runCommand("React Visual", `pnpm run --filter react-workshop test --spec="**/${formattedName}.*"`);
-  const cssResult = runCommand("CSS Visual", `pnpm run --filter css-workshop test --filter=${componentName}`);
-  const unitResult = runCommand("Unit", `cd packages/itwinui-react/src/core/${componentName} && pnpm run test:unit && cd ../../../../..`);
-  const a11yResult = runCommand("A11y", `cd testing/a11y && pnpm cypress run --component --spec "src/SpecificComponent.cy.tsx" --env componentName=${formattedName}`)
+  const reactResult = runCommand(
+    'React Visual',
+    `pnpm run --filter react-workshop test --spec="**/${formattedName}.*"`,
+  );
+  const cssResult = runCommand(
+    'CSS Visual',
+    `pnpm run --filter css-workshop test --filter=${componentName}`,
+  );
+  const unitResult = runCommand(
+    'Unit',
+    `cd packages/itwinui-react/src/core/${componentName} && pnpm run test:unit && cd ../../../../..`,
+  );
+  const a11yResult = runCommand(
+    'A11y',
+    `cd testing/a11y && pnpm cypress run --component --spec "src/SpecificComponent.cy.tsx" --env componentName=${formattedName}`,
+  );
 
-  console.log("-------------------------------")
-  console.log("      Test      |     Result   ")
-  console.log("-------------------------------")
-  console.log(`React Workshop  |    ${reactResult}  `)
-  console.log(`CSS Workshop    |    ${cssResult}  `)
-  console.log(`Unit            |    ${unitResult}  `)
-  console.log(`A11y            |    ${a11yResult}  `)
-  console.log("-------------------------------")
+  console.log('-------------------------------');
+  console.log('      Test      |     Result   ');
+  console.log('-------------------------------');
+  console.log(`React Workshop  |    ${reactResult}`);
+  console.log(`CSS Workshop    |    ${cssResult}`);
+  console.log(`Unit            |    ${unitResult}`);
+  console.log(`A11y            |    ${a11yResult}`);
+  console.log('-------------------------------');
 };
 
 runTests();
-
-
-
--- testing/a11y/src/SpecificComponent.cy.tsx
+++ testing/a11y/src/SpecificComponent.cy.tsx
@@ -2,24 +2,24 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import * as React from "react";
-import * as allExamples from "examples";
-import { ThemeProvider } from "@itwin/itwinui-react";
+import * as React from 'react';
+import * as allExamples from 'examples';
+import { ThemeProvider } from '@itwin/itwinui-react';
 
-describe("Should have no WCAG violations", () => {
-  const componentName = Cypress.env("componentName");
+describe('Should have no WCAG violations', () => {
+  const componentName = Cypress.env('componentName');
 
   Object.entries(allExamples)
     .filter(([name, Component]) => name.includes(componentName))
     .forEach(([name, Component]) => {
       it(name, () => {
         cy.mount(
-          <ThemeProvider theme="dark" style={{ height: "100vh" }}>
+          <ThemeProvider theme='dark' style={{ height: '100vh' }}>
             <Component />
-          </ThemeProvider>
+          </ThemeProvider>,
         );
         cy.injectAxe({
-          axeCorePath: Cypress.env("axeCorePath"),
+          axeCorePath: Cypress.env('axeCorePath'),
         });
 
         cy.configureAxe(axeConfigPerExample(name));
@@ -27,10 +27,10 @@ describe("Should have no WCAG violations", () => {
         cy.checkA11y(undefined, undefined, (violations) => {
           const violationData = violations.map(({ id, help }) => ({
             Component: name,
-            "Rule ID": id,
+            'Rule ID': id,
             Description: help,
           }));
-          cy.task("table", violationData);
+          cy.task('table', violationData);
         });
       });
     });
