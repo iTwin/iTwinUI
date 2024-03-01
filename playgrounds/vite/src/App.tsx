96b29525438dfbcda9136a145312a62a47fdf596
Jon Graft <849817+FlyersPh9@users.noreply.github.com>
2024-03-01 16:50:06 -0500

    Undo playground.
    Co-authored-by: Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,55 +1,11 @@
-import { Button, Flex, Select } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
   return (
     <>
-      <Flex flexDirection='column' alignItems='flex-start'>
-        <Demo native={false} />
-        <Demo native={true} />
-      </Flex>
+      <Button>Hello world</Button>
     </>
   );
 };
 
 export default App;
-
-const Demo = ({ native = false }: { native: boolean }) => {
-  const options = [
-    { value: '1', label: 'Item #1' },
-    { value: '2', label: 'Item #2', disabled: true },
-    { value: '3', label: 'Item #3' },
-  ];
-
-  return (
-    <>
-      <Select
-        options={options}
-        placeholder='Placeholder text'
-        styleType='borderless'
-        native={native}
-      />
-      <Select
-        options={options}
-        placeholder='Placeholder text'
-        styleType='borderless'
-        disabled
-        native={native}
-      />
-      <Select
-        options={options}
-        placeholder='Placeholder text'
-        styleType='borderless'
-        status='positive'
-        native={native}
-      />
-      <Select
-        options={options}
-        placeholder='Placeholder text'
-        styleType='borderless'
-        status='positive'
-        disabled
-        native={native}
-      />
-    </>
-  );
-};
