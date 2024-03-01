3518b518a63a70ec1ebd6889fb5f502020277f44
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-01 12:49:37 -0500

    Revert "Screen recording"
    This reverts commit 093ca0d931ad1b45ef2832ebb4b3df23ca70f4bc.

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,15 +1,9 @@
-import { SvgSearch } from '@itwin/itwinui-icons-react';
-import { Button, IconButton, SearchBox, Flex } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
 const App = () => {
   return (
     <>
-      <Flex flexDirection='column' alignItems='flex-start'>
-        {/* <IconButton>
-          <SvgSearch />
-        </IconButton> */}
-        <SearchBox expandable />
-      </Flex>
+      <Button>Hello world</Button>
     </>
   );
 };
