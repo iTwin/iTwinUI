6f8cbddecc0711d8f272fd84dc3f3b8568d282a4
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-06 17:49:50 -0500

    Playground testing

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -4,24 +4,21 @@ const App = () => {
   return (
     <>
       <Button>Hello world</Button>
-      <Select
-        // placeholder={<div>q</div>}
-        // placeholder={9}
-        // placeholder={false}
-        // placeholder={'Select an option'}
-        // defaultValue={['3']}
-        // defaultValue={'3'}
-        // styleType='borderless'
-        multiple={true}
-        // defaultValue='3'
-        // native={true}
-        native={false}
-        options={[
-          { label: 'Option 1', value: '1' },
-          { label: 'Option 2', value: '2' },
-          { label: 'Option 3', value: '3' },
-        ]}
-      />
+
+      {[false, true].flatMap((multiple) => {
+        return [false, true].flatMap((native) => {
+          return ['default', 'borderless'].flatMap((styleType) => {
+            return (
+              <SelectTest
+                multiple={multiple}
+                native={native}
+                styleType={styleType}
+              />
+            );
+          });
+        });
+      })}
+
       <LabeledSelect
         // placeholder={<div>q</div>}
         // placeholder={9}
@@ -44,3 +41,48 @@ const App = () => {
 };
 
 export default App;
+
+// ----------------------------------------------------------------------------
+
+const SelectTest = ({
+  multiple,
+  native,
+  styleType,
+}: {
+  multiple: any;
+  native: any;
+  styleType: any;
+}) => {
+  const invalid = native && multiple;
+
+  return (
+    <div
+      style={{
+        marginTop: '20px',
+        border: '1px solid grey',
+        padding: '5px',
+      }}
+    >
+      <b>
+        {JSON.stringify({
+          multiple: multiple,
+          native: native,
+          styleType: styleType,
+        })}
+      </b>
+
+      {!invalid && (
+        <Select
+          multiple={multiple}
+          native={native}
+          styleType={styleType}
+          options={[
+            { label: 'Option 1', value: '1' },
+            { label: 'Option 2', value: '2' },
+            { label: 'Option 3', value: '3' },
+          ]}
+        />
+      )}
+    </div>
+  );
+};
