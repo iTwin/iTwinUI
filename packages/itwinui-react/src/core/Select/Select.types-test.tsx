b8026f02180a1356c393eb926a24d666a6c988de
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-07 15:23:12 -0500

    Some type tests

--- packages/itwinui-react/src/core/Select/Select.types-test.tsx
+++ packages/itwinui-react/src/core/Select/Select.types-test.tsx
@@ -10,6 +10,10 @@
 import React, { useRef } from 'react';
 import { Select } from './Select.js';
 
+// ----------------------------------------------------------------------------
+// Testing onChange type in inferred and explicit cases
+// See: https://github.com/iTwin/iTwinUI/pull/1774
+
 () => {
   const ref = useRef(null);
   return (
@@ -80,6 +84,107 @@ import { Select } from './Select.js';
           const returnValue: string = value;
           return returnValue;
         }}
+      />
+    </>
+  );
+};
+
+// ----------------------------------------------------------------------------
+// Testing `styleType`
+// See: https://github.com/iTwin/iTwinUI/pull/1886
+() => {
+  return (
+    <>
+      {/* Expected type error: */}
+      {/* borderless but missing defaultValue (custom select) */}
+      {/* As a fallback, the first option is selected */}
+      {/* @ts-expect-error (TS 2322) */}
+      <Select
+        styleType='borderless'
+        multiple
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+      <Select
+        styleType='borderless'
+        defaultValue={'1'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+
+      {/* Observation: */}
+      {/* borderless but missing defaultValue (native select) */}
+      {/* No error since defaultValue is not required in borderless native select */}
+      <Select
+        native
+        styleType='borderless'
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+      <Select
+        native
+        styleType='borderless'
+        defaultValue={'1'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+
+      {/* Expected error: */}
+      {/* (borderless + defaultValue) with placeholder (both selects) */}
+      {/* Error since placeholder should not be passed to borderless */}
+      {/* @ts-expect-error (TS 2322) */}
+      <Select
+        styleType='borderless'
+        defaultValue={'1'}
+        placeholder={'Choose an option'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+      {/* @ts-expect-error (TS 2322) */}
+      <Select
+        native
+        styleType='borderless'
+        defaultValue={'1'}
+        placeholder={'Choose an option'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+      <Select
+        styleType='borderless'
+        defaultValue={'1'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
+      <Select
+        native
+        styleType='borderless'
+        defaultValue={'1'}
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
       />
     </>
   );
