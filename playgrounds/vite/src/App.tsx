062f2db7647a524692162254a560e843a9a9c36c
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-07 17:47:34 -0500

    Playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,9 +1,34 @@
-import { Button } from '@itwin/itwinui-react';
+import { Button, Select, LabeledSelect } from '@itwin/itwinui-react';
+import React from 'react';
 
 const App = () => {
+  const [value, setValue] = React.useState<string | undefined>('1');
+
   return (
     <>
-      <Button>Hello world</Button>
+      {/* <Button onClick={() => setValue(null)}>Hello world</Button> */}
+      {/* <Select
+        native
+        // styleType='borderless'
+        placeholder='Choose an option'
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      /> */}
+      <Select
+        value={value}
+        native
+        status='positive'
+        // label='foobar'
+        styleType='borderless'
+        options={[
+          { label: 'Option 1', value: '1' },
+          { label: 'Option 2', value: '2' },
+          { label: 'Option 3', value: '3' },
+        ]}
+      />
     </>
   );
 };
