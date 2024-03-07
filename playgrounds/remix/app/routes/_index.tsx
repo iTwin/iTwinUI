e9bd90292d466c9dd5ecf750a635f7b3877830d3
Ben.Pusey <Ben.Pusey@bentley.com>
2024-03-07 12:33:06 -0500

    Fixes various build issues.

--- playgrounds/remix/app/routes/_index.tsx
+++ playgrounds/remix/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Button } from '#itwinuireact';
+import { Button } from '@itwin/itwinui-react';
 
 export const meta: MetaFunction = () => {
   return [
