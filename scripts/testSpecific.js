6074362b3c6da207a7ca6e51d9190e0dd8881a93
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2024-03-11 08:51:17 -0400

    tests working again

--- scripts/testSpecific.js
+++ scripts/testSpecific.js
@@ -4,8 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 const { spawn } = require('node:child_process');
 
+// ['src/**/*.test.ts?(x)']
+// after scanning for valid component anmes comapre the arguement to that array if valid pass onto concurrenly
+// if not check if -- if -- then just pass the whole thing down thel ine and if not error 'not valid component name'
+
 try {
   const arguement = process.argv[2];
+  const secondArguement = process.argv[3] ? process.argv[3] : '';
   const baseCmd = 'turbo run test';
 
   if (!arguement) {
@@ -14,13 +19,13 @@ try {
       shell: true,
     });
   } else if (arguement.includes('--filter')) {
-    spawn(`${baseCmd} ${arguement}`, {
+    spawn(`${baseCmd} ${arguement} ${secondArguement}`, {
       stdio: 'inherit',
       shell: true,
     });
   } else {
     spawn(
-      `conc -g "pnpm turbo run --filter css-workshop test --filter=${arguement}" "pnpm --filter=@itwin/itwinui-react run test:unit ${arguement}" "pnpm --filter "./testing/a11y" run test  --env componentName=${arguement}" "${baseCmd} --filter react-workshop  --spec="**/${arguement}.*""`,
+      `conc -g -r "pnpm --filter=css-workshop run test --filter=${arguement.toLowerCase()}" "pnpm --filter=@itwin/itwinui-react run test:unit ${arguement}" "pnpm --filter "./testing/a11y" run test  --env componentName=${arguement}" "pnpm --filter=react-workshop run test --spec="**/${arguement}.*""`,
       {
         stdio: 'inherit',
         shell: true,
