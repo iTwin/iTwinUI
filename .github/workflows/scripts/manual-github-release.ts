1689339bed73b6dce5d5b3f15a788f9efbb56978
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-14 11:12:28 -0400

    Parse changelog instead

--- .github/workflows/scripts/manual-github-release.ts
+++ .github/workflows/scripts/manual-github-release.ts
@@ -75,10 +75,44 @@ const parseChangeset = (changesetFile: string) => {
   };
 };
 
-const changesetFiles = fs
-  .readdirSync('./.changeset')
-  .filter((file) => file.endsWith('.md'));
-const changesets = changesetFiles.map((file) => parseChangeset(file));
+type Package = 'itwinui-css' | 'itwinui-react' | 'itwinui-variables';
+
+/**
+ * Parses the changeset file and returns the latest version
+ */
+const parseChangelog = (pkg: Package) => {
+  const changelog = fs.readFileSync(`./packages/${pkg}/CHANGELOG.md`, 'utf8');
+  const lines = changelog.split('\n');
+
+  const h2Index = (lines: string[]) =>
+    lines.findIndex((line) => line.startsWith('## '));
+
+  const firstH2Index = h2Index(lines);
+  const secondH2Index = (() => {
+    const newLines = lines.slice(firstH2Index + 1);
+
+    let index = h2Index(newLines);
+    // If this is the only version, return where the next version would have started, if it existed
+    if (index === -1) {
+      index = newLines.length;
+    }
+
+    return index + firstH2Index + 1; // Add the offset to account for the slice
+  })();
+
+  const latestVersion = lines
+    .slice(firstH2Index + 2, secondH2Index - 1)
+    .join('\n');
+  return latestVersion;
+};
+
+// const changesetFiles = fs
+//   .readdirSync('./.changeset')
+//   .filter((file) => file.endsWith('.md'));
+// const changesets = changesetFiles.map((file) => parseChangeset(file));
+
+const changelog = parseChangelog('itwinui-react');
+console.log(changelog);
 
 // // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 // const octokit = new Octokit({ auth: `` });
@@ -92,4 +126,4 @@ const changesets = changesetFiles.map((file) => parseChangeset(file));
 //   body: '',
 // });
 
-console.log(changesets);
+// console.log(changesets);
