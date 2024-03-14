5e37ce40f8d4268612f9892e9f7e2b53e69a7c4e
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-03-14 12:34:46 -0400

    Brought back changeset parsing

--- .github/workflows/scripts/manual-github-release.ts
+++ .github/workflows/scripts/manual-github-release.ts
@@ -5,12 +5,111 @@
 import fs from 'node:fs';
 import { Octokit, App } from 'octokit';
 
-const publishablePackages = ['itwinui-react', 'itwinui-variables'] as const;
+const publishablePackages = [
+  '@itwin/itwinui-react',
+  '@itwin/itwinui-variables',
+] as const;
+type PublishablePackages = (typeof publishablePackages)[number];
+const possibleSemvers = ['major', 'minor', 'patch'] as const;
+type PossibleSemvers = (typeof possibleSemvers)[number];
+
+/**
+ * @example
+ * Input:
+ * ```md
+ * ---
+ * "@itwin/itwinui-css": patch
+ * ---
+ *
+ * This is a test changeset.
+ * ```
+ *
+ * Output:
+ * ```json
+ * {
+ *   "file": "file_name.md",
+ *   "packages": {
+ *    "@itwin/itwinui-css": "patch"
+ *   },
+ *   content: "This is a test changeset."
+ * }
+ * ```
+ *
+ * @example
+ * Input:
+ * ```md
+ * ---
+ * "@itwin/itwinui-react": patch
+ * "@itwin/itwinui-variables": minor
+ * ---
+ *
+ * This is a test changeset.
+ * ```
+ *
+ * Output:
+ * ```json
+ * {
+ *   "file": "file_name.md",
+ *   "packages": {
+ *     "@itwin/itwinui-react": "patch",
+ *     "@itwin/itwinui-variables": "minor"
+ *   },
+ *   content: "This is a test changeset."
+ * }
+ * ```
+ */
+const parseChangeset = (changesetFile: string) => {
+  const changeset = fs.readFileSync(`./.changeset/${changesetFile}`, 'utf8');
+
+  const [metadata, content] = changeset.split('---\n\n');
+  const packages = metadata
+    .split('\n')
+    .filter((line) => line.includes('@itwin'))
+    .map((line) => line.split(':'))
+    .reduce(
+      (acc, [pkg, version]) => {
+        acc[pkg.trim().substring(1, pkg.trim().length - 1)] =
+          version.trim() as PossibleSemvers;
+        return acc;
+      },
+      {} as Record<string, PossibleSemvers>,
+    );
+
+  return {
+    file: changesetFile,
+    packages,
+    content,
+  };
+};
+
+/**
+ * Returns a list of all the publishable packages that have at least one changeset.
+ */
+const getReleasePackages = (
+  changesets: ReturnType<typeof parseChangeset>[],
+) => {
+  const releasePackages = new Set<PublishablePackages>();
+
+  changesets.forEach((changeset) => {
+    Object.keys(changeset.packages).forEach((pkg) => {
+      if (!publishablePackages.includes(pkg as PublishablePackages)) {
+        return;
+      }
+      releasePackages.add(pkg as PublishablePackages);
+    });
+  });
+
+  return Array.from(releasePackages);
+};
+
+const shouldRelease = (pkg: string) => {
+  return;
+};
 
 /**
  * Parses the changeset file and returns the latest version
  */
-const parseChangelog = (pkg: (typeof publishablePackages)[number]) => {
+const parseChangelog = (pkg: PublishablePackages) => {
   const changelog = fs.readFileSync(`./packages/${pkg}/CHANGELOG.md`, 'utf8');
   const lines = changelog.split('\n');
 
@@ -38,15 +137,19 @@ const parseChangelog = (pkg: (typeof publishablePackages)[number]) => {
   };
 };
 
-publishablePackages.forEach((pkg) => {
-  const changelog = parseChangelog(pkg);
-  console.log(`${pkg}: `, changelog);
-});
+// publishablePackages.forEach((pkg) => {
+//   const changelog = parseChangelog(pkg);
+//   console.log(`${pkg}: `, changelog);
+// });
 
-// const changesetFiles = fs
-//   .readdirSync('./.changeset')
-//   .filter((file) => file.endsWith('.md'));
-// const changesets = changesetFiles.map((file) => parseChangeset(file));
+const changesetFiles = fs
+  .readdirSync('./.changeset')
+  .filter((file) => file.endsWith('.md'));
+const changesets = changesetFiles.map((file) => parseChangeset(file));
+const releases = getReleasePackages(changesets);
+
+console.log(changesets);
+console.log(releases);
 
 // const changelog = parseChangelog('itwinui-react');
 // console.log(changelog);
