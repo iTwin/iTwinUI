bd768f9361c0c6d4ac5014068539c7c014be8216
Mayank <9084735+mayank99@users.noreply.github.com>
2024-03-20 15:53:33 -0400

    fix index

--- apps/css-workshop/astro.config.mjs
+++ apps/css-workshop/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   build: {
     inlineStylesheets: 'never',
+    format: 'file',
   },
   compressHTML: true,
   devToolbar: { enabled: false },
