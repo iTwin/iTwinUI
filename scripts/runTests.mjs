b2e79118194d532ced711dc50d4c693aaf525964
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2024-03-25 15:15:09 -0400

    covert command back to one long string

--- scripts/runTests.mjs
+++ scripts/runTests.mjs
@@ -6,8 +6,6 @@ import { spawn } from 'node:child_process';
 
 try {
   const argument = process.argv[2];
-  const prefixes = '"CSS-workshop,Unit,A11y,React-workshop,e2e"';
-  const colors = '#8BCC2F,#D8598C,#539AD0,#D1B52A,white';
 
   if (argument?.includes('filter')) {
     spawn(`turbo run test ${argument}`, {
@@ -16,7 +14,7 @@ try {
     });
   } else if (argument) {
     spawn(
-      `conc -g -n ${prefixes} -c ${colors} "pnpm --filter=css-workshop run test --filter=${argument.toLowerCase()}" "pnpm --filter=@itwin/itwinui-react run test:unit ${argument}" "pnpm --filter "./testing/a11y" run test  --env componentName=${argument}" "pnpm --filter=react-workshop run test --spec="**/${argument}.*"" "pnpm --filter=e2e -- ${argument}"`,
+      `conc -g -n "CSS-workshop,Unit,A11y,React-workshop,e2e" -c #8BCC2F,#D8598C,#539AD0,#D1B52A,white "pnpm --filter=css-workshop run test --filter=${argument.toLowerCase()}" "pnpm --filter=@itwin/itwinui-react run test:unit ${argument}" "pnpm --filter "./testing/a11y" run test  --env componentName=${argument}" "pnpm --filter=react-workshop run test --spec="**/${argument}.*"" "pnpm --filter=e2e -- ${argument}"`,
       {
         stdio: 'inherit',
         shell: true,
