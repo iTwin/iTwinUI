a393289e9da6f3bcf6c6125f82a89d4e5f409ed0
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2024-03-25 15:19:07 -0400

    small fix

--- scripts/runTests.mjs
+++ scripts/runTests.mjs
@@ -14,7 +14,7 @@ try {
     });
   } else if (argument) {
     spawn(
-      `conc -g -n "CSS-workshop,Unit,A11y,React-workshop,e2e" -c #8BCC2F,#D8598C,#539AD0,#D1B52A,white "pnpm --filter=css-workshop run test --filter=${argument.toLowerCase()}" "pnpm --filter=@itwin/itwinui-react run test:unit ${argument}" "pnpm --filter "./testing/a11y" run test  --env componentName=${argument}" "pnpm --filter=react-workshop run test --spec="**/${argument}.*"" "pnpm --filter=e2e -- ${argument}"`,
+      `conc -g -n "CSS-workshop,Unit,A11y,React-workshop,e2e" -c #8BCC2F,#D8598C,#539AD0,#D1B52A,white "pnpm --filter=css-workshop run test --filter=${argument.toLowerCase()}" "pnpm --filter=@itwin/itwinui-react run test:unit ${argument}" "pnpm --filter "./testing/a11y" run test  --env componentName=${argument}" "pnpm --filter=react-workshop run test --spec="**/${argument}.*"" "pnpm --filter=e2e run test -- ${argument}"`,
       {
         stdio: 'inherit',
         shell: true,
