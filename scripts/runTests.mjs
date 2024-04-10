e7e70a4889048794e4bef1dcc6e82ead6e9853cf
Ben.Pusey <Ben.Pusey@bentley.com>
2024-04-10 14:58:35 -0400

    cleanup runTests script.

--- scripts/runTests.mjs
+++ scripts/runTests.mjs
@@ -7,37 +7,21 @@ import { spawn } from 'node:child_process';
 const argument = process.argv[2];
 
 if (argument?.includes('filter')) {
-  const subprocess = spawn(`turbo run test ${argument}`, {
-    stdio: 'inherit',
-    shell: true,
-  });
-  subprocess.on('exit', (code) => {
-    console.log('this is an exit event.');
-    if (code && code !== 0) {
-      process.exit(1);
-    }
-  });
+  startProcess(`turbo run test ${argument}`);
 } else if (argument) {
-  const subprocess = spawn(
+  startProcess(
     `conc -g -n "CSS-workshop,Unit,A11y,React-workshop,e2e" -c "#8BCC2F,#D8598C,#539AD0,#D1B52A,white" "pnpm test:css --filter=${argument.toLowerCase()}" "pnpm test:unit ${argument}" "pnpm test:a11y --env componentName=${argument}" "pnpm test:react --spec="**/${argument}.*"" "pnpm run test:e2e -- ${argument}"`,
-    {
-      stdio: 'inherit',
-      shell: true,
-    },
   );
-  subprocess.on('exit', (code) => {
-    console.log('this is an exit event.');
-    if (code && code !== 0) {
-      process.exit(1);
-    }
-  });
 } else {
-  const subprocess = spawn(`turbo run test`, {
+  startProcess(`turbo run test`);
+}
+
+function startProcess(command) {
+  const subprocess = spawn(command, {
     stdio: 'inherit',
     shell: true,
   });
   subprocess.on('exit', (code) => {
-    console.log('this is an exit event.');
     if (code && code !== 0) {
       process.exit(1);
     }
