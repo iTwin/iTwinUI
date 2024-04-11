c98c5e8847d2a219d7b098cfd979b6892c648920
Ben.Pusey <Ben.Pusey@bentley.com>
2024-04-11 12:22:14 -0400

    Adds astro-relative-links to astro-playground to fix path issues.

--- playgrounds/astro/astro.config.mjs
+++ playgrounds/astro/astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), relativeLinks()],
   server: {
     port: 1703,
   },
-  base: './',
 });
