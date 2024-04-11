c6f013eda06d0fa22c402abe1c0a3e06d962efdd
Ben.Pusey <Ben.Pusey@bentley.com>
2024-04-11 16:15:18 -0400

    Removes astro-playground deployment and relative links changes.

--- playgrounds/astro/astro.config.mjs
+++ playgrounds/astro/astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), relativeLinks()],
+  integrations: [react()],
   server: {
     port: 1703,
   },
