5a71a7f00fb2ffbafb63e67ba7b822c67d35df55
Adam Meza - Intern <adammeza-bentley@users.noreply.github.com>
2024-05-01 09:18:44 -0400

    small change

--- packages/itwinui-react/src/core/Tree/TreeNode.tsx
+++ packages/itwinui-react/src/core/Tree/TreeNode.tsx
@@ -304,7 +304,7 @@ export const TreeNode = (props: TreeNodeProps) => {
                 <Box
                   className={
                     (cx('iui-tree-node-content-caption'),
-                    sublabelProps.className)
+                    sublabelProps?.className)
                   }
                   {...sublabelProps}
                 >
