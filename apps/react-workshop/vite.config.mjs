017745d260da51d63ae7174bb962948cfd759ed6
Mayank <9084735+mayank99@users.noreply.github.com>
2024-05-01 11:07:30 -0400

    leftover

--- apps/react-workshop/vite.config.mjs
+++ apps/react-workshop/vite.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import { itwinuiReactAliases } from 'helpers';
 
-console.log(itwinuiReactAliases);
-
 export default defineConfig({
   server: { open: false },
   resolve: {
