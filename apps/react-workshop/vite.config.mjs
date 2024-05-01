1300a8631afcc583e30431d4ba7b199a6f621f47
Mayank <9084735+mayank99@users.noreply.github.com>
2024-05-01 11:00:01 -0400

    fix vite dev servers by directly using TS source files

--- apps/react-workshop/vite.config.mjs
+++ apps/react-workshop/vite.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
+import { itwinuiReactAliases } from 'helpers';
+
+console.log(itwinuiReactAliases);
 
 export default defineConfig({
   server: { open: false },
+  resolve: {
+    alias: itwinuiReactAliases,
+  },
 });
--- playgrounds/remix/vite.config.ts
+++ playgrounds/remix/vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { itwinuiReactAliases } from 'helpers';
 
 installGlobals();
 
@@ -9,5 +10,8 @@ export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
   server: {
     port: 1704,
+  },
+  resolve: {
+    alias: itwinuiReactAliases,
   },
 });
