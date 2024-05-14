5d6e1ab8118562be4e9b00e45a6e43ceeb7caeb3
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-05-14 14:08:43 -0400

    Revert playground

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,58 +1,11 @@
-import * as React from 'react';
-import { ComboBox, Select } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-const countriesList = [
-  { label: 'AAA', value: 'A' },
-  { label: 'BBB', value: 'B' },
-  { label: 'CCC', value: 'C' },
-];
-
-// Steps to reproduce:
-// * Select any other item in the ComboBox.
-// * Even though `value="C"`, the ComboBox value changes.
-// * This doesn't happen in Select.
-export default function App() {
-  const options = React.useMemo(() => countriesList, []);
-
-  const [value, setValue] = React.useState('C');
-
-  // // Every 3 seconds, cycle the value
-  // React.useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setValue((prev) => {
-  //       const index = options.findIndex((o) => o.value === prev);
-  //       return options[(index + 1) % options.length].value;
-  //     });
-  //   }, 3000);
-  //   return () => clearInterval(interval);
-  // }, [options]);
-
+const App = () => {
   return (
     <>
-      {/* <ComboBox
-        options={options}
-        value={value}
-        onChange={(v) => {
-          console.log('v', v);
-
-          // // Set value after 2 seconds
-          // setTimeout(() => {
-          //   setValue(v);
-          // }, 2000);
-
-          // setValue(v);
-        }}
-      />
-      <ComboBox
-        options={options}
-        multiple
-        onChange={(value, event) => console.log(value, event)}
-      /> */}
-      <ComboBox
-        options={options}
-        onChange={(value) => console.log('onChange', value)}
-      />
-      {/* <Select options={options} value='C' /> */}
+      <Button>Hello world</Button>
     </>
   );
-}
+};
+
+export default App;
