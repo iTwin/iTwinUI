2487b87dc117ba94b9c9e18e12793a1990b35814
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-05-20 18:51:19 -0400

    Added Menu e2e test for moving focus when open/close

--- testing/e2e/app/routes/Menu/spec.ts
+++ testing/e2e/app/routes/Menu/spec.ts
@@ -43,8 +43,10 @@ test.describe('Menu', () => {
     await page.waitForTimeout(50);
   });
 
-  test('should handle keyboard navigation', async ({ page }) => {
-    await page.goto('/Menu?children=complex&visible=false&listNavigation=true');
+  test('should move focus appropriately upon Menu open and close', async ({
+    page,
+  }) => {
+    await page.goto('/Menu?children=complex&visible=false');
 
     const menu = page.locator('.Menu');
     await expect(menu).not.toBeVisible();
@@ -55,18 +57,32 @@ test.describe('Menu', () => {
     await expect(menu).toBeVisible();
 
     // Opening the menu with a mouse click should keep the focus on the trigger itself.
-    await expect(page.getByTestId('toggle-menu-button')).toBeFocused();
+    await expect(button).toBeFocused();
 
     // Close the menu
     await button.click();
     await expect(menu).not.toBeVisible();
+    await expect(button).toBeFocused();
 
     // Focus the trigger and press Enter
-    await button.focus();
     await page.keyboard.press('Enter', keyboardPressOptions);
     await expect(menu).toBeVisible();
 
     // Opening the menu with a keyboard press should focus the first focusable item.
+    await expect(page.getByTestId('FocusTarget-0')).toBeFocused();
+  });
+
+  test('should handle keyboard navigation', async ({ page }) => {
+    await page.goto('/Menu?children=complex&visible=false&listNavigation=true');
+
+    const menu = page.locator('.Menu');
+    await expect(menu).not.toBeVisible();
+
+    const button = page.getByTestId('toggle-menu-button');
+    await button.focus();
+    await page.keyboard.press('Enter', keyboardPressOptions);
+
+    await expect(menu).toBeVisible();
     await expect(page.getByTestId('FocusTarget-0')).toBeFocused();
 
     // Should not loop around when navigating past the first or the last item.
