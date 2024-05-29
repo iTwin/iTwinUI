528847fa36cb8b50042482c5edafc7c7f56dd76e
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-05-29 16:57:37 -0400

    Simplified unit tests

--- packages/itwinui-react/src/core/Table/cells/DefaultCell.test.tsx
+++ packages/itwinui-react/src/core/Table/cells/DefaultCell.test.tsx
@@ -4,41 +4,26 @@
  *--------------------------------------------------------------------------------------------*/
 import { render } from '@testing-library/react';
 import * as React from 'react';
-import type { CellRendererProps } from '@itwin/itwinui-react/react-table';
 import { DefaultCell } from './DefaultCell.js';
 
-const renderComponent = (
-  props?: Partial<React.ComponentProps<typeof DefaultCell>>,
-) => {
-  const dummyTableCellRenderedProps = {
-    cellElementProps: {
-      className: 'iui-table-cell',
-      style: {
-        boxSizing: 'border-box',
-        flex: '1 1 145px',
-        minWidth: '72px',
-        width: '0px',
-      },
-    },
-    cellProps: {
-      column: {
-        id: 'id',
-      },
-      value: 'cell value',
-    },
-  } as CellRendererProps<any>;
-
-  return render(<DefaultCell {...dummyTableCellRenderedProps} {...props} />);
-};
-
 it('should merge className/style with cellElementProps', () => {
-  const { container } = renderComponent({
-    className: 'test-class',
-    style: { color: 'red' },
-  });
+  const { container } = render(
+    <DefaultCell
+      cellElementProps={{
+        key: 'key',
+        className: 'original-class',
+        style: { width: 100 },
+      }}
+      className='test-class'
+      style={{ color: 'red' }}
+      cellProps={{ column: { id: 'id' } } as any}
+    >
+      {null}
+    </DefaultCell>,
+  );
 
-  const cellElement = container.querySelector('.iui-table-cell') as HTMLElement;
-  expect(cellElement).toBeTruthy();
-  expect(cellElement.classList.contains('test-class')).toBe(true);
+  const cellElement = container.querySelector('.original-class') as HTMLElement;
+  expect(cellElement).toHaveClass('test-class');
+  expect(cellElement.style.width).toBe('100px');
   expect(cellElement.style.color).toBe('red');
 });
--- packages/itwinui-react/src/core/Table/cells/EditableCell.test.tsx
+++ packages/itwinui-react/src/core/Table/cells/EditableCell.test.tsx
@@ -5,48 +5,26 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import { EditableCell } from './EditableCell.js';
-import type { CellRendererProps } from '@itwin/itwinui-react/react-table';
-
-const renderComponent = (
-  props?: Partial<React.ComponentProps<typeof EditableCell>>,
-) => {
-  const dummyTableCellRenderedProps = {
-    cellElementProps: {
-      className: 'iui-table-cell',
-      style: {
-        boxSizing: 'border-box',
-        flex: '1 1 145px',
-        minWidth: '72px',
-        width: '0px',
-      },
-    },
-    cellProps: {
-      column: {
-        id: 'id',
-      },
-      value: 'cell value',
-    },
-  } as CellRendererProps<any>;
-
-  const onCellEdit = vi.fn();
-
-  return render(
-    <EditableCell
-      onCellEdit={onCellEdit}
-      {...dummyTableCellRenderedProps}
-      {...props}
-    />,
-  );
-};
 
 it('should merge className/style with cellElementProps', () => {
-  const { container } = renderComponent({
-    className: 'test-class',
-    style: { color: 'red' },
-  });
+  const { container } = render(
+    <EditableCell
+      cellElementProps={{
+        key: 'key',
+        className: 'original-class',
+        style: { width: 100 },
+      }}
+      className='test-class'
+      style={{ color: 'red' }}
+      cellProps={{ value: 'value' } as any}
+      onCellEdit={() => {}}
+    >
+      {null}
+    </EditableCell>,
+  );
 
-  const cellElement = container.querySelector('.iui-table-cell') as HTMLElement;
-  expect(cellElement).toBeTruthy();
-  expect(cellElement.classList.contains('test-class')).toBe(true);
+  const cellElement = container.querySelector('.original-class') as HTMLElement;
+  expect(cellElement).toHaveClass('test-class');
+  expect(cellElement.style.width).toBe('100px');
   expect(cellElement.style.color).toBe('red');
 });
