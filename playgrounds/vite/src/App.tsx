e9513b910f7111f342be84eb6a6d37bb37240ac5
Ben.Pusey <Ben.Pusey@bentley.com>
2024-06-11 15:02:43 -0400

    Removes playground code.

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,88 +1,11 @@
-import { Table, ThemeProvider } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-import '@itwin/itwinui-react/styles.css';
-import { useMemo } from 'react';
-
-export default function App() {
+const App = () => {
   return (
-    <ThemeProvider>
-      <ExpandableSubRowsTable />
-    </ThemeProvider>
-  );
-}
-
-const ExpandableSubRowsTable = () => {
-  const columns = useMemo(
-    () => [
-      {
-        id: 'name',
-        Header: 'Name',
-        accessor: 'name',
-      },
-      {
-        id: 'description',
-        Header: 'Description',
-        accessor: 'description',
-      },
-    ],
-    [],
-  );
-
-  const data = [
-    {
-      name: 'Row 1',
-      description: 'Description 1',
-      subRows: [
-        { name: 'Row 1.1', description: 'Description 1.1', subRows: [] },
-        {
-          name: 'Row 1.2',
-          description: 'Description 1.2',
-          subRows: [
-            {
-              name: 'Row 1.2.1',
-              description: 'Description 1.2.1',
-              subRows: [],
-            },
-            {
-              name: 'Row 1.2.2',
-              description: 'Description 1.2.2',
-              subRows: [],
-            },
-            {
-              name: 'Row 1.2.3',
-              description: 'Description 1.2.3',
-              subRows: [],
-            },
-            {
-              name: 'Row 1.2.4',
-              description: 'Description 1.2.4',
-              subRows: [],
-            },
-          ],
-        },
-        { name: 'Row 1.3', description: 'Description 1.3', subRows: [] },
-        { name: 'Row 1.4', description: 'Description 1.4', subRows: [] },
-      ],
-    },
-    {
-      name: 'Row 2',
-      description: 'Description 2',
-      subRows: [
-        { name: 'Row 2.1', description: 'Description 2.1', subRows: [] },
-        { name: 'Row 2.2', description: 'Description 2.2', subRows: [] },
-        { name: 'Row 2.3', description: 'Description 2.3', subRows: [] },
-      ],
-    },
-    { name: 'Row 3', description: 'Description 3', subRows: [] },
-  ];
-
-  return (
-    <Table
-      emptyTableContent='No data.'
-      isSelectable
-      selectSubRows={false}
-      data={data}
-      columns={columns}
-    />
+    <>
+      <Button>Hello world</Button>
+    </>
   );
 };
+
+export default App;
