e9d0f32f23c9027ff2d85bd9003dcd0d8761c582
Mayank <9084735+mayank99@users.noreply.github.com>
2024-06-12 13:50:03 -0400

    replace `tsc` with `swc` for compiling TSX files

---
+++ .changeset/lemon-kings-type.md
@@ -0,0 +1,5 @@
+---
+'@itwin/itwinui-react': minor
+---
+
+Replaced dependency on `tslib` with `@swc/helpers`. This is used mainly by the CommonJS build.
---
+++ packages/itwinui-react/scripts/build.mjs
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { execSync } from 'node:child_process';
+
+// https://swc.rs/docs/usage/cli
+const swcOptions = {
+  shared: [
+    '--strip-leading-paths',
+    '--no-swcrc',
+    '--ignore **/*.test.*,**/styles.js/*,**/styles*',
+  ].join(' '),
+
+  // https://swc.rs/docs/configuration/compilation
+  compilerOptions: [
+    '',
+    'jsc.parser.syntax=typescript',
+    'jsc.parser.tsx=true',
+    'jsc.transform.react.useBuiltins=true',
+    'jsc.target=es2020',
+    'jsc.minify.format.comments=false',
+    'jsc.externalHelpers=true',
+    'module.noInterop=true',
+  ].join(' -C '),
+
+  get esm() {
+    return [this.shared, this.compilerOptions, '-C module.type=es6'].join(' ');
+  },
+
+  get cjs() {
+    return [this.shared, this.compilerOptions, '-C module.type=commonjs'].join(
+      ' ',
+    );
+  },
+};
+
+execSync(`pnpm swc src -d esm ${swcOptions.esm}`);
+console.log('✓ Built esm.');
+
+execSync(`pnpm swc src -d cjs ${swcOptions.cjs}`);
+console.log('✓ Built cjs.');
