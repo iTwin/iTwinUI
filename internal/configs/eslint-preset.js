e3ec1a595dad7de318bca15d77ed817af4d7a40e
Mayank <9084735+mayank99@users.noreply.github.com>
2024-06-13 09:56:55 -0400

    disable using eslint plugin

--- internal/configs/eslint-preset.js
+++ internal/configs/eslint-preset.js
@@ -19,6 +19,7 @@ module.exports = {
   plugins: [],
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'plugin:react-hooks/recommended',
     'prettier',
@@ -42,7 +43,5 @@ module.exports = {
     curly: 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-restricted-imports': ['error', '..'],
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
   },
 };
