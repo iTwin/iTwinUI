33c571c84d9692bb075f8f8cfeed94aec68bab82
Rohan Kadkol <45748283+rohan-kadkol@users.noreply.github.com>
2024-06-17 15:49:41 -0400

    Added PanelHeader, PanelBackButton. Addressed TODOs.

--- packages/itwinui-react/src/core/Panel/Panel.tsx
+++ packages/itwinui-react/src/core/Panel/Panel.tsx
@@ -6,8 +6,16 @@ import * as React from 'react';
 import {
   Box,
   cloneElementWithRef,
+  getFocusableElements,
+  mergeEventHandlers,
+  SvgChevronLeft,
+  useMergedRefs,
   type PolymorphicForwardRefComponent,
 } from '../../utils/index.js';
+import { flushSync } from 'react-dom';
+import { IconButton } from '../Buttons/IconButton.js';
+import { Flex } from '../Flex/Flex.js';
+import { Text } from '../Typography/Text.js';
 
 type PanelWrapperProps = {
   defaultActiveId: string;
@@ -27,7 +35,7 @@ const PanelWrapper = React.forwardRef((props, forwardedRef) => {
   const { defaultActiveId, children, ...rest } = props;
 
   const [expandedId, setExpandedId] = React.useState(defaultActiveId);
-  const [triggers, setTriggers] = React.useState(
+  const triggers = React.useRef(
     new Map<string, { triggerId: string; panelId: string }>(),
   );
 
@@ -41,7 +49,7 @@ const PanelWrapper = React.forwardRef((props, forwardedRef) => {
         expandedId,
         setExpandedId,
         triggers,
-        setTriggers,
+        // setTriggers,
       }}
     >
       <Box ref={forwardedRef} {...rest}>
@@ -56,12 +64,15 @@ const PanelWrapperContext = React.createContext<
   | {
       expandedId: string | undefined;
       setExpandedId: React.Dispatch<React.SetStateAction<string | undefined>>;
-      triggers: Map<string, { triggerId: string; panelId: string }>;
-      setTriggers: React.Dispatch<
-        React.SetStateAction<
-          Map<string, { triggerId: string; panelId: string }>
-        >
+      triggers: React.RefObject<
+        Map<string, { triggerId: string; panelId: string }>
       >;
+      // triggers: Map<string, { triggerId: string; panelId: string }>;
+      // setTriggers: React.Dispatch<
+      //   React.SetStateAction<
+      //     Map<string, { triggerId: string; panelId: string }>
+      //   >
+      // >;
     }
   | undefined
 >(undefined);
@@ -73,9 +84,24 @@ type PanelProps = {
 };
 
 const Panel = React.forwardRef((props, forwardedRef) => {
-  const { id, ...rest } = props;
+  const { id: idProp, ...rest } = props;
+
+  const fallbackId = React.useId();
+  const id = idProp || fallbackId;
 
   const { expandedId } = React.useContext(PanelWrapperContext) || {};
+
+  // React.useEffect(() => {
+  //   // Every 1 second, focus itself
+  //   const interval = setInterval(() => {
+  //     if (id === expandedId) {
+  //       console.log('focused', id, document.getElementById(id));
+  //       document.getElementById(id)?.focus();
+  //     }
+  //   }, 1000);
+
+  //   return () => clearInterval(interval);
+  // }, [id, expandedId]);
 
   return (
     <PanelContext.Provider
@@ -83,7 +109,18 @@ const Panel = React.forwardRef((props, forwardedRef) => {
         id,
       }}
     >
-      <div ref={forwardedRef} id={id} hidden={id !== expandedId} {...rest} />
+      <Box
+        ref={useMergedRefs(
+          forwardedRef,
+          React.useCallback(
+            (el) => el?.focus(),
+            [],
+          ) as React.RefCallback<HTMLElement>,
+        )}
+        id={id}
+        hidden={id !== expandedId}
+        {...rest}
+      />
     </PanelContext.Provider>
   );
 }) as PolymorphicForwardRefComponent<'div', PanelProps>;
@@ -100,50 +137,130 @@ const PanelContext = React.createContext<
 
 type PanelTriggerProps = {
   for: string;
+  children: React.ReactElement;
 };
 
-const PanelTrigger = React.forwardRef((props, forwardedRef) => {
-  const { children, for: forProp, ...rest } = props;
+const PanelTrigger = (props: PanelTriggerProps) => {
+  const { children, for: forProp } = props;
+
+  const triggerFallbackId = React.useId();
+
+  const panelWrapperContext = React.useContext(PanelWrapperContext);
+  const panelContext = React.useContext(PanelContext);
 
   // const children = React.Children.only(childrenProp);
   if (!React.isValidElement(children)) {
     return null;
   }
 
-  const triggerFallbackId = React.useId();
   const triggerId = children?.props?.id || triggerFallbackId;
-
-  const panelWrapperContext = React.useContext(PanelWrapperContext);
-  const panelContext = React.useContext(PanelContext);
 
   // const [expandedId, setExpandedId] = useAtom(expandedIdAtom);
   // const [triggers, setTriggers] = useAtom(triggersAtom);
   // const panelId = React.useContext(PanelIdContext);
 
+  const triggersMatch = panelWrapperContext?.triggers.current?.get(forProp);
   if (
-    panelWrapperContext?.triggers.get(forProp)?.triggerId !== triggerId &&
-    panelContext?.id
+    !!panelContext?.id &&
+    (panelContext?.id !== triggersMatch?.panelId ||
+      triggerId !== triggersMatch?.triggerId)
   ) {
-    panelWrapperContext?.setTriggers(
-      new Map(
-        panelWrapperContext?.triggers.set(forProp, {
-          triggerId,
-          panelId: panelContext?.id,
-        }),
-      ),
-    );
+    panelWrapperContext?.triggers.current?.set(forProp, {
+      triggerId,
+      panelId: panelContext?.id,
+    });
   }
+
+  const onClick = () => {
+    // Only if forProp exists in the DOM, go to the new panel.
+    // This prevents empty panels with no way to go back.
+    if (!!document.getElementById(forProp)) {
+      flushSync(() => panelWrapperContext?.setExpandedId(forProp));
+
+      // Focus the first focusable element in the panel.
+      // This is useful for screen-reader users.
+
+      // Wait for the panel to be expanded before focusing. i.e. since setExpandedId triggers a re-render.
+      // setTimeout(() => {
+      //   console.log('focus', forProp, document.getElementById(forProp));
+      //   document.getElementById(forProp)?.focus();
+      // }, 1000);
+
+      const firstFocusable = getFocusableElements(
+        document.getElementById(forProp),
+      )?.[0] as HTMLElement | undefined;
+      firstFocusable?.focus();
+    }
+  };
 
   return cloneElementWithRef(children, (children) => {
     return {
+      ...children.props,
       id: triggerId,
-      onClick: () => panelWrapperContext?.setExpandedId(forProp),
+      onClick: mergeEventHandlers(children.props.onClick, onClick),
       'aria-expanded': panelWrapperContext?.expandedId === forProp,
       'aria-controls': forProp,
     };
   });
-  // TODO: I think only the components that return Box should use PolymorphicForwardRefComponent as the type
-}) as PolymorphicForwardRefComponent<'div', PanelTriggerProps>;
+};
+
+// ----------------------------------------------------------------------------
+
+const PanelHeader = React.forwardRef((props, forwardedRef) => {
+  const { children, ...rest } = props;
+
+  return (
+    <Flex ref={forwardedRef} {...rest}>
+      <PanelBackButton />
+      <Text
+        as='h2'
+        tabIndex={-1}
+        // TODO: Confirm that focus moves correctly to the Text after the next panel is opened.
+        // When a keyboard user triggers the panel, they should be able to continue tabbing into the panel.
+        // When a screen-reader user triggers the panel, they should hear the name of the panel announced.
+        //
+        // Alternate idea: maybe the Panel itself could be focused. But then the panel needs a role and a label.
+        ref={React.useCallback((el: HTMLElement | null) => el?.focus(), [])}
+      >
+        {children}
+      </Text>
+    </Flex>
+  );
+}) as PolymorphicForwardRefComponent<'div'>;
+
+const PanelBackButton = () => {
+  // const setExpandedId = useSetAtom(expandedIdAtom);
+  // const panelId = React.useContext(PanelIdContext);
+  // const trigger = useAtomValue(triggersAtom).get(panelId);
+
+  const panelWrapperContext = React.useContext(PanelWrapperContext);
+  const panelContext = React.useContext(PanelContext);
+
+  const { setExpandedId, triggers } = panelWrapperContext || {};
+  const { id: panelId } = panelContext || {};
+
+  const trigger = !!panelId ? triggers?.current?.get(panelId) : undefined;
+
+  const goBack = () => {
+    flushSync(() => setExpandedId?.(trigger?.panelId));
+
+    if (trigger?.triggerId) {
+      document.getElementById(trigger?.triggerId)?.focus();
+    }
+  };
+
+  return (
+    <IconButton
+      label='Back'
+      styleType='borderless'
+      onClick={goBack}
+      size='small'
+      data-iui-shift='left'
+    >
+      <SvgChevronLeft />
+    </IconButton>
+  );
+};
 
 // ----------------------------------------------------------------------------
 
@@ -151,4 +268,6 @@ export const Panels = {
   Wrapper: PanelWrapper,
   Panel,
   Trigger: PanelTrigger,
+  Header: PanelHeader,
+  BackButton: PanelBackButton,
 };
--- playgrounds/next/pages/index.tsx
+++ playgrounds/next/pages/index.tsx
@@ -10,7 +10,11 @@ export default function Home() {
 
   return (
     <>
-      <Panels.Wrapper as={Surface} defaultActiveId={basePanelId}>
+      <Panels.Wrapper
+        as={Surface}
+        defaultActiveId={basePanelId}
+        style={{ display: 'inline-block' }}
+      >
         <Panels.Panel as={List} id={basePanelId}>
           <ListItem>Repeat</ListItem>
           <ListItem>
@@ -18,16 +22,56 @@ export default function Home() {
               <ListItem.Action>Quality</ListItem.Action>
             </Panels.Trigger>
           </ListItem>
-          <ListItem>Speed</ListItem>
-          <ListItem>Accessibility</ListItem>
+          <ListItem>
+            <Panels.Trigger for={speedPanelId}>
+              <ListItem.Action>Speed</ListItem.Action>
+            </Panels.Trigger>
+          </ListItem>
+          <ListItem>
+            <Panels.Trigger for={accessibilityPanelId}>
+              <ListItem.Action>Accessibility</ListItem.Action>
+            </Panels.Trigger>
+          </ListItem>
         </Panels.Panel>
 
         <Panels.Panel as={List} id={qualityPanelId}>
+          <Surface.Header as={Panels.Header}>Quality</Surface.Header>
           <ListItem>240p</ListItem>
           <ListItem>360p</ListItem>
           <ListItem>480p</ListItem>
           <ListItem>720p</ListItem>
           <ListItem>1080p</ListItem>
+        </Panels.Panel>
+
+        {/* TODO: Remove the temp _iui3-menu class */}
+        <Panels.Panel as={List} id={speedPanelId} className='_iui3-menu'>
+          <Surface.Header as={Panels.Header}>Speed</Surface.Header>
+          <ListItem>0.2x</ListItem>
+          <ListItem>0.3x</ListItem>
+          <ListItem>0.4x</ListItem>
+          <ListItem>0.5x</ListItem>
+          <ListItem>0.6x</ListItem>
+          <ListItem>0.7x</ListItem>
+          <ListItem>0.8x</ListItem>
+          <ListItem>0.9x</ListItem>
+          <ListItem>1.0x</ListItem>
+          <ListItem>1.1x</ListItem>
+          <ListItem>1.2x</ListItem>
+          <ListItem>1.3x</ListItem>
+          <ListItem>1.4x</ListItem>
+          <ListItem>1.5x</ListItem>
+          <ListItem>1.6x</ListItem>
+          <ListItem>1.7x</ListItem>
+          <ListItem>1.8x</ListItem>
+          <ListItem>1.9x</ListItem>
+          <ListItem>2.0x</ListItem>
+        </Panels.Panel>
+
+        <Panels.Panel as={List} id={accessibilityPanelId}>
+          <Surface.Header as={Panels.Header}>Accessibility</Surface.Header>
+          <ListItem>High contrast</ListItem>
+          <ListItem>Large text</ListItem>
+          <ListItem>Screen reader</ListItem>
         </Panels.Panel>
       </Panels.Wrapper>
     </>
