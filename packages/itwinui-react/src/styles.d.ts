3b5fe97caf19d4314e0f91645c9ec6b9cdb1c8d8
Mayank <9084735+mayank99@users.noreply.github.com>
2024-06-20 12:56:24 -0400

    export `meta` object containing version and module (#2107)

--- packages/itwinui-react/src/styles.d.ts
+++ packages/itwinui-react/src/styles.d.ts
@@ -2,4 +2,5 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-export default styles as Record<string, string>;
+export declare const styles: Record<string, string>;
+export declare const version: string;
