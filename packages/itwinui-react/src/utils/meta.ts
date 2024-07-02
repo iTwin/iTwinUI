2a1a6b5389acdff9d2fbaf08de19ea3935762665
Mayank <9084735+mayank99@users.noreply.github.com>
2024-07-02 10:18:17 -0400

    fix `module` variable in CJS (#2124)

--- packages/itwinui-react/src/utils/meta.ts
+++ packages/itwinui-react/src/utils/meta.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 import { version } from '../styles.js';
 
-const module: 'ESM' | 'CJS' =
+const _moduleType: 'ESM' | 'CJS' =
   // @ts-expect-error: __module gets injected by SWC
   typeof __module !== 'undefined' ? __module : 'DEV';
 
@@ -13,5 +13,5 @@ export const meta = {
   /** The exact version of `@itwin/itwinui-react`. */
   version,
   /** The current module format (i.e. "ESM" vs "CJS"). */
-  module,
+  module: _moduleType,
 };
