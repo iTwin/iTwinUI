286b9f307563edb49ac5e9d6846c20e6ca98b31d
Ben.Pusey <Ben.Pusey@bentley.com>
2024-07-05 15:43:19 -0400

    Updates Table with new useVirtualScroll implementation, adds align center and edits scroll e2e test to check for visibility of the row being scrolled to.

--- packages/itwinui-react/src/core/Table/Table.tsx
+++ packages/itwinui-react/src/core/Table/Table.tsx
@@ -72,7 +72,7 @@ import {
   onTableResizeStart,
 } from './actionHandlers/index.js';
 import { SELECTION_CELL_ID } from './columns/index.js';
-import type { VirtualItem, Virtualizer } from '@tanstack/react-virtual';
+import { Virtualizer, type VirtualItem } from '@tanstack/react-virtual';
 
 const singleRowSelectedAction = 'singleRowSelected';
 const shiftRowSelectedAction = 'shiftRowSelected';
@@ -831,9 +831,14 @@ export const Table = <
     count: page.length,
     getScrollElement: () => tableRef.current,
     estimateSize: () => rowHeight,
-    scrollToIndex,
     getItemKey: (index) => page[index].id,
   });
+
+  useLayoutEffect(() => {
+    if (scrollToIndex) {
+      virtualizer.scrollToIndex(scrollToIndex, { align: 'center' });
+    }
+  }, [virtualizer, scrollToIndex]);
 
   const getPreparedRow = React.useCallback(
     (
