b4eb444924292c0a5a876aa9b25863cf30a9648b
Mayank <9084735+mayank99@users.noreply.github.com>
2024-07-09 11:53:41 -0400

    create `FieldsetBase` component

---
+++ .changeset/mean-hornets-attend.md
@@ -0,0 +1,5 @@
+---
+'@itwin/itwinui-css': minor
+---
+
+Added a new `iui-fieldset-base` class that removes default `<fieldset>` styles.
---
+++ packages/itwinui-react/src/utils/components/FieldsetBase.tsx
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { polymorphic } from '../functions/polymorphic.js';
+
+export const FieldsetBase = polymorphic.fieldset('iui-fieldset-base');
