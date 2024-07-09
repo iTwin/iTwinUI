0f6fe3afa1d63fb1117aa7b1f9f4c05fc23b54ee
Mayank <9084735+mayank99@users.noreply.github.com>
2024-07-09 12:02:22 -0400

    jsdocs

--- packages/itwinui-react/src/utils/components/FieldsetBase.tsx
+++ packages/itwinui-react/src/utils/components/FieldsetBase.tsx
@@ -4,4 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 import { polymorphic } from '../functions/polymorphic.js';
 
+/**
+ * Thin wrapper over the `<fieldset>` element that removes its default styles.
+ *
+ * @private
+ */
 export const FieldsetBase = polymorphic.fieldset('iui-fieldset-base');
