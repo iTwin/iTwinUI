c772b20fed2d49e9947b49a93e1290a8447ea0dd
Ben.Pusey <Ben.Pusey@bentley.com>
2024-07-12 09:57:34 -0400

    Removes changes to vite playground.

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,54 +1,11 @@
-import { Table } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function App() {
+const App = () => {
   return (
     <>
-      <Table
-        columns={[
-          {
-            Header: '#',
-            accessor: 'index',
-            width: 100,
-          },
-          {
-            Header: 'Name',
-            accessor: 'name',
-          },
-          {
-            Header: 'Description',
-            accessor: 'description',
-            width: '200px',
-          },
-        ]}
-        data={data}
-        emptyTableContent='No data.'
-        isResizable
-      />
+      <Button>Hello world</Button>
     </>
   );
-}
+};
 
-const data = [
-  {
-    index: 1,
-    name: 'Name1',
-    description: 'Description1',
-  },
-  {
-    index: 2,
-    name: 'Name2',
-    description: 'Description2',
-  },
-  {
-    index: 3,
-    name: 'Name3',
-    description: 'Description3',
-    subRows: [
-      {
-        index: 4,
-        name: 'Name3.1',
-        description: 'Description3.1',
-      },
-    ],
-  },
-];
+export default App;
