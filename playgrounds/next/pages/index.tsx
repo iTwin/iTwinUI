bca632a4d78c2909f0fd65471475357f593d293f
Rohan <45748283+rohan-kadkol@users.noreply.github.com>
2024-07-16 07:12:06 -0400

    Reverted playgrounds

--- playgrounds/next/pages/index.tsx
+++ playgrounds/next/pages/index.tsx
@@ -1,31 +1,9 @@
-import { ComboBox } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function App() {
-  const data: { label: string; value: number }[] = [];
-  for (let i = 0; i < 15; i++) {
-    data.push({ label: `option ${i}`, value: i });
-  }
-  const widths = [];
-  for (let i = 0; i < 20; i++) {
-    widths.push(790 + i * 3);
-  }
-
+export default function Home() {
   return (
     <>
-      {widths.slice(0, 1).map((width) => (
-        <ComboBox
-          key={width}
-          style={{ width: `${width}px`, maxWidth: '80vw' }}
-          enableVirtualization={false}
-          multiple={true}
-          options={data}
-          value={data.map((x) => x.value)}
-          onChange={() => {}}
-          inputProps={{
-            placeholder: 'Placeholder',
-          }}
-        />
-      ))}
+      <Button>Hello world</Button>
     </>
   );
 }
--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,58 +1,11 @@
-import { SvgPlaceholder } from '@itwin/itwinui-icons-react';
-import { ButtonGroup, ComboBox, IconButton } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function App() {
-  const data: { label: string; value: number }[] = [];
-  for (let i = 0; i < 15; i++) {
-    data.push({
-      label: `option ${i}`,
-      value: i,
-    });
-  }
-  const widths = [];
-  for (let i = 0; i < 20; i++) {
-    widths.push(790 + i * 3);
-  }
-
+const App = () => {
   return (
     <>
-      {/* {widths.slice(0, 1).map((width) => (
-        <ComboBox
-          key={width}
-          style={{ width: `${width}px`, maxWidth: '80vw' }}
-          enableVirtualization={false}
-          multiple={true}
-          options={data}
-          defaultValue={data.map((x) => x.value)}
-          onChange={() => {}}
-          inputProps={{
-            placeholder: 'Placeholder',
-          }}
-        />
-      ))} */}
-      <div
-        id='container'
-        style={{ resize: 'block', overflow: 'hidden', padding: '5px' }}
-      >
-        <ButtonGroup
-          role='toolbar'
-          orientation={'vertical'}
-          style={{ height: '100%' }}
-          overflowButton={(firstOverflowingIndex) => {
-            return <IconButton>{firstOverflowingIndex}</IconButton>;
-          }}
-        >
-          <IconButton label='Button 1'>
-            <SvgPlaceholder />
-          </IconButton>
-          <IconButton label='Button 2' isActive>
-            <SvgPlaceholder />
-          </IconButton>
-          <IconButton label='Button 3'>
-            <SvgPlaceholder />
-          </IconButton>
-        </ButtonGroup>
-      </div>
+      <Button>Hello world</Button>
     </>
   );
-}
+};
+
+export default App;
