58cd05f90ebcf3dc949a38bc3f03b5a08e1c9ac3
Mayank <9084735+mayank99@users.noreply.github.com>
2024-07-23 15:39:44 -0400

    return shared CSS from `useVirtualScroll`

--- packages/itwinui-react/src/core/Table/Table.tsx
+++ packages/itwinui-react/src/core/Table/Table.tsx
@@ -829,7 +829,7 @@ export const Table = <
     }
   });
 
-  const virtualizer = useVirtualScroll({
+  const { virtualizer, css: virtualizerCss } = useVirtualScroll({
     count: page.length,
     getScrollElement: () => tableRef.current,
     estimateSize: () => rowHeight,
@@ -1152,15 +1152,11 @@ export const Table = <
             (isSelectable && selectionMode === 'multi') || undefined
           }
         >
-          <ShadowRoot>
+          <ShadowRoot css={virtualizerCss}>
             {enableVirtualization && data.length !== 0 ? (
               <div
-                style={{
-                  minBlockSize: virtualizer.getTotalSize(),
-                  minInlineSize: '100%',
-                  contain: 'layout',
-                  position: 'relative',
-                }}
+                data-iui-virtualizer='root'
+                style={{ minBlockSize: virtualizer.getTotalSize() }}
               >
                 <slot />
               </div>
