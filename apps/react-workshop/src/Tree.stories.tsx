a8422d55da7468af79268f46d88699c58bd11fd3
Ben.Pusey <Ben.Pusey@bentley.com>
2024-07-24 14:30:16 -0400

    Adds story for virtualized tree with horizontal scroll and react test images for the new story and virtualized tree.

--- apps/react-workshop/src/Tree.stories.tsx
+++ apps/react-workshop/src/Tree.stories.tsx
@@ -682,3 +682,122 @@ export const Small = () => {
     />
   );
 };
+
+export const VirtualizedWithHorizontalScroll = () => {
+  type StoryData = {
+    id: string;
+    label: string;
+    sublabel: string;
+    subItems: StoryData[];
+  };
+
+  const [selectedNodes, setSelectedNodes] = useState<Record<string, boolean>>({
+    'Node-0': true,
+    'Node-3-2': true,
+    'Node-22': true,
+  });
+  const onSelectedNodeChange = useCallback(
+    (nodeId: string, isSelected: boolean) => {
+      if (isSelected) {
+        setSelectedNodes((oldSelected) => ({ ...oldSelected, [nodeId]: true }));
+        console.log(`Selected node ${nodeId}`);
+      } else {
+        setSelectedNodes((oldSelected) => ({
+          ...oldSelected,
+          [nodeId]: false,
+        }));
+        console.log(`Unselected node ${nodeId}`);
+      }
+    },
+    [],
+  );
+
+  const [expandedNodes, setExpandedNodes] = useState<Record<string, boolean>>({
+    'Node-2': true,
+    'Node-2-1': true,
+    'Node-3': true,
+  });
+  const onNodeExpanded = useCallback((nodeId: string, isExpanded: boolean) => {
+    if (isExpanded) {
+      setExpandedNodes((oldExpanded) => ({ ...oldExpanded, [nodeId]: true }));
+      console.log(`Expanded node ${nodeId}`);
+    } else {
+      setExpandedNodes((oldExpanded) => ({
+        ...oldExpanded,
+        [nodeId]: false,
+      }));
+      console.log(`Closed node ${nodeId}`);
+    }
+  }, []);
+
+  const [disabledNodes] = useState<Record<string, boolean>>({
+    'Node-4': true,
+    'Node-3-0': true,
+    'Node-6': true,
+    'Node-10': true,
+  });
+
+  const generateItem = useCallback(
+    (index: number, parentNode = '', depth = 0): StoryData => {
+      const keyValue = parentNode ? `${parentNode}-${index}` : `${index}`;
+      return {
+        id: `Node-${keyValue}`,
+        label: `Node ${keyValue}`,
+        sublabel: `Sublabel for Node ${keyValue}`,
+        subItems:
+          depth < 10
+            ? Array(Math.round(index % 5))
+                .fill(null)
+                .map((_, index) => generateItem(index, keyValue, depth + 1))
+            : [],
+      };
+    },
+    [],
+  );
+
+  const data = useMemo(
+    () =>
+      Array(100000)
+        .fill(null)
+        .map((_, index) => generateItem(index)),
+    [generateItem],
+  );
+
+  const getNode = useCallback(
+    (node: StoryData): NodeData<StoryData> => {
+      return {
+        subNodes: node.subItems,
+        nodeId: node.id,
+        node: node,
+        isExpanded: expandedNodes[node.id],
+        isDisabled: disabledNodes[node.id],
+        isSelected: selectedNodes[node.id],
+        hasSubNodes: node.subItems.length > 0,
+      };
+    },
+    [disabledNodes, expandedNodes, selectedNodes],
+  );
+
+  return (
+    <Tree<StoryData>
+      data={data}
+      getNode={getNode}
+      enableVirtualization
+      nodeRenderer={useCallback(
+        ({ node, ...rest }) => (
+          <TreeNode
+            label={node.label}
+            sublabel={node.sublabel}
+            onExpanded={onNodeExpanded}
+            onSelected={onSelectedNodeChange}
+            checkbox={<Checkbox variant='eyeball' disabled={rest.isDisabled} />}
+            icon={<SvgPlaceholder />}
+            {...rest}
+          />
+        ),
+        [onNodeExpanded, onSelectedNodeChange],
+      )}
+      style={{ height: 'min(400px, 90vh)', width: '250px' }}
+    />
+  );
+};
