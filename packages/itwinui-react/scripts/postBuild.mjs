92adbb30741163c88b90a2b17588748f6900699e
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-16 13:46:53 -0400

    add missing `"type": "commonjs"` to `DEV-cjs` output

--- packages/itwinui-react/scripts/postBuild.mjs
+++ packages/itwinui-react/scripts/postBuild.mjs
@@ -5,11 +5,12 @@
 import fs from 'node:fs';
 import { execSync } from 'node:child_process';
 
-// This creates a dummy package.json file in the cjs folder.
+// This creates a dummy package.json file in the cjs and DEV-cjs folders.
 // Without this, all cjs files would need the .cjs extension
 // because .js files are treated as ESM according to the main package.json's "type".
 try {
   fs.writeFileSync('cjs/package.json', '{ "type": "commonjs" }');
+  fs.writeFileSync('DEV-cjs/package.json', '{ "type": "commonjs" }');
 } catch (e) {
   console.error('Cannot create cjs/package.json', e);
 }
@@ -17,7 +18,7 @@ try {
 // Run prettier on all compiled output because it gets jumbled by tsc.
 try {
   execSync(
-    'npx prettier --write --ignore-path="../../.gitignore" "{esm,cjs}/**/*.js"',
+    'npx prettier --write --ignore-path="../../.gitignore" "{esm,cjs,DEV-esm,DEV-cjs}/**/*.js"',
   );
 } catch (error) {
   console.error('Error when running prettier', error);
