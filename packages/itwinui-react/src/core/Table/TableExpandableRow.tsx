34691ccaf2a2c9c1b65d645de68f3d3f1ec339f7
Uyen Doan <uyen.doan@bentley.com>
2024-08-26 13:34:45 -0500

    added copyright

--- packages/itwinui-react/src/core/Table/TableExpandableRow.tsx
+++ packages/itwinui-react/src/core/Table/TableExpandableRow.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import cx from 'classnames';
 import * as React from 'react';
 import type { Row } from '../../react-table/react-table.js';
