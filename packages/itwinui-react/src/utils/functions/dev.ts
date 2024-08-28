f93059b1dd6a96edff923681f32b999382aeebeb
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-28 12:45:20 -0400

    create `useWarningLogger` hook

--- packages/itwinui-react/src/utils/functions/dev.ts
+++ packages/itwinui-react/src/utils/functions/dev.ts
@@ -3,6 +3,8 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
+import * as React from 'react';
+
 // @ts-expect-error -- jest is not used in iTwinUI but may be used in consuming apps
 const isJest = typeof jest !== 'undefined';
 
@@ -14,20 +16,6 @@ const isVitest = typeof (globalThis as any).__vitest_index__ !== 'undefined';
 
 const isUnitTest = isJest || isVitest || isMocha;
 
-/**
- * Returns a function that can be used to log one-time warnings in dev environments.
- *
- * **Note**: The actual log call should be wrapped in a check against `process.env.NODE_ENV === 'development'`
- * to ensure that it is removed from the production build output (by SWC).
- * Read more about the [`NODE_ENV` convention](https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production).
- *
- * @example
- * const logWarning = createWarningLogger();
- *
- * if (process.env.NODE_ENV === 'development') {
- *   logWarning("please don't use this")
- * }
- */
 const createWarningLogger =
   process.env.NODE_ENV === 'development' && !isUnitTest
     ? () => {
@@ -41,4 +29,23 @@ const createWarningLogger =
       }
     : () => () => {};
 
-export { isUnitTest, createWarningLogger };
+/**
+ * Returns a function that can be used to log one-time warnings in dev environments.
+ *
+ * **Note**: The actual log call should be wrapped in a check against `process.env.NODE_ENV === 'development'`
+ * to ensure that it is removed from the production build output (by SWC).
+ * Read more about the [`NODE_ENV` convention](https://nodejs.org/en/learn/getting-started/nodejs-the-difference-between-development-and-production).
+ *
+ * @example
+ * const logWarning = useWarningLogger();
+ *
+ * if (process.env.NODE_ENV === 'development') {
+ *   logWarning("please don't use this")
+ * }
+ */
+function useWarningLogger() {
+  const logWarning = React.useMemo(() => createWarningLogger(), []);
+  return logWarning;
+}
+
+export { isUnitTest, useWarningLogger };
