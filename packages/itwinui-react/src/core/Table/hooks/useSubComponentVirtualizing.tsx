645dba60f564d07a90dbe787666587ea3d126d40
Uyen Doan <uyen.doan@bentley.com>
2024-09-11 15:14:17 -0500

    added copyright

--- packages/itwinui-react/src/core/Table/hooks/useSubComponentVirtualizing.tsx
+++ packages/itwinui-react/src/core/Table/hooks/useSubComponentVirtualizing.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import type { Row, TableState } from '../../../react-table/react-table.js';
 
