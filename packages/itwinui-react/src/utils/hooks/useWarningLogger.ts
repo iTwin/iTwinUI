5da53037891d636aaec16c9195c0ff8812f3bfd8
Rohan <45748283+rohan-kadkol@users.noreply.github.com>
2024-09-18 23:00:48 -0400

    Use `getWindow`

--- packages/itwinui-react/src/utils/hooks/useWarningLogger.ts
+++ packages/itwinui-react/src/utils/hooks/useWarningLogger.ts
@@ -5,6 +5,7 @@
 
 import * as React from 'react';
 import { isUnitTest } from '../functions/dev.js';
+import { getWindow } from '../functions/dom.js';
 
 const _React = React as any;
 const ReactInternals =
@@ -28,7 +29,7 @@ export const useWarningLogger =
   process.env.NODE_ENV === 'development' && !isUnitTest
     ? function () {
         const loggedRef = React.useRef(false);
-        const timeoutRef = React.useRef<number | null>(null);
+        const timeoutRef = React.useRef<number | undefined>(undefined);
 
         // https://stackoverflow.com/a/71685253
         const stack =
@@ -43,13 +44,8 @@ export const useWarningLogger =
 
         const logWarning = React.useCallback(
           (message: string) => {
-            // Bail if window not available (e.g. on server)
-            if (typeof window === 'undefined') {
-              return;
-            }
-
             // Using setTimeout to delay execution until after rendering is complete.
-            timeoutRef.current = window.setTimeout(() => {
+            timeoutRef.current = getWindow()?.setTimeout(() => {
               if (!loggedRef.current) {
                 console.error(prefix, message);
                 loggedRef.current = true;
@@ -63,13 +59,8 @@ export const useWarningLogger =
           // Clearing timeout on unmount to avoid double execution in StrictMode.
           // The warning should be logged only once per component instance.
           return () => {
-            // Bail if window not available (e.g. on server)
-            if (typeof window === 'undefined') {
-              return;
-            }
-
             if (timeoutRef.current) {
-              window.clearTimeout(timeoutRef.current);
+              getWindow()?.clearTimeout(timeoutRef.current);
             }
           };
         }, []);
