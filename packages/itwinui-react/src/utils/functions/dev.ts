c82c3b933ed00984b31749abde2a5c1df2e8fe5c
Rohan <45748283+r100-stack@users.noreply.github.com>
2024-09-30 07:56:45 -0400

    Fix

--- packages/itwinui-react/src/utils/functions/dev.ts
+++ packages/itwinui-react/src/utils/functions/dev.ts
@@ -6,10 +6,12 @@
 // @ts-expect-error -- jest is not used in iTwinUI but may be used in consuming apps
 const isJest = typeof jest !== 'undefined';
 
+const isCypress = typeof (globalThis as any).Cypress !== 'undefined';
 const isMocha =
   typeof (globalThis as any).beforeEach !== 'undefined' &&
   `${(globalThis as any).beforeEach}`.replace(/\s/g, '') ===
-    'function(name,fn){suites[0].beforeEach(name,fn);}';
+    'function(name,fn){suites[0].beforeEach(name,fn);}' &&
+  !isCypress;
 const isVitest = typeof (globalThis as any).__vitest_index__ !== 'undefined';
 
 const isUnitTest = isJest || isVitest || isMocha;
