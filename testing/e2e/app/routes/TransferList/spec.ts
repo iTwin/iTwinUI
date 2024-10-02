3ee1ba6d10c030888e6633e0590c2eb5f09d7479
Rohan <45748283+r100-stack@users.noreply.github.com>
2024-10-02 11:52:21 -0400

    Better tests

--- testing/e2e/app/routes/TransferList/spec.ts
+++ testing/e2e/app/routes/TransferList/spec.ts
@@ -1,42 +1,62 @@
 import { test, expect } from '@playwright/test';
 
-test('should support toolbar keyboard navigation in TransferList.Toolbar', async ({
-  page,
-}) => {
-  await page.goto('/TransferList');
-  const buttons = page.locator('button');
-  const listboxes = page.getByRole('listbox');
+test.describe('toolbar pattern in TransferList.Toolbar', () => {
+  test("should have role='toolbar'", async ({ page }) => {
+    await page.goto('/TransferList');
+    const toolbar = page.getByRole('toolbar');
+    await expect(toolbar).toHaveCount(1);
+  });
 
-  await page.keyboard.press('Tab');
-  await expect(listboxes.first()).toBeFocused();
+  test('should support toolbar arrow-key keyboard navigation', async ({
+    page,
+  }) => {
+    await page.goto('/TransferList');
+    const listboxes = page.getByRole('listbox');
+    const toolbar = page.getByRole('toolbar');
+    const buttons = toolbar.first().locator('button');
 
-  await page.keyboard.press('Tab');
-  await expect(buttons.nth(0)).toBeFocused();
-  await page.keyboard.press('ArrowDown');
-  await expect(buttons.nth(1)).toBeFocused();
-  await page.keyboard.press('ArrowDown');
-  await expect(buttons.nth(2)).toBeFocused();
-  await page.keyboard.press('ArrowDown');
-  await expect(buttons.nth(3)).toBeFocused();
-  await page.keyboard.press('ArrowDown');
-  await expect(buttons.nth(0)).toBeFocused();
-  await page.keyboard.press('ArrowUp');
-  await expect(buttons.nth(3)).toBeFocused();
-});
+    await page.keyboard.press('Tab');
+    await expect(listboxes.first()).toBeFocused();
 
-test('should have only one tab stop in TransferList.Toolbar (toolbar pattern)', async ({
-  page,
-}) => {
-  await page.goto('/TransferList');
-  const buttons = page.locator('button');
-  const listboxes = page.getByRole('listbox');
+    await page.keyboard.press('Tab');
+    await expect(buttons.nth(0)).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(buttons.nth(1)).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(buttons.nth(2)).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(buttons.nth(3)).toBeFocused();
+    await page.keyboard.press('ArrowDown');
+    await expect(buttons.nth(0)).toBeFocused();
+    await page.keyboard.press('ArrowUp');
+    await expect(buttons.nth(3)).toBeFocused();
+  });
 
-  await page.keyboard.press('Tab');
-  await expect(listboxes.first()).toBeFocused();
+  test('should have only one tab stop and support Tab/Shift+Tab key navigation', async ({
+    page,
+  }) => {
+    await page.goto('/TransferList');
+    const listboxes = page.getByRole('listbox');
+    const toolbar = page.getByRole('toolbar');
+    const buttons = toolbar.first().locator('button');
 
-  await page.keyboard.press('Tab');
-  await expect(buttons.first()).toBeFocused();
+    await page.keyboard.press('Tab');
+    await expect(listboxes.first()).toBeFocused();
 
-  await page.keyboard.press('Tab');
-  await expect(listboxes.last()).toBeFocused();
+    await page.keyboard.press('Tab');
+    await expect(buttons.first()).toBeFocused();
+
+    await page.keyboard.press('Tab');
+    await expect(listboxes.last()).toBeFocused();
+
+    await page.keyboard.down('Shift');
+    await page.keyboard.press('Tab');
+    await page.keyboard.up('Shift');
+    await expect(buttons.first()).toBeFocused();
+
+    await page.keyboard.down('Shift');
+    await page.keyboard.press('Tab');
+    await page.keyboard.up('Shift');
+    await expect(listboxes.first()).toBeFocused();
+  });
 });
