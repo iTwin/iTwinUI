0e0a2bfcb687acc9c845781b07ac7fa50dbfffc4
Uyen Doan <uyen.doan@bentley.com>
2024-10-02 14:19:57 -0500

    added more into isAnimated example

--- examples/ProgressLinear.isAnimated.jsx
+++ examples/ProgressLinear.isAnimated.jsx
@@ -3,21 +3,36 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
-import { ProgressLinear } from '@itwin/itwinui-react';
+import { Button, ProgressLinear } from '@itwin/itwinui-react';
 
 export default () => {
   const [value, setValue] = React.useState(0);
+  const timeoutRef = React.useRef(null);
 
   React.useEffect(() => {
-    setTimeout(() => {
-      const newValue = value + 20;
-      setValue(newValue > 100 ? 0 : newValue);
-    }, 3000);
+    if (value < 100) {
+      timeoutRef.current = setTimeout(
+        () => setValue((prevValue) => prevValue + 20),
+        1500,
+      );
+    } else {
+      clearTimeout(timeoutRef.current);
+    }
+
+    return () => clearTimeout(timeoutRef.current);
   }, [value]);
 
   return (
     <div className='demo-container'>
-      <ProgressLinear value={value} isAnimated />
+      <ProgressLinear
+        value={value}
+        isAnimated
+        labels={
+          value < 100 ? ['Loading...', `${value}%`] : ['Upload succeeded.']
+        }
+        status={value === 100 ? 'positive' : undefined}
+      />
+      <Button onClick={() => setValue(0)}>Reset process</Button>
     </div>
   );
 };
