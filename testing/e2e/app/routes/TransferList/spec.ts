392150924a6ac678f5328e28726229222c06a352
Rohan <45748283+r100-stack@users.noreply.github.com>
2024-10-02 11:17:32 -0400

    e2e test focus checks. new e2e test.

--- testing/e2e/app/routes/TransferList/spec.ts
+++ testing/e2e/app/routes/TransferList/spec.ts
@@ -5,8 +5,10 @@ test('should support toolbar keyboard navigation in TransferList.Toolbar', async
 }) => {
   await page.goto('/TransferList');
   const buttons = page.locator('button');
+  const listboxes = page.getByRole('listbox');
 
   await page.keyboard.press('Tab');
+  await expect(listboxes.first()).toBeFocused();
 
   await page.keyboard.press('Tab');
   await expect(buttons.nth(0)).toBeFocused();
@@ -20,4 +22,21 @@ test('should support toolbar keyboard navigation in TransferList.Toolbar', async
   await expect(buttons.nth(0)).toBeFocused();
   await page.keyboard.press('ArrowUp');
   await expect(buttons.nth(3)).toBeFocused();
+});
+
+test('should have only one tab stop in TransferList.Toolbar (toolbar pattern)', async ({
+  page,
+}) => {
+  await page.goto('/TransferList');
+  const buttons = page.locator('button');
+  const listboxes = page.getByRole('listbox');
+
+  await page.keyboard.press('Tab');
+  await expect(listboxes.first()).toBeFocused();
+
+  await page.keyboard.press('Tab');
+  await expect(buttons.first()).toBeFocused();
+
+  await page.keyboard.press('Tab');
+  await expect(listboxes.last()).toBeFocused();
 });
