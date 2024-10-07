2bae264280dce5f9492f39563ba1796c3cf3fc6d
Uyen Doan <uyen.doan@bentley.com>
2024-10-07 16:26:01 -0500

    made sure item.subRows exist before getting its length

--- packages/itwinui-react/src/core/Table/Table.tsx
+++ packages/itwinui-react/src/core/Table/Table.tsx
@@ -658,8 +658,8 @@ export const Table = <
 
   if (
     process.env.NODE_ENV === 'development' &&
-    data.some((item) => (item.subRows as T[]).length > 0) &&
-    subComponent
+    subComponent &&
+    data.some((item) => !!(item.subRows as T[] | undefined)?.length)
   ) {
     logWarning(
       `Passing both \`subComponent\` and \`data\` with \`subRows\` is not supported. There are features designed for \`subRows\` that are not compatible with \`subComponent\` and vice versa.`,
