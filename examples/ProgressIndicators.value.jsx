a6a24bb6620aa7b833ff8f73756f95bde595b600
Uyen Doan <56598021+smmr-dn@users.noreply.github.com>
2024-10-08 09:12:56 -0500

    Updated documentation for Progress Indicators (#2271)

---
+++ examples/ProgressIndicators.value.jsx
@@ -0,0 +1,15 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import * as React from 'react';
+import { ProgressLinear, ProgressRadial } from '@itwin/itwinui-react';
+
+export default () => {
+  return (
+    <div className='demo-container'>
+      <ProgressLinear value={20} />
+      <ProgressRadial value={50} />
+    </div>
+  );
+};
