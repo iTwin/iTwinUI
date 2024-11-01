866d10f75e0b664de0a98f381e5d3bc9c4a9c2fc
Uyen Doan <uyen.doan@bentley.com>
2024-11-01 14:29:53 -0500

    attempted label a11y error

--- examples/Tree.visibility.jsx
+++ examples/Tree.visibility.jsx
@@ -69,6 +69,7 @@ export default () => {
                 triggerProps={{
                   'aria-label': 'Visibility',
                 }}
+                label=''
                 variant='eyeball'
                 disabled={rest.isDisabled}
               />
