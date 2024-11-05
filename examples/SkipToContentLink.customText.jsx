4df97dffefecb440af35923df348faf0d399695f
Uyen Doan <uyen.doan@bentley.com>
2024-11-05 10:05:52 -0600

    revert example changes

--- examples/SkipToContentLink.customText.jsx
+++ examples/SkipToContentLink.customText.jsx
@@ -7,16 +7,32 @@ import { SkipToContentLink, Text, Kbd, Anchor } from '@itwin/itwinui-react';
 export default () => {
   return (
     <>
-      <SkipToContentLink href='#props'>Go to Props</SkipToContentLink>
+      <SkipToContentLink href='#main-content'>
+        Go to main content
+      </SkipToContentLink>
       <Text variant='body'>
         This is not the main content. Press&nbsp;
         <Kbd>tab</Kbd>&nbsp;to see skip-to-content-link component. You might
         need to click on the top of the page or the URL first. Press&nbsp;
         <Kbd>&crarr;</Kbd>
-        &nbsp;after focusing on skip-to-content-link to skip to the Props
-        section.
-        <br />
+        &nbsp;after focusing on skip-to-content-link to skip to the main content
+        below. <Anchor href='#'>This link will be skipped.</Anchor> <br />
       </Text>
+      <div
+        style={{
+          border: 'solid 1px var(--iui-color-background-border)',
+          height: 1000,
+          padding: '11px 12px',
+        }}
+        id='main-content'
+      >
+        <Text variant='body'>
+          This is the main content. Focus will be directed here from the
+          skip-to-content-link component.&nbsp;
+          <Anchor href='#'>Tab again to focus on this link.</Anchor>
+          &nbsp;
+        </Text>
+      </div>
     </>
   );
 };
--- examples/SkipToContentLink.main.jsx
+++ examples/SkipToContentLink.main.jsx
@@ -7,16 +7,30 @@ import { SkipToContentLink, Text, Kbd, Anchor } from '@itwin/itwinui-react';
 export default () => {
   return (
     <>
-      <SkipToContentLink href='#custom-text' />
+      <SkipToContentLink href='#main-content' />
       <Text variant='body'>
         This is not the main content. Press&nbsp;
         <Kbd>tab</Kbd>&nbsp;to see skip-to-content-link component. You might
         need to click on the top of the page or the URL first. Press&nbsp;
         <Kbd>&crarr;</Kbd>
-        &nbsp;after focusing on skip-to-content-link to skip to the Custom Text
-        section.
-        <br />
+        &nbsp;after focusing on skip-to-content-link to skip to the main content
+        below. <Anchor href='#'>This link will be skipped.</Anchor> <br />
       </Text>
+      <div
+        style={{
+          border: 'solid 1px var(--iui-color-background-border)',
+          height: 1000,
+          padding: '11px 12px',
+        }}
+        id='main-content'
+      >
+        <Text variant='body'>
+          This is the main content. Focus will be directed here from the
+          skip-to-content-link component.&nbsp;
+          <Anchor href='#'>Tab again to focus on this link.</Anchor>
+          &nbsp;
+        </Text>
+      </div>
     </>
   );
 };
