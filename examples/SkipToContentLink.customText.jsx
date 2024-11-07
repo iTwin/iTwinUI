c5f60651d900a0e7db0c9451a55276018e5db45d
Uyen Doan <uyen.doan@bentley.com>
2024-11-07 11:47:11 -0600

    changed examples and wording

--- examples/SkipToContentLink.customText.jsx
+++ examples/SkipToContentLink.customText.jsx
@@ -2,36 +2,18 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Flex, SkipToContentLink, Text } from '@itwin/itwinui-react';
+import { Kbd, SkipToContentLink } from '@itwin/itwinui-react';
 
 export default () => {
   return (
-    <Flex flexDirection='column'>
+    <>
       <SkipToContentLink href='#main-content'>
-        Go to Main Section
+        Go to main section
       </SkipToContentLink>
-      <Text variant='headline'>Page Header</Text>
-
-      <Flex.Item className='demo-section' id='introduction'>
-        <Text variant='subheading'>Introduction</Text>
-        <Text variant='body'>
-          Some introduction text that explains what this page is about.
-        </Text>
-      </Flex.Item>
-      <Flex.Item className='demo-section' id='main-content'>
-        <Text variant='subheading'>Main Section</Text>
-        <Text variant='body'>
-          Some main content that the user is interested in, which should be
-          focused on when the skip link is clicked.
-        </Text>
-      </Flex.Item>
-      <Flex.Item className='demo-section' id='conclusion'>
-        <Text variant='subheading'>Conclusion</Text>
-        <Text variant='body'>
-          Some conclusion text that summarizes what the user has learned from
-          this page.
-        </Text>
-      </Flex.Item>
-    </Flex>
+      <div id='main-content'>
+        To view the skip link, click inside this box then press <Kbd>Enter</Kbd>
+        .
+      </div>
+    </>
   );
 };
