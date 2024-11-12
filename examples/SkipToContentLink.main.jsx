693f5264413046f883b30d338d7b74a1e9f8d2b7
Uyen Doan <uyen.doan@bentley.com>
2024-11-12 14:20:41 -0600

    added css comment and used normal button and kbd

--- examples/SkipToContentLink.main.jsx
+++ examples/SkipToContentLink.main.jsx
@@ -2,13 +2,13 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { Button, Kbd, SkipToContentLink } from '@itwin/itwinui-react';
+import { SkipToContentLink } from '@itwin/itwinui-react';
 
 export default () => {
   return (
     <div className='demo-container'>
       <div>
-        <Button>Click here</Button> then press <Kbd>Tab</Kbd> to see the skip
+        <button>Click here</button> then press <kbd>Tab</kbd> to see the skip
         link.
       </div>
       {/* The SkipToContentLink component should normally be placed at the
