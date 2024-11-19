d869473be2ba95702696b2accdaa5aa1ef61fa9c
Uyen Doan <56598021+smmr-dn@users.noreply.github.com>
2024-11-19 08:27:51 -0600

    Allowed passing customized icon for completed steps in `Stepper` (#2345)

--- packages/itwinui-react/src/core/Stepper/StepperStep.tsx
+++ packages/itwinui-react/src/core/Stepper/StepperStep.tsx
@@ -38,6 +38,10 @@ export type StepperStepProps = {
    */
   description?: string;
   /**
+   * Custom content displayed in the step's circle.
+   */
+  stepContent?: () => React.ReactNode;
+  /**
    * Allows props to be passed for stepper step.
    */
   stepProps?: React.ComponentProps<'li'>;
@@ -70,6 +74,7 @@ export const StepperStep = React.forwardRef((props, forwardedRef) => {
     trackContentProps,
     circleProps,
     nameProps,
+    stepContent,
     ...rest
   } = props;
 
@@ -132,7 +137,7 @@ export const StepperStep = React.forwardRef((props, forwardedRef) => {
           {...circleProps}
           className={cx('iui-stepper-circle', circleProps?.className)}
         >
-          {index + 1}
+          {stepContent ? stepContent() : index + 1}
         </Box>
       </Box>
 
