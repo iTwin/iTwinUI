38610b95dbd7f5c8e236000c246547230f5096af
Uyen Doan <56598021+smmr-dn@users.noreply.github.com>
2024-11-25 15:00:02 -0600

    Created documentation for `MiddleTextTruncation` (#2329)

--- examples/index.tsx
+++ examples/index.tsx
@@ -807,6 +807,26 @@ export { ListComboboxExample };
 
 // ----------------------------------------------------------------------------
 
+import { default as MiddleTextTruncationMainExampleRaw } from './MiddleTextTruncation.main';
+const MiddleTextTruncationMainExample = withThemeProvider(
+  MiddleTextTruncationMainExampleRaw,
+);
+export { MiddleTextTruncationMainExample };
+
+import { default as MiddleTextTruncationEndCharsCountExampleRaw } from './MiddleTextTruncation.endCharsCount';
+const MiddleTextTruncationEndCharsCountExample = withThemeProvider(
+  MiddleTextTruncationEndCharsCountExampleRaw,
+);
+export { MiddleTextTruncationEndCharsCountExample };
+
+import { default as MiddleTextTruncationCustomizationExampleRaw } from './MiddleTextTruncation.customizeTruncation';
+const MiddleTextTruncationCustomizationExample = withThemeProvider(
+  MiddleTextTruncationCustomizationExampleRaw,
+);
+export { MiddleTextTruncationCustomizationExample };
+
+// ----------------------------------------------------------------------------
+
 import { default as NonIdealStateBadgatewayExampleRaw } from './NonIdealState.badgateway';
 const NonIdealStateBadgatewayExample = withThemeProvider(
   NonIdealStateBadgatewayExampleRaw,
