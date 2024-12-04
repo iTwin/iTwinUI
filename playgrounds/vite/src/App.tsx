49a812e6a92a0b357e7f555221212991353e0fbe
Uyen Doan <uyen.doan@bentley.com>
2024-12-04 16:38:37 -0600

    reduced min-block-size

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,11 +1,20 @@
-import { Button } from '@itwin/itwinui-react';
+import { ComboBox, Flex, Select } from '@itwin/itwinui-react';
 
-const App = () => {
+const options = [
+  { value: 1, label: 'Option 1' },
+  { value: 2, label: 'Option 2' },
+  { value: 3, label: 'Option 3' },
+];
+
+export default function App() {
   return (
-    <>
-      <Button>Hello world</Button>
-    </>
+    <Flex flexDirection='row' style={{ border: 'red solid 1px' }}>
+      <ComboBox
+        options={options}
+        defaultValue={1}
+        inputProps={{ size: 'small' }}
+      />
+      <Select options={options} placeholder='Select something' size='small' />
+    </Flex>
   );
-};
-
-export default App;
+}
