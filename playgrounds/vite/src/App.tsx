2592d4c6020c9fa7582131bdc3c2d99410139d96
Uyen Doan <uyen.doan@bentley.com>
2024-12-05 14:39:07 -0600

    revert App.tsx

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,20 +1,11 @@
-import { ComboBox, Flex, Select } from '@itwin/itwinui-react';
+import { Button } from '@itwin/itwinui-react';
 
-const options = [
-  { value: 1, label: 'Option 1' },
-  { value: 2, label: 'Option 2' },
-  { value: 3, label: 'Option 3' },
-];
-
-export default function App() {
+const App = () => {
   return (
-    <Flex flexDirection='row' style={{ border: 'red solid 1px' }}>
-      <ComboBox
-        options={options}
-        defaultValue={1}
-        inputProps={{ size: 'small' }}
-      />
-      <Select options={options} placeholder='Select something' size='small' />
-    </Flex>
+    <>
+      <Button>Hello world</Button>
+    </>
   );
-}
+};
+
+export default App;
