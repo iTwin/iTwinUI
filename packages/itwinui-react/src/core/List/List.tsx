b6f5e6ef5bc1963ce4d93b00c32bee8168d8cc7d
Rohan <45748283+r100-stack@users.noreply.github.com>
2024-12-13 07:57:50 -0500

    Fix test

--- packages/itwinui-react/src/core/List/List.tsx
+++ packages/itwinui-react/src/core/List/List.tsx
@@ -6,11 +6,20 @@ import * as React from 'react';
 import { Box, type PolymorphicForwardRefComponent } from '../../utils/index.js';
 import { FloatingDelayGroup } from '@floating-ui/react';
 import { defaultTooltipDelay } from '../Tooltip/Tooltip.js';
+import cx from 'classnames';
 
 export const List = React.forwardRef((props, ref) => {
+  const { className, ...rest } = props;
+
   return (
     <FloatingDelayGroup delay={defaultTooltipDelay}>
-      <Box as='div' ref={ref} className='iui-list' role='list' {...props} />
+      <Box
+        as='div'
+        ref={ref}
+        role='list'
+        {...rest}
+        className={cx('iui-list', className)}
+      />
     </FloatingDelayGroup>
   );
 }) as PolymorphicForwardRefComponent<'div'>;
