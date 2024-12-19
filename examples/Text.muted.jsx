25583c44802a372df4e9d88674ba803e20c75547
Uyen Doan <uyen.doan@bentley.com>
2024-12-19 15:00:18 -0600

    flex import in live demo

--- examples/Text.muted.jsx
+++ examples/Text.muted.jsx
@@ -3,11 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
-import { Text } from '@itwin/itwinui-react';
+import { Flex, Text } from '@itwin/itwinui-react';
 
 export default () => {
   return (
-    <Flex>
+    <Flex flexDirection='column'>
       <Text>This is a regular text</Text>
       <Text isMuted>This is a muted text</Text>
     </Flex>
