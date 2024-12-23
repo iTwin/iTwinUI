8dc34afac752ea49e9678454675bdcb82ca9c82c
Uyen Doan <uyen.doan@bentley.com>
2024-12-23 15:31:37 -0600

    combined block and inline label in one example

--- examples/Label.displayStyles.jsx
+++ examples/Label.displayStyles.jsx
@@ -6,22 +6,15 @@ import * as React from 'react';
 import { InputGrid, Input, Label, LabeledSelect } from '@itwin/itwinui-react';
 
 export default () => {
-  const [displayStyle, setDisplayStyle] = React.useState('block');
   return (
     <div className='demo-container'>
-      <LabeledSelect
-        label='Display style:'
-        options={[
-          { value: 'block', label: 'Block (default)' },
-          { value: 'inline', label: 'Inline' },
-        ]}
-        onChange={(value) => setDisplayStyle(value)}
-      />
-      <InputGrid
-        labelPlacement={displayStyle === 'inline' ? 'inline' : 'default'}
-      >
-        <Label displayStyle={displayStyle}>Name:</Label>
-        <Input defaultValue='James Bond' />
+      <InputGrid>
+        <Label displayStyle='block'>Block label</Label>
+        <Input />
+      </InputGrid>
+      <InputGrid labelPlacement='inline'>
+        <Label displayStyle='inline'>Inline label</Label>
+        <Input />
       </InputGrid>
     </div>
   );
