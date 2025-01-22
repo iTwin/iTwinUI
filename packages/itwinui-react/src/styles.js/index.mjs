d138510c3d4cc198702ac50db37e27b98bc57200
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-01-22 09:21:12 -0500

    Continue using module.css

--- packages/itwinui-react/src/styles.js/index.mjs
+++ packages/itwinui-react/src/styles.js/index.mjs
@@ -5,7 +5,7 @@
 import { version } from '../../package.json';
 
 // Side-effect import to ensure CSS file is generated even though the classes are not used.
-import './styles.css';
+import styles from './styles.module.css';
 
 // This proxy converts `iui-` to `_iui3-` in class names with very little code.
 // This is more efficient than exporting the entire mapping of original-to-transformed classes.
