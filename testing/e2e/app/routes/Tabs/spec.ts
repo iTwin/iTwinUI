7810cd166e968c3b56876402921f2de3ff17f304
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-03-19 10:56:03 -0400

    Fix test

--- testing/e2e/app/routes/Tabs/spec.ts
+++ testing/e2e/app/routes/Tabs/spec.ts
@@ -1,24 +1,33 @@
 import { expect, test } from '@playwright/test';
 
 // https://github.com/iTwin/iTwinUI/issues/2465
-test('Keyboard navigation should work on virtualized tree', async ({
+test('tab stripe is positioned correctly when tablist overflows/scrolls', async ({
   page,
 }) => {
   await page.goto('/Tabs');
 
   const testWrapper = page.getByTestId('test-wrapper');
+  const tabsWrapper = page.getByTestId('tabs-wrapper');
+  const tabs = page.getByRole('tab');
 
-  // Resize testContainer to trigger overflow
+  // Resize testWrapper to trigger overflow
   await testWrapper.evaluate((el) => {
     el.style.width = '585px';
   });
 
   await page.getByTestId('test1').click();
 
-  const tabsWrapper = page.getByTestId('tabs-wrapper');
-  const stripePosition = await tabsWrapper.evaluate((el) => {
-    return getComputedStyle(el).getPropertyValue('--iui-tabs-stripe-position');
-  });
+  const stripePosition = await tabsWrapper.evaluate((el) =>
+    getComputedStyle(el).getPropertyValue('--iui-tabs-stripe-position'),
+  );
 
-  await expect(stripePosition).toContain('658'); // The stripe should be where test1 is. i.e. at 658px
+  // test1 is the third tab, so we need to get the width of the first two tabs
+  const expectedStripePosition = await tabs.evaluateAll((elements) =>
+    elements
+      .slice(0, 2)
+      .map((el) => el.getBoundingClientRect().width)
+      .reduce((a, b) => a + b, 0),
+  );
+
+  await expect(stripePosition).toEqual(`${expectedStripePosition}px`);
 });
