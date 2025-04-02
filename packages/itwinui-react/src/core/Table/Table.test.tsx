26638674249cc3d26e4dc2933479261131658fb6
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-04-02 18:56:42 -0400

    Failing unit tests to e2e tests

--- packages/itwinui-react/src/core/Table/Table.test.tsx
+++ packages/itwinui-react/src/core/Table/Table.test.tsx
@@ -3924,38 +3924,6 @@ it('should render row with loading status', () => {
   expect(rows[1]).not.toHaveClass(`iui-loading`);
 });
 
-it('should navigate through table filtering with the keyboard', async () => {
-  const onFilter = vi.fn();
-  const mockedColumns = [
-    {
-      id: 'name',
-      Header: 'Name',
-      accessor: 'name',
-      Filter: tableFilters.TextFilter(),
-      fieldType: 'text',
-    },
-  ];
-  renderComponent({
-    columns: mockedColumns,
-    onFilter,
-  });
-
-  await userEvent.tab(); // tab to filter icon button
-  await userEvent.keyboard('{Enter}');
-  await userEvent.keyboard('2');
-  await userEvent.tab(); // tab to filter menu 'Filter' submit button
-  await userEvent.keyboard('{Enter}');
-  expect(onFilter).toHaveBeenCalledWith(
-    [{ fieldType: 'text', filterType: 'text', id: 'name', value: '2' }],
-    expect.objectContaining({ filters: [{ id: 'name', value: '2' }] }),
-    expect.arrayContaining([
-      expect.objectContaining({
-        values: expect.objectContaining({ name: 'Name2' }),
-      }),
-    ]),
-  );
-});
-
 it('should ignore top-level Header if one is passed', async () => {
   const data = mockedData();
   const { container } = render(
@@ -4050,43 +4018,4 @@ it('should pass custom props to different parts of Table', () => {
   ) as HTMLElement;
   expect(emptyTableContent).toBeTruthy();
   expect(emptyTableContent.style.fontSize).toBe('12px');
-});
-
-it('should apply clamp, if cell is string value and no custom Cell is rendered', () => {
-  const data = [{ name: 'name' }];
-  const columns: Column<TestDataType>[] = [
-    {
-      Header: 'Name',
-      accessor: 'name',
-      cellClassName: 'test-cell',
-    },
-  ];
-  const { container } = renderComponent({
-    columns,
-    data,
-  });
-  const host = container.querySelector('.test-cell');
-  expect(host?.shadowRoot).toBeTruthy();
-  const lineClamp = host?.shadowRoot?.querySelector('div');
-  expect(lineClamp).toBeTruthy();
-});
-
-it('should not apply clamp, if custom Cell is used', () => {
-  const data = [{ name: 'name' }];
-  const columns: Column<TestDataType>[] = [
-    {
-      Header: 'Name',
-      accessor: 'name',
-      cellClassName: 'test-cell',
-      Cell: () => 'my custom content',
-    },
-  ];
-  const { container } = renderComponent({
-    columns,
-    data,
-  });
-  const host = container.querySelector('.test-cell');
-  expect(host?.shadowRoot).toBeTruthy();
-  const lineClamp = host?.shadowRoot?.querySelector('div');
-  expect(lineClamp).toBeNull();
 });
