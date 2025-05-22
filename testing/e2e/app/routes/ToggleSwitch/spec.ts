08fa7d64dea79486cb27ff565ec8b1c4e016db72
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-05-22 11:45:15 -0400

    Unit tests to e2e

--- testing/e2e/app/routes/ToggleSwitch/spec.ts
+++ testing/e2e/app/routes/ToggleSwitch/spec.ts
@@ -1,60 +1,99 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('className, style, and ...rest are applied on the correct element', () => {
-  [true, false].forEach((shouldPassWrapperProps) => {
-    test(`shouldPassWrapperProps: ${shouldPassWrapperProps}`, async ({
-      page,
+  (
+    [
+      {
+        shouldPassWrapperProps: false,
+        themeProviderConsistentPropsSpread: false,
+        expectedClassNameAndStyleLocation: 'wrapper',
+      },
+      {
+        shouldPassWrapperProps: true,
+        themeProviderConsistentPropsSpread: false,
+        expectedClassNameAndStyleLocation: 'input',
+      },
+      {
+        shouldPassWrapperProps: false,
+        themeProviderConsistentPropsSpread: true,
+        expectedClassNameAndStyleLocation: 'input',
+      },
+      {
+        shouldPassWrapperProps: true,
+        themeProviderConsistentPropsSpread: true,
+        expectedClassNameAndStyleLocation: 'input',
+      },
+    ] as const
+  ).forEach(
+    ({
+      shouldPassWrapperProps,
+      themeProviderConsistentPropsSpread,
+      expectedClassNameAndStyleLocation,
     }) => {
-      await page.goto(
-        `/ToggleSwitch?shouldPassWrapperProps=${shouldPassWrapperProps}`,
-      );
+      test(`shouldPassWrapperProps: ${shouldPassWrapperProps}, themeProviderConsistentPropsSpread: ${themeProviderConsistentPropsSpread}`, async ({
+        page,
+      }) => {
+        await page.goto(
+          `/ToggleSwitch?shouldPassWrapperProps=${shouldPassWrapperProps}&themeProviderConsistentPropsSpread=${themeProviderConsistentPropsSpread}`,
+        );
 
-      const wrapper = page.locator("div:has(> input[role='switch'])");
-      const input = page.locator('input[role="switch"]');
+        const wrapper = page.locator("div:has(> input[role='switch'])");
+        const input = page.locator('input[role="switch"]');
 
-      if (shouldPassWrapperProps) {
-        // className goes to input. wrapperProps.className goes to wrapper.
-        await expect(wrapper).toContainClass('wrapper-class');
-        await expect(wrapper).not.toContainClass('my-class');
-        await expect(input).not.toContainClass('wrapper-class');
-        await expect(input).toContainClass('my-class');
+        if (shouldPassWrapperProps) {
+          await expect(wrapper).toContainClass('wrapper-class');
+          await expect(input).not.toContainClass('wrapper-class');
 
-        // style goes to input. wrapperProps.style goes to wrapper.
-        await expect(
-          await wrapper.evaluate((el) => getComputedStyle(el).backgroundColor),
-        ).toBe('rgb(0, 0, 255)');
-        await expect(
-          await input.evaluate((el) => getComputedStyle(el).backgroundColor),
-        ).toBe('rgb(255, 0, 0)');
+          await expect(
+            await wrapper.evaluate(
+              (el) => getComputedStyle(el).backgroundColor,
+            ),
+          ).toBe('rgb(0, 0, 255)');
+          await expect(
+            await input.evaluate((el) => getComputedStyle(el).backgroundColor),
+          ).not.toBe('rgb(0, 0, 255)');
+        }
 
-        // ...rest goes to input. wrapperProps go to wrapper.
-        await expect(wrapper).toHaveAttribute(
+        if (expectedClassNameAndStyleLocation === 'wrapper') {
+          // className
+          await expect(wrapper).toContainClass('my-class');
+          await expect(input).not.toContainClass('my-class');
+
+          // style
+          await expect(
+            await wrapper.evaluate(
+              (el) => getComputedStyle(el).backgroundColor,
+            ),
+          ).toBe('rgb(255, 0, 0)');
+          await expect(
+            await input.evaluate((el) => getComputedStyle(el).backgroundColor),
+          ).not.toBe('rgb(255, 0, 0)');
+        } else {
+          // className
+          await expect(wrapper).not.toContainClass('my-class');
+          await expect(input).toContainClass('my-class');
+
+          // style
+          await expect(
+            await wrapper.evaluate(
+              (el) => getComputedStyle(el).backgroundColor,
+            ),
+          ).not.toBe('rgb(255, 0, 0)');
+          await expect(
+            await input.evaluate((el) => getComputedStyle(el).backgroundColor),
+          ).toBe('rgb(255, 0, 0)');
+        }
+
+        // rest props should always go to input
+        await expect(wrapper).not.toHaveAttribute(
           'data-dummy-data-attr',
-          'dummy-value-wrapper-props',
+          'dummy-value-root',
         );
         await expect(input).toHaveAttribute(
           'data-dummy-data-attr',
           'dummy-value-root',
         );
-      } else {
-        // className goes to the wrapper.
-        await expect(wrapper).toContainClass('my-class');
-        await expect(wrapper).not.toContainClass('wrapper-class');
-        await expect(input).not.toContainClass('my-class');
-        await expect(input).not.toContainClass('wrapper-class');
-
-        // style goes to the wrapper.
-        await expect(
-          await wrapper.evaluate((el) => getComputedStyle(el).backgroundColor),
-        ).toBe('rgb(255, 0, 0)');
-
-        // ...rest goes to the input.
-        await expect(wrapper).not.toHaveAttribute('data-dummy-data-attr');
-        await expect(input).toHaveAttribute(
-          'data-dummy-data-attr',
-          'dummy-value-root',
-        );
-      }
-    });
-  });
+      });
+    },
+  );
 });
