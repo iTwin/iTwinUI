7385416f524c332591ad5bc02310b3c1af275ca2
Rohan <45748283+r100-stack@users.noreply.github.com>
2025-05-22 09:51:28 -0400

    Undo potentially conflicting changes

--- packages/itwinui-react/src/core/ToggleSwitch/ToggleSwitch.tsx
+++ packages/itwinui-react/src/core/ToggleSwitch/ToggleSwitch.tsx
@@ -6,7 +6,6 @@ import * as React from 'react';
 import cx from 'classnames';
 import { Box, SvgCheckmark } from '../../utils/index.js';
 import type { PolymorphicForwardRefComponent } from '../../utils/index.js';
-import { ThemeProviderFutureContext } from '../ThemeProvider/ThemeProvider.js';
 
 type ToggleSwitchProps = {
   /**
@@ -59,17 +58,6 @@ type ToggleSwitchProps = {
 
 /**
  * A switch for turning on and off.
- *
- * ---
- *
- * Note: `className` and `style` props are applied depending on the `ThemeProvider`'s `future.consistentPropsSpread` prop:
- * - `consistentPropsSpread=false/undefined`: `className` and `style` applied on the wrapper instead of the `input` element where all the other props are applied.
- * - `consistentPropsSpread=true`: `className` and `style` applied on the `input` element where all the other props are applied.
- *
- * Also consider the `wrapperProps` props for passing `className`, `style`, and other props to the wrapper.
- *
- * ---
- *
  * @example
  * <caption>Basic toggle</caption>
  * <ToggleSwitch onChange={(e) => console.log(e.target.checked)} defaultChecked />
@@ -85,38 +73,6 @@ type ToggleSwitchProps = {
  * @example
  * <caption>Toggle with icon</caption>
  * <ToggleSwitch label='With icon toggle' icon={<svg viewBox='0 0 16 16'><path d='M1 1v14h14V1H1zm13 1.7v10.6L8.7 8 14 2.7zM8 7.3L2.7 2h10.6L8 7.3zm-.7.7L2 13.3V2.7L7.3 8zm.7.7l5.3 5.3H2.7L8 8.7z' /></svg>} />
- *
- * @example
- * <caption>ThemeProvider's consistentPropsSpread: false/undefined</caption>
- * <ThemeProvider>
- *   <ToggleSwitch
- *     className='my-class' // applied to wrapper
- *     style={{ width: 80 }} // applied to wrapper
- *     wrapperProps={{
- *       className: 'my-wrapper-class', // applied to wrapper
- *       style: { width: 80 }, // applied to wrapper
- *     }}
- *
- *     // Other props are applied to input
- *     data-dummy='value' // applied to input
- *   />
- * </ThemeProvider>
- *
- * @example
- * <caption>ThemeProvider's consistentPropsSpread: true</caption>
- * <ThemeProvider future={{ consistentPropsSpread: true }}>
- *   <ToggleSwitch
- *     className='my-class' // applied to input
- *     style={{ width: 80 }} // applied to input
- *     wrapperProps={{
- *       className: 'my-wrapper-class', // applied to wrapper
- *       style: { width: 80 }, // applied to wrapper
- *     }}
- *
- *     // Other props are applied to input
- *     data-dummy='value' // applied to input
- *   />
- * </ThemeProvider>
  */
 export const ToggleSwitch = React.forwardRef((props, ref) => {
   const {
@@ -132,32 +88,13 @@ export const ToggleSwitch = React.forwardRef((props, ref) => {
     ...rest
   } = props;
 
-  const consistentPropsSpread =
-    React.useContext(ThemeProviderFutureContext)?.consistentPropsSpread ===
-    true;
-
   // Disallow custom icon for small size, but keep the default checkmark when prop is not passed.
   const shouldShowIcon =
     iconProp === undefined || (iconProp !== null && size !== 'small');
 
-  const consistencyRelatedProps = React.useMemo(() => {
-    if (consistentPropsSpread) {
-      return {
-        wrapperProps: { className: undefined, style: undefined },
-        inputProps: { ...rest, className, style },
-      };
-    }
-
-    return {
-      wrapperProps: { className, style },
-      inputProps: { ...rest, className: undefined, style: undefined },
-    };
-  }, [className, consistentPropsSpread, rest, style]);
-
   return (
     <Box
       as={label ? 'label' : 'div'}
-      {...consistencyRelatedProps.wrapperProps}
       {...(wrapperProps as any)}
       className={cx(
         'iui-toggle-switch-wrapper',
@@ -166,26 +103,23 @@ export const ToggleSwitch = React.forwardRef((props, ref) => {
           'iui-label-on-right': label && labelPosition === 'right',
           'iui-label-on-left': label && labelPosition === 'left',
         },
-        consistencyRelatedProps.wrapperProps.className,
+        className,
         wrapperProps?.className,
       )}
+      data-iui-size={size}
       style={{
-        ...consistencyRelatedProps.wrapperProps.style,
+        ...style,
         ...wrapperProps?.style,
       }}
-      data-iui-size={size}
     >
       <Box
         as='input'
-        {...consistencyRelatedProps.inputProps}
-        className={cx(
-          'iui-toggle-switch',
-          consistencyRelatedProps.inputProps.className,
-        )}
+        className='iui-toggle-switch'
         type='checkbox'
         role='switch'
         disabled={disabled}
         ref={ref}
+        {...rest}
       />
       {shouldShowIcon && (
         <Box as='span' className='iui-toggle-switch-icon' aria-hidden>
