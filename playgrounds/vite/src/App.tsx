2b457ad8bbc5ef53f919e532a5cb4fef99d7e268
pachhaibipin <bipin.pachhai@bentley.com>
2025-11-05 12:12:30 -0600

    revert to original

--- playgrounds/vite/src/App.tsx
+++ playgrounds/vite/src/App.tsx
@@ -1,56 +1,11 @@
-import { Table } from '@itwin/itwinui-react';
-import * as React from 'react';
+import { Button } from '@itwin/itwinui-react';
 
-export default function App() {
-  const generateItem = React.useCallback((index: any, parentRow = '') => {
-    const keyValue = parentRow ? `${parentRow}.${index + 1}` : `${index + 1}`;
-    return {
-      product: `Product ${keyValue}`,
-      price: ((index % 10) + 1) * 15,
-      subRows: [
-        {
-          product: `Sub Product ${keyValue}`,
-          price: (((index % 10) + 1) * 15) / 2,
-        },
-      ],
-    };
-  }, []);
-
-  const data = React.useMemo(
-    () =>
-      Array(3)
-        .fill(null)
-        .map((_, index) => generateItem(index)),
-    [generateItem],
-  );
-
-  const columns: any = React.useMemo(
-    () => [
-      {
-        id: 'product',
-        Header: 'Product',
-        accessor: 'product',
-      },
-      {
-        id: 'price',
-        Header: 'Price',
-        accessor: 'price',
-      },
-    ],
-    [],
-  );
-
+const App = () => {
   return (
-    <div className='demo-container'>
-      <Table
-        columns={columns}
-        emptyTableContent='No data.'
-        data={data}
-        isSelectable={true}
-        selectionMode='multi'
-        selectSubRows={false}
-        selectRowOnAllSubRows={false}
-      />
-    </div>
+    <>
+      <Button>Hello world</Button>
+    </>
   );
-}
+};
+
+export default App;
